98e11a18cc1f523791e2c9f172f47f23fee09374
msung99 <88240193+msung99@users.noreply.github.com>
2022-03-07 17:37:40 +0900

    [Silver III] Title: 키로거, Time: 240 ms, Memory: 99376 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int n;
+string str;
+
+struct node{
+  char data;
+  node* prev, *next = NULL;
+};
+
+class linkedlist{
+private:
+  node* cursor, *head, *tail;
+
+public:
+  linkedlist() // 생성자
+  {
+    head = new node;
+    tail = new node;
+
+    head -> next = tail;
+    tail -> prev = head;
+
+    cursor = head;
+  }
+
+  node* gethead(){
+    return head;
+  }
+
+  // 커서 포인터 오른쪽에다 노드 삽입
+  void enterpass(char _data){
+    node* newnode = new node;
+    newnode -> data = _data;
+    
+    cursor->next->prev = newnode;
+    newnode -> next = cursor -> next;
+
+    cursor -> next = newnode;
+    newnode -> prev = cursor;
+
+    cursor = newnode;
+  }
+
+  // 커서 포인터 왼쪽으로 한칸이동
+  void moveleft(){
+    if(cursor != head)
+    {
+      cursor = cursor->prev; 
+    }
+  }
+ 
+  // 커서 포인터 오른쪽으로 한칸이동
+  void moveright(){
+    if(cursor ->next != tail)
+    {
+      cursor = cursor->next;
+    }
+  }
+
+  // 커서 포인터의 노드 삭제
+  void deletepass(){
+    if(cursor != head){
+      cursor->next ->prev = cursor-> prev;
+      cursor->prev ->next = cursor->next;  
+    
+      cursor = cursor -> prev;
+    }
+  }
+
+  // 링크드 리스트 출력
+  void printpass(){
+    for(node* temp = head -> next; temp != tail; temp = temp -> next)
+    {
+      cout << temp -> data;
+    }
+    cout << '\n';
+  }
+};
+
+int main(void)
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin >> n;
+  
+  while(n--)
+  {
+    linkedlist pw;
+
+    cin >> str;
+
+    for(int i=0; i<str.size(); i++)
+    {
+      if(str[i] == '-')
+      {
+        pw.deletepass();
+      }
+      else if(str[i] == '<')
+      {
+        pw.moveleft();
+      }
+      else if(str[i] == '>')
+      {
+        pw.moveright();
+      }
+      else // 명령어가 아닌 그냥 문자인 경우
+      {
+        pw.enterpass(str[i]);
+      }
+    }
+    pw.printpass(); // 한 문장에대해 명령어 실행이 끝나면 결과를 출력
+  }
+  return 0;
+}
