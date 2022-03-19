a8f51d1ba55c0d4cdcc26aaf2c6f72ef472cfccb
msung99 <88240193+msung99@users.noreply.github.com>
2022-03-19 20:17:41 +0900

    [Silver I] Title: 그림, Time: 28 ms, Memory: 3988 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <utility>
+#include <queue>
+#include <string>
+using namespace std;
+
+int board[502][502];
+int visit[502][502];
+
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m;
+	int mx = 0; // 그림의 최댓값
+	int num = 0; // 그림의 수
+	cin >> n >> m;
+
+	// board판 초기화
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> board[i][j];
+		}
+	}
+
+	// board판의 모든 정점에 대해 실행
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			// 상하좌우 탐색 시작 이전에, 탐색을 시작할 정점을 설정
+			if (board[i][j] == 0 || visit[i][j])
+				continue;
+			// if문에 걸리지 않은 board[i][j] 가 시작 정점이 된다.
+			queue<pair<int, int>> q;
+			pair<int, int> start = { i,j }; // 시작 좌표(i,j)
+			q.push(start);
+			visit[i][j] = 1; // 방문 처리
+			num++; // 그림의 총 개수 증가
+
+			int case_max = 0; // 각 그림의 넓이 
+			while (!q.empty())
+			{
+				pair<int, int> cur = q.front();
+				q.pop();
+				case_max++;
+
+				// 상하좌우 탐색
+				for (int i = 0; i < 4; i++) {
+					int nx = cur.first + dx[i];
+					int ny = cur.second + dy[i];
+
+					if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+						continue;
+					if (board[nx][ny] != 1 || visit[nx][ny]) // 이전에 방문한 노드이거나, 그림이 없는 칸인 경우
+						continue;
+
+					q.push({ nx,ny });
+					visit[nx][ny] = 1;
+				}
+			}
+			mx = max(mx, case_max);
+		}
+	}
+	cout << num << '\n' << mx;
+}
