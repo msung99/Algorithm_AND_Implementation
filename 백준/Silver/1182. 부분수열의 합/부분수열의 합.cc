e4b55eb504d5a3a8a13f18a454c5a0da8ab3bfc7
이민성 <88240193+msung99@users.noreply.github.com>
2022-03-27 00:46:26 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int n, s;
+int num_list[100];
+
+int cnt;
+
+void func(int cur, int total) // cur : 현재 탐색하며 살펴보고 있는 숫자의 인덱스 번호, total : 부분수열의 원소들의 합
+{
+	if (cur == n) // 부분수열의 원소가 n개가 되었을 때
+	{
+		if (total == s) // 만든 부분수열의 원소들의 합이 s 가 되었다면
+			cnt++; // 경우의 수 카운팅
+		return;
+	}
+	// 부분수열에 들어갈 주어진 숫자들에 대해 2가지 경우로 나눔. 1) 수열에 그 숫자( = num_list[cur])를 포함시킨다 2) 포함 안시킨다
+	func(cur + 1, total); // 부분수열에 포함 안시키는 경우
+	func(cur + 1, total + num_list[cur]); // 포함시키는 경우 
+}
+
+int main(void)
+{
+	cin >> n >> s;
+	for (int i = 0; i < n; i++)
+		cin >> num_list[i];
+
+	func(0, 0);
+	if (s == 0)
+		cnt--;
+	cout << cnt;
+}
