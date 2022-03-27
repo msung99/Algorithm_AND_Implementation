e211d5d01c06df66a9037e885fb6e1debf82051d
이민성 <88240193+msung99@users.noreply.github.com>
2022-03-27 20:19:56 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[10]; // arr : 출력할 부분수열 원소들을 모아두는 배열
+int num[10]; // num : 입력받은 원소들을 저장하고 있는 기존 배열
+int n, m;
+// bool visited[10];
+
+void func(int k, int start)
+{
+	if (k == m)
+	{
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;
+	}
+
+	int tmp = -1;
+	for (int i = start; i < n; i++)
+	{
+		if (tmp != num[i]) // 이전 수열의 마지막 항과 새로운 수열의 마지막 항이 같으면 중복 수열
+		{
+			arr[k] = num[i];
+			tmp = arr[k];
+			func(k + 1, i);
+		}
+	}
+}
+
+
+int main(void)
+{
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+		cin >> num[i];
+
+	sort(num, num + n);  // 오름차순 정렬
+
+	func(0, 0);
+}
