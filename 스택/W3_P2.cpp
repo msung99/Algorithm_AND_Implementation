4fb4239f5631f7961063d3255e7022b2ebf2369d
이민성 <88240193+msung99@users.noreply.github.com>
2022-04-12 14:45:51 +0900

    Update W3_P2.cpp

--- 스택/W3_P2.cpp
+++ 스택/W3_P2.cpp
@@ -7,91 +7,79 @@
 #include <string>
 using namespace std;
 
-class arrayStack {
-public:
-	arrayStack(int capacity);
-	bool empty();
-	int size();
-	char top();
-	void push(char data);
-	void pop();
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Stack {
 private:
-	int* arr;  // 스택의 원소를 저장하는 배열
-	int capacity; // 스택의 최대 크기를 저장하는 변수
-	int topIndex; // 배열에서 스택의 top이 저장되어 있는 인덱스를 저장하는 변수
+	char* arr;
+	int capacity;
+	int topIndex;
+public:
+	Stack(int capacity);
+	void push(char data);
+	char top();
+	void pop();
+	int size();
+	bool empty();
 };
 
-arrayStack::arrayStack(int capacity) {
+Stack::Stack(int capacity)
+{
 	this->capacity = capacity;
-	arr = new int[capacity];
+	arr = new char[capacity];
 	topIndex = -1;
 }
 
-bool arrayStack::empty() {
-	if (topIndex == -1) {
-		return true;
-	}
-	else {
-		return false;
-	}
+bool Stack::empty()
+{
+	return (topIndex == -1);
 }
 
-char arrayStack::top() {
-	return char(arr[topIndex]);
-}
-
-void arrayStack::push(char data) {
-	if (size() == capacity)
-	{
-		cout << "FULL";
-		return;
-	}
-
-	topIndex++;
-	arr[topIndex] = data;
-}
-
-int arrayStack::size() {
+int Stack::size()
+{
 	return topIndex + 1;
 }
 
-void arrayStack::pop() {
-	if (empty()) {
+void Stack::pop()
+{
+	if (empty())
 		return;
-	}
+	topIndex = topIndex - 1;
+}
 
-	cout << top();
-	topIndex--;
+char Stack::top()
+{
+	if (empty())
+		return -1;
+
+	return arr[topIndex];
+}
+
+void Stack::push(char data)
+{
+	arr[topIndex + 1] = data;
+	topIndex++;
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int n;
-	cin >> n;
-
-	arrayStack s(1000);
-
-	// 1-2+3 
-	// 정상 :  12-3+
-	// 실제 :  1-2343
-
-
-	// 12-3+
-	// 스택:+
-	while (n--)
+	Stack s(1000);
+	int testCase;
+	cin >> testCase;
+	while (testCase--)
 	{
 		string str;
 		cin >> str;
-		for (int i = 0; i < str.length(); i++) 
+		for (int i = 0; i < str.length(); i++)
 		{
 			if (str[i] == '+' || str[i] == '-')
 			{
 				while (!s.empty())
 				{
-					s.pop(); 
+					cout << s.top();
+					s.pop();
 				}
 				s.push(str[i]);
 			}
@@ -99,6 +87,7 @@ int main(void)
 			{
 				while (s.top() == '*' || s.top() == '/')
 				{
+					cout << s.top();
 					s.pop();
 				}
 				s.push(str[i]);
@@ -108,9 +97,11 @@ int main(void)
 				cout << str[i];
 			}
 		}
-		
 		while (!s.empty())
+		{
+			cout << s.top();
 			s.pop();
-		cout << '\n';
+		}
+		cout << endl;
 	}
 }
