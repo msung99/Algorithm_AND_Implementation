47ae18da4f96b2ff3b4979484d7327cabffd1192
이민성 <88240193+msung99@users.noreply.github.com>
2022-05-28 09:07:30 +0900

    Update W9_P1.cpp

--- 우선순위 큐/W9_P1.cpp
+++ 우선순위 큐/W9_P1.cpp
@@ -3,25 +3,26 @@
 #include <vector>
 using namespace std;
 
-// 비교자 - 두 원소의 우선순위를 비교하는 ADT
-// compare(p ,q) : p가 q보다 우선순위가 높은지를 판단.
-// ex) compare(10,3) : 10 > 3 ? => false (10이 3보다 우선순위가 낮으므로 false를 리턴)
+// 배열에서 우선순위가 가장 높은 원소는 맨 뒤에 위치해있다. => minPQ 인 경우 : 최솟값이 맨뒤에 위치, maxPQ인 경우 : 최댓값이 맨뒤에 위치
+// max PQ 를 가정.  문제의 출력예시 보면, 오름차순으로 정렬되어 있다. 즉, 우선순위가 높은 갚이 배열의 뒤쪽에 위치해 있는다.
+
 struct compare {
 	bool operator()(const int& p, const int& q) const {
-		return p > q;  // 만일 maxPQ 가 아닌 minPQ 의 비교자를 구현한다면, 
-	}                  // 우선순위가 최댓값 아닌 최솟값이므로 "return p < q" 로 수정해야 한다. (여기서는 minPQ를 가정)
+		return p > q; // p 가 우선순위 가 더 높은 경우, 즉 p 의 값이 더 큰 경우에 true를 리턴
+	}
 };
 
 
+// 참고로 insert, print 함수만 구현해도 된다. (removeMax, max 는 구현할 필요x)
 class sortedSeqPQ {
 private:
-	vector<int> seq; // 내림차순으로 정렬된 시퀀스를 저장하는 벡터
+	vector<int> seq;
 public:
 	int size();
 	bool empty();
 	void insert(int e);
-	int min();
-	void removeMin();
+	int max();
+	void removeMax(); // 우선순위가 가장 높은 값(= 최댓값) 을 삭제. 이때 최댓값은 배열의 끝에 위치해 있음.
 	void print();
 };
 
@@ -30,36 +31,33 @@ int sortedSeqPQ::size() {
 }
 
 bool sortedSeqPQ::empty() {
-	return (seq.size() == 0);
+	return seq.size() == 0;
 }
 
-// unsortedSeqPq 과 달리, 시퀀스 벡터에 원소를 삽입시 그냥 삽입하지 않고 삽입될 적절한 위치를 찾아가서 삽입된다.
 void sortedSeqPQ::insert(int data) {
 	compare C;
 	int idx = 0;
 
-	// data 원소가 삽입될 적절한 위치를 탐색
+	// 삽입할 원소보다 우선순위가 더 높은 원소가 처음 등장하는 위치 탐색
 	for (idx = 0; idx < seq.size(); idx++) {
-		if (C(seq[idx], data))  // 삽입할 원소보다 우선순위가 높은(= key값이 더 작은) 원소가 처음 등장하는 위치를 탐색 
+		if (C(seq[idx], data)) // 삽입될 적절한 위치를 찾은 경우
 			break;
 	}
-	seq.insert(seq.begin() + idx, data);  // 찾은 위치에 새로운 데이터 값 삽입
+	seq.insert(seq.begin() + idx, data);
 }
 
-// unsortedSeqPQ 와 달리 이미 시퀀스가 정렬되있는 상태이므로, 그냥 바로 최솟값 리턴해주면 된다.
-int sortedSeqPQ::min() {
+int sortedSeqPQ::max() {
 	if (empty())
 		return -1;
 
-	return seq.back(); // 시퀀스 맨 뒤에 저장되있는 최솟값을 리턴
+	return seq.back(); // 우선순위가 가장 높은 원소인 최댓값은 배열의 맨 뒤에 위치해 있는다.
 }
 
-// unsortedSeqPQ 와 달리 이미 시퀀스가 정렬되있는 상태이므로, 그냥 바로 최솟값을 삭제하면 끝이다.
-void sortedSeqPQ::removeMin() {
+void sortedSeqPQ::removeMax() {
 	if (empty())
 		return;
 
-	seq.pop_back(); // 시퀀스 맨 뒤에 저장되있는 최솟값을 삭제
+	seq.pop_back();  // 우선순위가 가장 높은 원소인 최댓값은 배열의 맨 뒤에 위치해 있는다.
 }
 
 void sortedSeqPQ::print() {
