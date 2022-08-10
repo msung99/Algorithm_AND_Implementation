11e5ffc07320d78ee129726b6fe55c931f1b5000
이민성 <88240193+msung99@users.noreply.github.com>
2022-08-10 21:04:18 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	cin >> n;
+	while (n--) {
+		int data1, data2;
+		cin >> data1 >> data2;
+		cout << data1 + data2 << '\n';
+	}
+}
