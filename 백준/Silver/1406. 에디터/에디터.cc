1fc2441ac3027fbaa037ebfa0b611e67ccb50729
이민성 <88240193+msung99@users.noreply.github.com>
2022-08-23 12:58:54 +0900

    [Silver II] Title: 에디터, Time: 76 ms, Memory: 21020 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.cc
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -1,140 +1,36 @@
-#include <stdio.h>
-#include <iostream>
-#include <algorithm>
-#include <map>
-#include <string>
-#include <vector>
-#include <stdlib.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
-typedef struct node
-{
-    struct node *prev;
-    struct node *next;
-    char data;
-}node;
- 
-node *head = NULL;
-node *cur = NULL;
- 
-string s;
-int commandNum;
- 
-void insert(char data)
-{
-    node *newNode = (node*)malloc(sizeof(node)*1);
-    newNode->data = data;
-    newNode->prev = NULL;
-    newNode->next = NULL;
-    
-    if(head->next == NULL)
-    {
-        head->next = newNode;
-        newNode->prev = head;
-        
-        cur = newNode;
-    }
-    else if(cur->next == NULL)
-    {
-        newNode->prev = cur;
-        cur->next = newNode;
- 
-        cur = newNode;
-    }
-    // 중간에 삽입할 경우 
-    else if(cur->next != NULL)
-    {
-        newNode->next = cur->next;
-        cur->next->prev = newNode;
-        
-        cur->next = newNode;
-        newNode->prev = cur;
-        
-        cur = newNode;
-    }
-}
- 
-void left()
-{
-    if(cur->prev != NULL)
-    {
-        cur = cur->prev;
-    }
-}
- 
-void right()
-{
-    if(cur->next != NULL)
-    {
-        cur = cur->next;
-    }
-}
- 
-void del()
-{
-    if(cur != head)
-    {
-        cur->prev->next = cur->next;
-        
-        // 중간에 삭제하는 경우 
-        if(cur->next != NULL)
-        {
-            cur->next->prev = cur->prev;
-        }
-        else
-        {
-            cur->next = NULL;
-        }
-        
-        cur = cur->prev;
-    }
-}
 
-int main(void)
-{
-  head = new node;
-  head-> next = NULL;
-  head-> prev = NULL;
-
-  cin >> s >> commandNum;
-
-  // 리스트 삽입
-  for(int i=0; i < s.size(); i++)
-  {
-    insert(s[i]);
-  }
-
-  for(int i=1; i <= commandNum; i++)
-  {
-    char command;
-    cin >> command;
-
-    if(command == 'P')
-    {
-      char data;
-      cin >> data;
-      insert(data);
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  string init;
+  cin >> init;
+  list<char> L;
+  for (auto c : init) L.push_back(c);
+  auto cursor = L.end();
+  int q;
+  cin >> q;
+  while (q--) {
+    char op;
+    cin >> op;
+    if (op == 'P') {
+      char add;
+      cin >> add;
+      L.insert(cursor, add);
     }
-    else if(command == 'L')
-    {
-      left();
+    else if (op == 'L') {
+      if (cursor != L.begin()) cursor--;
     }
-    else if(command == 'D')
-    {
-      right();
+    else if (op == 'D') {
+      if (cursor != L.end()) cursor++;
     }
-    else if(command == 'B')
-    {
-      del();
+    else { // 'B'
+      if (cursor != L.begin()) {
+        cursor--;
+        cursor = L.erase(cursor);
+      }
     }
   }
-
-  cur = head -> next;
-
-  // 링크드 리스트 명령어 입력 후 결과 출력
-  while(cur != NULL)
-  {
-    cout << cur -> data;
-    cur = cur -> next;
-  }
+  for (auto c : L) cout << c;
 }
