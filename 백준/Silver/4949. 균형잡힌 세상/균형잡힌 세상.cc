4e60bdf55efc8e83890c014aa2e16d611859c72e
이민성 <88240193+msung99@users.noreply.github.com>
2022-08-29 14:10:31 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -1,72 +1,56 @@
 #include <iostream>
-#include <stack>
 #include <string>
+#include <stack>
 using namespace std;
 
 int main(void)
 {
-	while (1)
-	{
-		stack<char> s;
-		string str;
-		bool check = true;
-		
-		getline(cin, str); // 문자열에 공백이 포함되어 있으므로 getline 으로 읽어들임
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
+	while (true) {
+		string str;
+		// cin >> str;   => cin 은 공백을 기준으로하여 하나의 문자열을 구성하므로, 공백이 기준이 아닌 공백포함 문자열 저장시 getline() 를 활용
+		getline(cin, str); // getline() : <string> 에 정의. string 형에 문자열을 저장시 사용
+		// 인자1 : 입력방식(파일입력, 표준입력 등등), 인자2 : string형 변수명
 		if (str == ".")
 			break;
 
-		for (int i = 0; i < str.size(); i++)
-		{
-			// 여는 괄호인 경후 
-			if (str[i] == '(' || str[i] == '{' || str[i] == '[')
-			{
-				s.push(str[i]); // 스택에 저장
-			}
-			// 닫는 괄호인 경우
-			else if (str[i] == ')' || str[i] == '}' || str[i] == ']')
-			{
-				if (s.empty())
-				{
-					check = false;
+		stack<char> s;
+		bool isValid = true;
+
+		for (auto mychar : str) {
+			//cout << "mychar:" << mychar << '\n';
+
+			if (mychar == '(' || mychar == '[')
+				s.push(mychar);
+
+			else if (mychar == ')') {
+				if (s.empty() || s.top() != '(' ) { // 닫는 괄호를 만났는데, 부적합한 괄호쌍이거나 스택이 비어있는 경우
+					isValid = false;
 					break;
 				}
+				s.pop();
+			}
 
-				if (s.top() == '(')
-				{
-					if (str[i] != ')')
-					{
-						check = false;
-						break; // for문에서 탈출하고, 다음 while 루프 실행
-					}
+			else if (mychar == ']') {
+				if (s.empty() || s.top() != '[') {
+					isValid = false;
+					break;
 				}
-				else if (s.top() == '{')
-				{
-					if (str[i] != '}')
-					{
-						check = false;
-						break;
-					}
-				}
-				else if (s.top() == '[')
-				{
-					if (str[i] != ']')
-					{
-						check = false;
-						break;
-					}
-				}
-				s.pop(); // 스택의 top() 값을 확인해보니 올바른 괄호 쌍일경우 pop()시킴
-			} 
+				s.pop();
+			}
 		}
-		// 문자열을 다 확인하고 나서 스택을 확인해보니 비어있는게 아니면, 
-		// 즉 아직도 처리해야할 열린 괄호가 스택에 담겨있다면 false
-		if (!s.empty())
-			check = false;
 
-		if (check)
+		if (!s.empty())
+			isValid = false;
+
+		if (isValid)
 			cout << "yes\n";
 		else
 			cout << "no\n";
 	}
+
+	return 0;
 }
+
