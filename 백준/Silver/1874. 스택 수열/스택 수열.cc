ef53a400ea607c947424062ef3c3428dff9de897
이민성 <88240193+msung99@users.noreply.github.com>
2022-08-31 15:13:19 +0900

    Update 스택 수열.cc

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,36 +1,35 @@
-#include <iostream>
-#include <string>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	cout.tie(0);
 
 	stack<int> s;
+	int cnt = 1;
+	string ans;
+
 	int n;
 	cin >> n;
-	int count;
-	int t = 1; // 
-	string res;
-
-	while (n--) // n개의 숫자에 대해 실행
-	{
-		cin >> count; 
-		while (t <= count) // count 까지 오름차순으로 숫자를 push 
-		{
-			s.push(t);
-			t++;
-			res += "+\n";
+	while (n--) {
+		int num;
+		cin >> num;
+		// 현재 처리를 해야하는 수인 num 에 접근해서 처리하기 위해, cnt 를 계속 1씩 더해간다.
+		while (cnt <= num) { // 현재 처리를 해야하는 수는 cnt 이다. cnt 이상인 값 num 이 주어지면,
+			s.push(cnt);  // 그 값을 pop 할 수 있게 cnt 가 num 이 될때까지 push 해야한다.
+			cnt++;
+			ans += "+\n";
 		}
-		if (s.top() != count)  
-		{
-			cout << "NO\n";
+		// 값 cnt 에 대한 처리를 하기전에, 올바른 수열인지를 검사한다.
+		if (s.top() != num) {  // 올바르지 않은 수열인 경우
+			cout << "NO \n";
 			return 0;
 		}
+		// 값 cnt 에 대한 처리
 		s.pop();
-		res += "-\n";
+		ans += "- \n";
 	}
-	cout << res;
+	cout << ans;
 }
