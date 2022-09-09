90897509b7125d8e12d05670f328925b716cc0af
이민성 <88240193+msung99@users.noreply.github.com>
2022-09-10 00:52:10 +0900

    [Silver I] Title: 그림, Time: 24 ms, Memory: 3248 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,68 +1,70 @@
-#include <iostream>
-#include <utility>
-#include <queue>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-int board[502][502];
-int visit[502][502];
-
-int dx[4] = { 1,0,-1,0 };
+int board[501][501];
+bool vistied[501][501];
+int dx[4] = { -1,0,1,0 };
 int dy[4] = { 0,1,0,-1 };
+int n, m; 
+
+// 1. 상하좌우로 연결된 그림의 크기를 찾아내기
+// 2. 도화지에 있는 모든 그림을 찾아내기
 
 int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	cout.tie(0);
 
-	int n, m;
-	int mx = 0; // 그림의 최댓값
-	int num = 0; // 그림의 수
 	cin >> n >> m;
 
-	// board판 초기화
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> board[i][j];
 		}
 	}
 
-	// board판의 모든 정점에 대해 실행
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			// 상하좌우 탐색 시작 이전에, 탐색을 시작할 정점을 설정
-			if (board[i][j] == 0 || visit[i][j])
-				continue;
-			// if문에 걸리지 않은 board[i][j] 가 시작 정점이 된다.
-			queue<pair<int, int>> q;
-			pair<int, int> start = { i,j }; // 시작 좌표(i,j)
-			q.push(start);
-			visit[i][j] = 1; // 방문 처리
-			num++; // 그림의 총 개수 증가
+	int count = 0;
+	int max_area = 0;
+	int area_count = 0;
 
-			int case_max = 0; // 각 그림의 넓이 
-			while (!q.empty())
+	for (int i = 0; i < n; i++) 
+	{
+		for (int j = 0; j < m; j++) 
+		{
+			if (board[i][j] == 0 || vistied[i][j])
+				continue;
+
+			area_count++;
+			queue<pair<int, int>> q;
+			q.push({ i,j });
+			vistied[i][j] = true;
+			while (!q.empty()) 
 			{
 				pair<int, int> cur = q.front();
 				q.pop();
-				case_max++;
 
-				// 상하좌우 탐색
-				for (int i = 0; i < 4; i++) {
-					int x = cur.first + dx[i];
-					int y = cur.second + dy[i];
+				count++;
+				
+				// 4방향으로 뻗음
+				for (int k = 0; k < 4; k++) 
+				{
+					int nx = cur.first + dx[k];
+					int ny = cur.second + dy[k];
 
-					if (x < 0 || x >= n || y < 0 || y >= m)
+					if (nx < 0 || nx >= n || ny < 0 || ny >= m)
 						continue;
-					if (board[x][y] != 1 || visit[x][y]) // 이전에 방문한 노드이거나, 그림이 없는 칸인 경우
+
+					if (vistied[nx][ny] || board[nx][ny] == 0)
 						continue;
 
-					q.push({ x,y });
-					visit[x][y] = 1;
+					q.push({ nx,ny });
+					vistied[nx][ny] = true;
 				}
 			}
-			mx = max(mx, case_max);
+			max_area = max(max_area, count);
+			count = 0;
 		}
 	}
-	cout << num << '\n' << mx;
+	cout << area_count << '\n' << max_area;
 }
