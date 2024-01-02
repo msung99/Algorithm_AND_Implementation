0a83f0b1b09bf8cfd1fe4594564521afbe5cd7ba
msung99 <88240193+msung99@users.noreply.github.com>
2024-01-03 00:14:07 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,46 +1,46 @@
-#include <iostream>
-#include <string>
-#include <stack>
+// Authored by : tony9402
+// Co-authored by : -
+// Link : http://boj.kr/46d4186fe7394dfa8ffbbcb9045e6078
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int main(void)
-{
-    ios::sync_with_stdio(0);
+int main(){
+    ios::sync_with_stdio(false);
     cin.tie(0);
-    cout.tie(0);
 
-    int t;
-    cin >> t;
-    
+    int N; cin >> N;
+
     stack<int> st;
-    while(t--) {
-        string s;
-        int num;
-        cin >> s;
-        if(s == "push") {
-            cin >> num;            
-            st.push(num);
-        } else if(s == "top") {
-            if(st.empty()) {
-                cout << -1 << "\n";
-            } else {
-                cout << st.top() << "\n";
+
+    for(int i=0;i<N;i++){
+        string cmd; cin >> cmd;
+        if(cmd == "push") { 
+            int X; cin >> X;
+            st.push(X);
+        }
+        else if(cmd == "pop") {
+            if(st.empty()) { 
+                cout << -1 << '\n';
             }
-        } else if (s == "empty") {
-            if(st.empty()) {
-                cout << 1 << "\n";
-            } else {
-                cout << 0 << "\n";
-            }
-        } else if (s == "pop") {
-            if(st.empty()) {
-                cout << -1 << "\n";
-            } else {
-                cout << st.top() << "\n";
+            else {
+                cout << st.top() << '\n';
                 st.pop();
             }
-        } else if (s == "size") {
-            cout << st.size() << "\n";            
+        }
+        else if(cmd == "size") {
+            cout << (int)st.size() << '\n';
+        }
+        else if(cmd == "empty") {
+            cout << st.empty() << '\n';
+        }
+        else if(cmd == "top") {
+            if(st.empty()) {
+                cout << -1 << '\n';
+            }
+            else {
+                cout << st.top() << '\n';
+            }
         }
     }
 }
