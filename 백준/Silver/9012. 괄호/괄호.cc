2d7905dbf7ad712c5a3ca4e7f7d629ee33435aca
msung99 <88240193+msung99@users.noreply.github.com>
2024-01-03 09:46:32 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -1,42 +1,36 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <stack>
 using namespace std;
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	int t;
-	cin >> t;
+    int t;
+    cin >> t;
+    while(t--) {
+        string str;
+        cin >> str;
 
-	while (t--) {
-		string str;
-		cin >> str; 
-		stack<char> s;
-		bool isValid = true;
-
-		for (auto cur : str) {
-			if (cur == '(')  // 여는괄호 "(" 만 스택에 push 
-				s.push(cur);
-			
-			else if (cur == ')') {
-				if (s.empty()) { // 스택이 비어있다는 것은 곧, 현재 바라보고 있는 닫는괄호에 대한 여는괄호 쌍이 존재하지 않는다는것
-					isValid = false;
-					break;
-				}
-				else  
-					s.pop();
-			}
-		}
-
-		// 스택에 아직 여는괄호가 남아 있다는 것 == 적절한 닫는괄호 쌍을 못찾은 것
-		if (!s.empty())
-			isValid = false;
-
-		if (!isValid)
-			cout << "NO\n";
-		else
-			cout << "YES\n";
-	}
+        stack<char> st;
+        for(int i=0; i< str.size(); i++) {
+            if(str[i] == '(') {
+                st.push('(');
+            } else if(str[i] == ')') {
+                if(!st.empty() && st.top() == '(') {
+                    st.pop();
+                } else {
+                    st.push(')');
+                }
+            }
+        }
+        if(st.empty()) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+    }
 }
