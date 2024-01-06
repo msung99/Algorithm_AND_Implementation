99f0f775de2927f779eaf71310ba71c820410c67
msung99 <88240193+msung99@users.noreply.github.com>
2024-01-06 10:33:58 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2488 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,30 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int cnt;
+
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	int t;
-	int count = 0;
-	cin >> t;
+    int t;
+    cin >> t;
+    while(t--){
+        string str;
+        cin >> str;
 
-	while (t--) {
-		string str;
-		stack<char> s;
-		cin >> str;
-
-		for (auto mychar : str) {
-			if (s.empty() || mychar != s.top()) // 스택이 비어있는 경우를 감안할 것!
-				s.push(mychar);
-			else
-				s.pop();
-		}
-
-		// 모든 내용을 다 처리했음에도 아직 스택에 잔여물이 남아있다면 "좋은 단어" 가 아니다. 스택이 비어있어야 좋은단어임!
-		if (s.empty())
-			count++;
-	}
-	cout << count;
+        stack<char> s;
+        for(int i=0; i<str.size(); i++) {
+            if(s.empty()) {
+                s.push(str[i]);
+            } else if(s.top() == str[i]) {
+                s.pop();
+            } else {
+                s.push(str[i]);
+            }
+        }   
+        if(s.empty()) {
+            cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
 }
