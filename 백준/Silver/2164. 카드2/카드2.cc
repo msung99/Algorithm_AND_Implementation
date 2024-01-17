bb23f5e531b77ab498115a41e09719e2368e32e2
msung99 <88240193+msung99@users.noreply.github.com>
2024-01-18 08:15:47 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -1,27 +1,25 @@
-#include <iostream>
-#include <string>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(void)
 {
-	queue<int> q;
-	int top;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	int n;
-	
 	cin >> n;
-	// 1부터 n까지의 원소 큐에 삽입
-	for (int i = 1; i <= n; i++)
-	{
+
+	queue<int> q;
+	for (int i = 1; i <= n; i++) {
 		q.push(i);
 	}
 
-	while (q.size() > 1) // q.size() == 1일때, 즉 큐에 원소가 하나 남을때 중단
-	{
-		q.pop(); // 맨 앞 원소 삭제
-		top = q.front(); // 맨 앞 원소를 저장
-		q.pop(); // 그 다음 맨 앞 원소 삭제
-		q.push(top);
+	while (q.size() != 1) {
+		q.pop();
+		int data = q.front();
+		q.pop();
+		q.push(data);
 	}
-	cout << q.front(); // 최종적으로 큐에 하나 남은 원소를 출력
+	cout << q.front();
 }
