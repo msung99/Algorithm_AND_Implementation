b4bd48a85688f142ca90ace11ba0070cafe0ed84
msung99 <88240193+msung99@users.noreply.github.com>
2024-01-18 11:02:29 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -1,56 +1,34 @@
-#include <iostream>
-#include <string>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(void)
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	while (true) {
-		string str;
-		// cin >> str;   => cin 은 공백을 기준으로하여 하나의 문자열을 구성하므로, 공백이 기준이 아닌 공백포함 문자열 저장시 getline() 를 활용
-		getline(cin, str); // getline() : <string> 에 정의. string 형에 문자열을 저장시 사용
-		// 인자1 : 입력방식(파일입력, 표준입력 등등), 인자2 : string형 변수명
-		if (str == ".")
-			break;
-
-		stack<char> s;
-		bool isValid = true;
-
-		for (auto mychar : str) {
-			//cout << "mychar:" << mychar << '\n';
-
-			if (mychar == '(' || mychar == '[')
-				s.push(mychar);
-
-			else if (mychar == ')') {
-				if (s.empty() || s.top() != '(' ) { // 닫는 괄호를 만났는데, 부적합한 괄호쌍이거나 스택이 비어있는 경우
-					isValid = false;
-					break;
-				}
-				s.pop();
-			}
-
-			else if (mychar == ']') {
-				if (s.empty() || s.top() != '[') {
-					isValid = false;
-					break;
-				}
-				s.pop();
-			}
-		}
-
-		if (!s.empty())
-			isValid = false;
-
-		if (isValid)
-			cout << "yes\n";
-		else
-			cout << "no\n";
-	}
-
-	return 0;
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  while(true){
+    string a;
+    getline(cin, a);
+    if(a == ".") break;
+    stack<char> s;
+    bool isValid = true;
+    for(auto c : a){
+      if(c == '(' || c == '[') s.push(c);
+      else if(c == ')'){
+        if(s.empty() || s.top() != '('){
+          isValid = false;
+          break;
+        }
+        s.pop();
+      }
+      else if(c == ']'){
+        if(s.empty() || s.top() != '['){
+          isValid = false;
+          break;
+        }
+        s.pop();
+      }
+    }
+    if(!s.empty()) isValid = false;
+    if(isValid) cout << "yes\n";
+    else cout << "no\n";
+  }
 }
-
