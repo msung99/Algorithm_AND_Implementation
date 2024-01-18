29808472087e88f3154eecb90d2c03ecb0cfbc5a
msung99 <88240193+msung99@users.noreply.github.com>
2024-01-18 09:04:08 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <deque>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(void)
@@ -8,50 +7,45 @@ int main(void)
 	cin.tie(0);
 	cout.tie(0);
 
-	deque<int> dq;
-	int n, m;
-	int index = 0;
-	int count = 0;
+	int n, m; // n : 큐의 크기, m : 뽑아내려고 하는 원소 개수
 	cin >> n >> m;
 
+	deque<int> dq;
 	for (int i = 1; i <= n; i++) {
 		dq.push_back(i);
 	}
 
+	int count = 0;
+
 	while (m--) {
 		int data;
 		cin >> data;
-		
-		// 탐색하려는 숫자의 인덱스 저장
-		for (int i = 0; i < dq.size(); i++) {
-			if (dq[i] == data) {
-				index = i;
-				break;
-			}
-		}
 
-		// 왼쪽으로 가는게 더 횟수가 적은경우
-		if (index < dq.size() - index) {
-			while (dq.front() != data) {  // 계속 왼쪽으로 한칸씩 이동시키다가 맨 앞 원소가 원하던 숫자임을 찾아낼때 까지 반복
-				int front_data = dq.front();
-				dq.push_back(front_data);
+		int idx = find(dq.begin(), dq.end(), data) - dq.begin();
+		if (idx < (dq.size() / 2) + 1 ) { // 원소가 앞단에 있다면
+			while (true) {
+				if (dq.front() == data) {
+					dq.pop_front();
+					break;
+				} 
+				int front = dq.front();
 				dq.pop_front();
+				dq.push_back(front);
 				count++;
 			}
-			dq.pop_front();
 		}
-
-		// 오른쪽으로 가는게 더 횟수가 적은경우
-		else if (index >= dq.size() - index) {
-			while (dq.front() != data) {  // 오른쪽으로 한칸씩 계속 이동시키다가 원하던 숫자를 찾아낼 때 까지 반복
-				int back_data = dq.back();
-				dq.push_front(back_data);
+		else { // 원소가 뒷단에 있다면
+			while (true) {
+				if (dq.front() == data) {
+					dq.pop_front();
+					break;
+				}
+				int back = dq.back();
 				dq.pop_back();
+				dq.push_front(back);
 				count++;
 			}
-			dq.pop_front();
 		}
 	}
-
 	cout << count;
 }
