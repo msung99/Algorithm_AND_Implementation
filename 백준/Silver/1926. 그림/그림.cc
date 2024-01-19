4af45ceda6fcb642dfeea54939c09723516d2779
msung99 <88240193+msung99@users.noreply.github.com>
2024-01-19 09:38:39 +0900

    [Silver I] Title: 그림, Time: 32 ms, Memory: 3248 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,14 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m;
 int board[501][501];
-bool vistied[501][501];
-int dx[4] = { -1,0,1,0 };
-int dy[4] = { 0,1,0,-1 };
-int n, m; 
+bool visited[501][501];
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { -1,0,1,0 };
 
-// 1. 상하좌우로 연결된 그림의 크기를 찾아내기
-// 2. 도화지에 있는 모든 그림을 찾아내기
+int bfs(int curX, int curY) {
+	queue<pair<int,int>> q;
+	q.push({curX, curY});
+
+	int area = 0;
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+
+		visited[x][y] = true; 
+		area++;
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+				continue;
+			}
+
+			if (!visited[nx][ny] && board[nx][ny] == 1) {
+				q.push({nx, ny});
+				visited[nx][ny] = true;
+			}
+		}
+	}
+	return area;
+}
 
 int main(void)
 {
@@ -16,55 +43,28 @@ int main(void)
 	cin.tie(0);
 	cout.tie(0);
 
+
 	cin >> n >> m;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> board[i][j];
+			int data;
+			cin >> data;
+			board[i][j] = data;
 		}
 	}
 
-	int count = 0;
-	int max_area = 0;
-	int area_count = 0;
-
-	for (int i = 0; i < n; i++) 
-	{
-		for (int j = 0; j < m; j++) 
-		{
-			if (board[i][j] == 0 || vistied[i][j])
-				continue;
-
-			area_count++;
-			queue<pair<int, int>> q;
-			q.push({ i,j });
-			vistied[i][j] = true;
-			while (!q.empty()) 
-			{
-				pair<int, int> cur = q.front();
-				q.pop();
-
-				count++;
-				
-				// 4방향으로 뻗음
-				for (int k = 0; k < 4; k++) 
-				{
-					int nx = cur.first + dx[k];
-					int ny = cur.second + dy[k];
-
-					if (nx < 0 || nx >= n || ny < 0 || ny >= m)
-						continue;
-
-					if (vistied[nx][ny] || board[nx][ny] == 0)
-						continue;
-
-					q.push({ nx,ny });
-					vistied[nx][ny] = true;
-				}
+	int maxVal = 0;
+	int areas = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (!visited[i][j] && board[i][j] == 1) {
+				maxVal = max(maxVal, bfs(i, j));
+				areas++;
 			}
-			max_area = max(max_area, count);
-			count = 0;
 		}
 	}
-	cout << area_count << '\n' << max_area;
+
+	cout << areas << "\n" << maxVal;
+	return 0;
 }
