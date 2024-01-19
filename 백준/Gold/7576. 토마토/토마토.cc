813f797922e75273703c62d4ce04b19df1e076c6
msung99 <88240193+msung99@users.noreply.github.com>
2024-01-19 11:00:20 +0900

    [Gold V] Title: 토마토, Time: 80 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,46 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define X first
-#define Y second
-int board[1002][1002];
-int dist[1002][1002];
-int n,m;
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> m >> n;
-  queue<pair<int,int> > Q;
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      cin >> board[i][j];
-      if(board[i][j] == 1)
-        Q.push({i,j});
-      if(board[i][j] == 0)
-        dist[i][j] = -1;
-    }
-  }
-  while(!Q.empty()){
-    auto cur = Q.front(); Q.pop();
-    for(int dir = 0; dir < 4; dir++){
-      int nx = cur.X + dx[dir];
-      int ny = cur.Y + dy[dir];
-      if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-      if(dist[nx][ny] >= 0) continue;
-      dist[nx][ny] = dist[cur.X][cur.Y]+1;
-      Q.push({nx,ny});
-    }
-  }
-  int ans = 0;
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){
-      if(dist[i][j] == -1){
-        cout << -1;
-        return 0;
-      }
-      ans = max(ans, dist[i][j]);
-    }
-  }
-  cout << ans;
+
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { -1,0,1,0 };
+int board[1001][1001];
+int dist[1001][1001];
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int m, n;
+	cin >> m >> n;
+	queue<pair<int, int>> q;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> board[i][j];
+			if (board[i][j] == 1) {
+				q.push({i,j});
+			}
+			if (board[i][j] == 0) {
+				dist[i][j] = -1;
+			}
+		}
+	}
+
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+				continue;
+			}
+			if (dist[nx][ny] >= 0) {
+				continue;
+			}
+			dist[nx][ny] = dist[x][y] + 1;
+			q.push({ nx,ny });
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (dist[i][j] == -1) {
+				cout << -1;
+				return 0;
+			}
+			ans = max(ans, dist[i][j]);
+		}
+	}
+	cout << ans;
+	return 0;
 }
