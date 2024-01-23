c36497ef1c91603fc3b560b0c614ad612fad0f6d
msung99 <88240193+msung99@users.noreply.github.com>
2024-01-23 10:30:47 +0900

    [Silver I] Title: 나이트의 이동, Time: 44 ms, Memory: 2508 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.cc
@@ -1,51 +1,55 @@
-#include <iostream>
-#include <string>
-#include <utility>
-#include <queue>
-#include <tuple>
+#include <bits/stdc++.h>
 using namespace std;
 
-int dist[300][300];
-
-int dx[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
-int dy[8] = {1, 2, 2, 1, -1, -2 ,-2 ,-1};
+int dist[302][302];
+int dx[8] = { -2,-1,1,2,2,1,-1,-2 };
+int dy[8] = { 1,2,2,1,-1,-2,-2,-1 };
 
 int main(void)
 {
-	int testCase;
-	cin >> testCase;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-	while (testCase--)
-	{
-		int length;
-		cin >> length;
-		for (int i = 0; i < length; i++) // dist 데이터 값을 -1로 다 초기화
-			fill(dist[i], dist[i] + length,  -1);
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		int s1, s2, e1, e2;
+		cin >> s1 >> s2;
+		cin >> e1 >> e2;
+		pair<int, int> start = make_pair(s1, s2);
+		pair<int, int> end = make_pair(e1, e2);
 
-		int start_x, start_y, end_x, end_y;
-		cin >> start_x >> start_y; // 나이트의 처음 좌표
-		cin >> end_x >> end_y; // 나이트가 도달하고자 하는 최종 이동 좌표 
+		for (int i = 0; i < n; i++) {
+			fill(dist[i], dist[i] + n, -1);
+		}
 
 		queue<pair<int, int>> q;
-		q.push({ start_x,start_y });
-		dist[start_x][start_y] = 0;
+		q.push({ s1,s2 });
+		dist[s1][s2] = 0;
 
-		while (!q.empty())
-		{
-			auto cur = q.front();
+		while (!q.empty()) {
+			int x = q.front().first;
+			int y = q.front().second;
 			q.pop();
 
 			for (int i = 0; i < 8; i++) {
-				int x = cur.first + dx[i];
-				int y = cur.second + dy[i];
-				if (x < 0 || x >= length || y < 0 || y >= length)
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+
+				if (nx < 0 || nx >= n || ny < 0 || ny >= n)
 					continue;
-				if (dist[x][y] != -1) // 이미 거리계산 완료한 좌표 칸인 경우
+
+				if (dist[nx][ny] != -1)
 					continue;
-				dist[x][y] = dist[cur.first][cur.second] + 1;
-				q.push({ x,y });
+
+				dist[nx][ny] = dist[x][y] + 1;
+				q.push({ nx,ny });
 			}
 		}
-		cout << dist[end_x][end_y] << '\n';
+		cout << dist[end.first][end.second] << "\n";
 	}
+	return 0;
 }
