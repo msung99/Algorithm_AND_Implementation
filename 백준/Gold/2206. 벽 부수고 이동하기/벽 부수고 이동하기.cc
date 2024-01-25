bc8654efd042fbfd9a183744cf68edc6cc1239cd
msung99 <88240193+msung99@users.noreply.github.com>
2024-01-25 10:41:49 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 68 ms, Memory: 12680 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,49 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dx[4] = { 0,1,0,-1 };
-int dy[4] = { 1,0,-1,0 };
-
-char board[1000][1000];
-int dist[1000][1000][2];
-// dist[x][y][0] : 벽을 하나도 부수지 않고 (x,y)까지 오는데 걸리는 비용
-// dist[x][y][1] : 벽을 하나만 부수고 (x,y) 까지 오는데 걸리는 비용. 이때 (x,y) 가 벽이라서 부수는 경우 포함
 int n, m;
-
-bool oob(int x, int y) {
-	return x < 0 || x >= n || y < 0 || y >= m;
-}
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { -1,0,1,0 };
+char board[1001][1001];
+int dist[1001][1001][2];
 
 int bfs() {
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			dist[i][j][0] = dist[i][j][1] = -1;  // 초기값 모두 -1로 초기화
-	
-	dist[0][0][0] = dist[0][0][1] = 1; // (0,0) 까지 오는데 걸리는 비용은 1
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			dist[i][j][0] = -1;
+			dist[i][j][1] = -1;
+		}
+	}
+	dist[0][0][0] = 1;
+	dist[0][0][1] = 1;
+
 	queue<tuple<int, int, int>> q;
-	q.push({ 0,0,0 });  // (0,0) 좌표에서부터 출발
+	q.push({0,0,0});
 
 	while (!q.empty()) {
 		int x, y, broken;
 		tie(x, y, broken) = q.front();
+		q.pop();
+
 		if (x == n - 1 && y == m - 1)
 			return dist[x][y][broken];
-		q.pop();
-		int nextdist = dist[x][y][broken] + 1;
-		for (int dir = 0; dir < 4; dir++) {
-			int nx = x + dx[dir];
-			int ny = y + dy[dir];
-			if (oob(nx, ny))
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m)
 				continue;
-			// (nx, ny) 좌표에 벽이 존재하지 않고, (당연히도) 벽을 부수는 행위도 하지 않는경우
+
 			if (board[nx][ny] == '0' && dist[nx][ny][broken] == -1) {
-				dist[nx][ny][broken] = nextdist;
+				dist[nx][ny][broken] = dist[x][y][broken] + 1;
 				q.push({ nx, ny, broken });
 			}
-			// (nx, ny) 좌표에 있는 벽을 부수는 경우
+			
 			if (!broken && board[nx][ny] == '1' && dist[nx][ny][1] == -1) {
-				dist[nx][ny][1] = nextdist;
-				q.push({ nx, ny, 1 });
+				dist[nx][ny][1] = dist[x][y][broken] + 1;
+				q.push({ nx,ny,1 });
 			}
 		}
 	}
@@ -54,12 +53,12 @@ int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	cout.tie(0);
 
 	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < m; ++j)
 			cin >> board[i][j];
-		}
-	}
 	cout << bfs();
+	return 0;
 }
