3be22cda4215943841eb44ee6e515984650fb10d
msung99 <88240193+msung99@users.noreply.github.com>
2024-02-03 11:15:56 +0900

    [Silver II] Title: 종이의 개수, Time: 352 ms, Memory: 20928 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,48 +1,53 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 int n;
+int cnt[3];
 int board[2200][2200];
-int ans[3];
 
-bool check(int x, int y, int n)
-{
-	int first_element = board[x][y];
-	for (int i = x; i < x + n; i++) 
-		for (int j = y; j < y + n; j++) 
-			if (first_element != board[i][j])
+bool check(int x, int y, int size) {
+	for (int i = x; i < x + size; i++) {
+		for (int j = y; j < y + size; j++) {
+			if (board[x][y] != board[i][j]) {
 				return false;
+			}
+		}
+	}
 	return true;
 }
 
-void func(int x, int y, int n)
-{
-
-	if (check(x, y, n))
-	{
-		ans[board[x][y] + 1] += 1; // 카운팅
+void func(int x, int y, int size) {
+	if (check(x, y, size)) {
+		cnt[board[x][y] + 1]++;
 		return;
 	}
 
-	int size = n / 3;
-	for (int i = 0; i < 3; i++)
-		for (int j = 0; j < 3; j++)
-			func(x + size * i, y + size * j, size);
+	int divideSize = size / 3;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			func(x + i*divideSize, y + j*divideSize, divideSize);
+		}
+	}
 }
 
 int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+	cout.tie(0);
 
 	cin >> n;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++) 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
 			cin >> board[i][j];
+		}
+	}
 
-	func(0,0,n);
+	func(0, 0, n);
 
-	for (int i = 0; i < 3; i++)
-		cout << ans[i] << '\n';
+	for (int i = 0; i < 3; i++) {
+		cout << cnt[i] << "\n";
+	}
+
+	return 0;
 }
