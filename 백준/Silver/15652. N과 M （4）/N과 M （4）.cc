86bac7de07107d6e8943120f177eb1244ad496e6
msung99 <88240193+msung99@users.noreply.github.com>
2024-02-08 14:14:51 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -1,34 +1,39 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int num_list[10];
-bool visited[10];
+int arr[8];
 int n, m;
 
-void func(int k)
-{
-	if (k == m)
-	{
-		for (int i = 0; i < m; i++)
-			cout << num_list[i] << ' ';
-		cout << '\n';
+// 현재 k개까지 수를 택했다.
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << "\n";
 		return;
+	}	
+
+	int start = 1; // 첫번째로 선택할땐 1을 선택
+
+	if (k != 0) { // 그 외의 선택에선 지난번에 선택한 숫자(k-1번째 숫자) 보다 더 큰 숫자를 선택
+		start = arr[k - 1];
 	}
 
-	int start = 1;
-	if (k != 0)
-		start = num_list[k-1];
-
-	for (int i = start; i <= n; i++)
-	{
-		num_list[k] = i;
+	for (int i = start; i <= n; i++) {
+		arr[k] = i;
 		func(k + 1);
 	}
 }
 
-
 int main(void)
 {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	cin >> n >> m;
 	func(0);
+
+	return 0;
 }
