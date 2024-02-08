a7e0fcf6fe2a0b57baba44c79055b24082c48d83
msung99 <88240193+msung99@users.noreply.github.com>
2024-02-08 13:50:23 +0900

    [Silver III] Title: N과 M (3), Time: 368 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -1,30 +1,33 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int num_list[10];
-bool visited[10];
+int arr[8];
 int n, m;
 
-void func(int k)
-{
-	if (k == m)
-	{
-		for (int i = 0; i < m; i++)
-			cout << num_list[i] << ' ';
-		cout << '\n';
+// 현재 k개까지 수를 택했다.
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << "\n";
 		return;
 	}
 
-	for (int i = 1; i <= n; i++)
-	{
-		num_list[k] = i;
+	for (int i = 1; i <= n; i++) {
+		arr[k] = i;
 		func(k + 1);
 	}
 }
 
-
 int main(void)
 {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	cin >> n >> m;
 	func(0);
+
+	return 0;
 }
