323a7a1d6add365a21679b38b1ff5a6156d67031
msung99 <88240193+msung99@users.noreply.github.com>
2024-02-12 11:39:55 +0900

    [Silver III] Title: N과 M (7), Time: 480 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -1,41 +1,38 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int origin_list[10];
-int new_list[10];
 int n, m;
+int num[10];
+int arr[10];
+bool isused[10];
 
+void func(int k) { // 현재 k개까지 수를 택했음.
+    if (k == m) { // m개를 모두 택했으면
+        for (int i = 0; i < m; i++)
+            cout << num[arr[i]] << ' '; // arr에 기록해둔 인덱스에 대응되는 수를 출력
+        cout << '\n';
+        return;
+    }
 
-// 이전에 풀었던 N과 M(4) 문제에서 살짝만 변형시키면 된다.
-// new_list 가 0 ~ N 사이의 숫자를 가지고 조합을 만들게 하고, 그 조합의 숫자들은 origin_list 의 인덱스 번호를 나타내주면 된디.
-
-void func(int k)
-{
-	if (k == m)
-	{
-		for (int i = 0; i < m; i++)
-			cout << origin_list[new_list[i]] << ' '; // new_list 에 기록해둔 인덱스에 대응되는 수를 출력
-		cout << '\n';
-		return;
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		new_list[k] = i;
-		func(k + 1);
-	}
+    for (int i = 0; i < n; i++) {
+            arr[k] = i; // k번째 인덱스를 i로 정함
+            func(k + 1); // 다음 인덱스를 정하러 한 단계 더 들어감        }
+    }
 }
-
 
 int main(void)
 {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	cin >> n >> m;
 
-	for (int i = 0; i < n; i++)
-		cin >> origin_list[i];
-
-	sort(origin_list, origin_list + n);  // 오름차순 정렬
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+	sort(num, num + n);
 
 	func(0);
+
 }
