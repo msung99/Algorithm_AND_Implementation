49fedc65ad485e26e1c474d885c6d6c11f628fcb
msung99 <88240193+msung99@users.noreply.github.com>
2024-02-12 11:32:36 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -1,53 +1,47 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int origin_list[10];
-int new_list[10];
-bool visited[10];
 int n, m;
+int num[10];
+int arr[10];
+bool isused[10];
 
-
-// 이전에 풀었던 N과 M(2) - 조합 구하기 문제에서 살짝만 변형시키면 된다.
-// new_list 가 0 ~ N 사이의 숫자를 가지고 조합을 만들게 하고, 그 조합의 숫자들은 origin_list 의 인덱스 번호를 나타내주면 된디.
-
-void func(int k)
-{
-	if (k == m)
-	{
-		for (int i = 0; i < m; i++)
-			cout << origin_list[new_list[i]] << ' '; // new_list 에 기록해둔 인덱스에 대응되는 수를 출력
-		cout << '\n';
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << num[arr[i]] << ' ';
+		}
+		cout << "\n";
+		return;
 	}
 
 	int start = 0;
-	if (k != 0)
-		start = new_list[k - 1] + 1;
+	if (k != 0) {
+		start = arr[k - 1] + 1;
+	}
 
-	// num_list 는 0~N 사이의 숫자로 구성된 조합 부분순열이 저장됨. 
-	// 예를들어 (0,1), (0,2), (0,3), (1,2), (1,3), (2,3) 이런 6개의 부분수열에 num_list이 저장된다.
-	// num_list 의 원소들은 new_list 의 인덱스 번호로 활용된다. 
-	for (int i = start; i < n; i++)
-	{
-		if (!visited[i])
-		{
-			new_list[k] = i;
-			visited[i] = true;
+	for (int i = start; i < n; i++) {
+		if (!isused[i]) {
+			arr[k] = i;
+			isused[k] = true;
 			func(k + 1);
-			visited[i] = false;
+			isused[k] = false;
 		}
 	}
 }
 
-
 int main(void)
 {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	cin >> n >> m;
 
-	for (int i = 0; i < n; i++)
-		cin >> origin_list[i];
-
-	sort(origin_list, origin_list + n);  // 오름차순 정렬
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+	sort(num, num + n);
 
 	func(0);
 }
