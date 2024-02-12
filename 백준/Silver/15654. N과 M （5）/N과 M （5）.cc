f1e964eda379ce6f52a04af0f6c50c8c90ecb209
msung99 <88240193+msung99@users.noreply.github.com>
2024-02-12 10:56:27 +0900

    [Silver III] Title: N과 M (5), Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -1,43 +1,47 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int num_list[10];
-int new_list[10];
-bool visited[10];
 int n, m;
+int arr[10];
+int num[10];
+bool isused[10]; // i번째 숫자 사용 유무 
 
-void func(int k)
-{
-	if (k == m)
-	{
-		for (int i = 0; i < m; i++)
-			cout << new_list[i] << ' ';
-		cout << '\n';
+// k 번째 숫자와, 다음번에 뽑을 k+1 번째 숫자는 서로 다른 숫자여야 한다.
+
+void func(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++) {
+			cout << num[arr[i]] << ' ';
+		}
+		cout << "\n";
 		return;
 	}
-
-	for (int i = 0; i < n; i++)
-	{
-		if (!visited[i]) 
-		{
-			new_list[k] = num_list[i];
-			visited[i] = true;
+	
+	for (int i = 0; i < n; i++) {
+		if (!isused[i]) {
+			arr[k] = i; // k번째 숫자로 인덱스 i에 해당하는 숫자가 사용되었다고 표기함
+			isused[i] = true;  // 인덱스 i에 해당하는 숫자가 사용되었다고 표기
 			func(k + 1);
-			visited[i] = false;
+			isused[i] = false;
 		}
 	}
 }
 
-
 int main(void)
 {
-	cin >> n >> m;
-	
-	for (int i = 0; i < n; i++)
-		cin >> num_list[i];
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-	sort(num_list, num_list + n); // 오름차순 정렬
-	
+	cin >> n >> m; // 4, 2
+
+	for (int i = 0; i < n; i++) {
+		cin >> num[i];
+	}
+
+	sort(num, num + n);
+
 	func(0);
+
+	return 0;
 }
