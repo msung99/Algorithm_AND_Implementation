be63624753f82e22c8fef662b7d3f8b6c8ef7a94
Haon <88240193+msung99@users.noreply.github.com>
2024-03-15 12:59:18 +0900

    [Silver I] Title: 경로 찾기, Time: 12 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.cc
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -1,48 +1,62 @@
 #include <bits/stdc++.h>
-/*
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <stack>
-*/
 using namespace std;
 
-vector<int> adj_list[100];
-int visited[100];
+int n;
+int arr[105][105];
+bool visited[105];
 
-// DFS 탐색이 끝나고 나면, visited에 정점 x에 대해 방문처리된 결과가 저장된다.
-void DFS(int x) {
-	// 정점 x의 인접 리스트에 담겨있는 정점들에 대한 방문 처리 
-	for (int i = 0; i < adj_list[x].size(); i++) { 
-		if (!visited[adj_list[x][i]]) 
-		{
-			visited[adj_list[x][i]] = 1;
-			DFS(adj_list[x][i]); // 재귀 호출을 해서 계속 한 방향으로 깊게 쭉 뻗어나가면서 방문 처리 시도
-		}
-	}
+bool dfs(int start, int end) {
+  fill(visited, visited + n + 2, false);
+  stack<int> s;
+  s.push(start);
+
+  while(!s.empty()) {
+    int cur = s.top();
+    s.pop();
+
+    if(visited[cur]) {
+      continue;
+    }
+
+    if(cur != start) {
+      visited[cur] = 1;
+    }
+
+    for(int next=0; next<n; next++) {
+      if(visited[next]) {
+        continue;
+      }
+      if(arr[cur][next]) {
+        if(next == end) {
+          return true;
+        }
+        s.push(next);
+      }
+    }
+  }
+  return false;
 }
 
 int main(void)
 {
-	int n, vertex;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> vertex;
-			if (vertex == 1) // 각 정점 i에 대해 연결된 정점 j를 정점 i의 인접 리스트에 push
-				adj_list[i].push_back(j);
-		}
-	}
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	// 각 정점들에 대해 실행 
-	for (int i = 0; i < n; i++) {
-		fill(visited, visited + n, 0); // 다음 정점에 대한 DFS 호출을 해서 방문 정보를 visited 에 담기위해 0으로 초기화
-		DFS(i); // DFS 탐색으로 정점 i에 대해 visited 에다 방문처리 정보를 저장한다,
+  cin >> n;
 
-		// 정점 i에 대한 visited 에 방문처리가 완료된 정보를 출력 
-		for (int j = 0; j < n; j++) {  
-			cout << visited[j] << ' '; 
-		}
-		cout << '\n';
-	}
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<n; j++) {
+      cin >> arr[i][j];
+    }
+  }
+
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<n; j++) {
+      cout << dfs(i, j) << " ";
+    }
+    cout << "\n";
+  }
+
+  return 0;
 }
