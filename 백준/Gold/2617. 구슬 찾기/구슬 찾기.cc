7e3d7bb5a6d5821694fd810ca016a5359e210096
Haon <88240193+msung99@users.noreply.github.com>
2024-03-18 14:34:58 +0900

    [Gold IV] Title: 구슬 찾기, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.cc
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.cc
@@ -1,99 +1,62 @@
-#include <iostream>
-#include <stack>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-/*
-정방향, 역방향 DFS를 각각 진행했을때 자신의 순서가 중간순서 번호보다 크다면 중간 순서가 될 수 없다.
-=> 무게 정보를 보면 간선이 한 방향이다. 이렇게 주어졌기 떄문에 나보다 가벼운 것만 알 수 있다.
-   따라서 인접 리스트를 하나 더 만들고 간선을 거꾸로 넣어준다. (결과적으로 DFS를 정방향, 역방향으로 2번 탐색함)
-*/
 
-vector<int> adj_list[100]; // 정방향 인접리스트 
-vector<int> reverse_adj_list[100]; // 역방향 인접리스트
-bool visited[100]; // 정방향 인접리스트의 DFS 방문여부 체크
-bool r_visited[100]; // 역방향 인접리스트의 DFS 방문여부 체크
-int n, m, u, v;
-int cnt, rev_cnt; // 정방향, 역방향 순회할때 카운팅 변수
-int result, mid;
+int n, m;
+bool visited[101];
+vector<int> heavy[101]; // heavy[i] : i보다 무거운 구슬 리스트
+vector<int> light[101];
 
+bool solve(int start, vector<int> (&adj_list)[]) {
+  fill(visited, visited + n + 1, false);
 
+  queue<int> q;
+  q.push(start);
+  visited[start] = true;
 
-void DFS(int node)
-{
-	stack<int> s;
-	s.push(node);
-	visited[node] = true;
-	while (!s.empty())
-	{
-		int cur = s.top();
-		s.pop();
-		for (auto nxt : adj_list[cur])
-		{
-			if (visited[nxt])
-				continue;
+  int mid = (n+1) / 2;
+  int cnt = 0;
 
-			s.push(nxt);
-			visited[nxt] = true;
-			cnt++;
-		}
-	}
+  while(!q.empty()) {
+    int cur = q.front();
+    q.pop();
+
+    for(int next : adj_list[cur]) {
+      if(visited[next]) {
+        continue;
+      }
+      cnt++;
+      q.push(next);
+      visited[next] = true;
+    }
+  }
+
+  if(cnt >= mid) {
+    return true;
+  } 
+  return false;
 }
-
-
-void rDFS(int node)
-{
-	stack<int> s;
-	s.push(node);
-	r_visited[node] = true;
-	while (!s.empty())
-	{
-		int cur = s.top();
-		s.pop();
-		for (auto nxt : reverse_adj_list[cur])
-		{
-			if (r_visited[nxt])
-				continue;
-
-			s.push(nxt);
-			r_visited[nxt] = true;
-			rev_cnt++;
-		}
-	}
-}
-
 
 int main(void)
 {
-	cin >> n >> m;
-	mid = (n + 1) / 2; // 무게 중간순서 계산
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	// 정방향,역방향 인접 리스트에 정보 입력
-	for (int i = 0; i < m; i++)
-	{
-		cin >> u >> v;
-		adj_list[u].push_back(v);
-		reverse_adj_list[v].push_back(u);
-	}
+  cin >> n >> m;
 
-	// DFS 탐색
-	for (int i = 1; i <= n; i++)
-	{
-		// DFS 수행전에 방문 기록 초기화
-		cnt = 0;
-		rev_cnt = 0;
-		for (int i = 1; i <= n; i++)
-		{
-			visited[i] = false;
-			r_visited[i] = false;
-		}
-		
-		// 각 정점마다 DFS 수행
-		DFS(i);
-		rDFS(i);
+  for(int i=0; i<m; i++) {
+    int u, v;
+    cin >> u >> v;
+    heavy[u].push_back(v);
+    light[v].push_back(u);
+  }
 
-		// 정방향, 역방향 순회 결과
-		if (cnt >= mid || rev_cnt >= mid)
-			result++;
-	}
-	cout << result;
+  int ans = 0;
+  for(int i=1; i<=n; i++) {
+    int start = i;
+    ans += (solve(start, heavy) || solve(start, light));
+  }
+
+  cout << ans;
+  return 0;
 }
