016131f80b7242ee9423dc18b84dd608e7795219
Haon <88240193+msung99@users.noreply.github.com>
2024-03-19 12:45:30 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -2,23 +2,28 @@
 using namespace std;
 
 int n, m;
-int a[100004], d[100004];
+long long arr[100002];
+long long total[100002];
+long long d[100002];
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n >> m;
-	d[0] = 0;
-	for (int i = 1; i <= n; i++) {
-		cin >> a[i];
-		d[i] = d[i - 1] + a[i]; // d[i] : 1 ~ i번쨰 사이의 수의 합
-	}
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	// i ~ j 번째 수의 합 = (1~j 번째 사이의 수의 합) - (1 ~ i-1 번쨰 사이의 수의 합)
-	while (m--) {
-		int start, end;
-		cin >> start >> end;
-		cout << d[end] - d[start - 1] << '\n';
-	}
+  cin >> n >> m;
+  for(int i=1; i<=n; i++) {
+    cin >> arr[i];
+    total[i] = total[i-1] + arr[i];
+  }
+
+  while(m--) {
+    int start, end;
+    cin >> start >> end;
+
+    cout << total[end] - total[start-1] << "\n";
+  }
+
+  return 0;
 }
