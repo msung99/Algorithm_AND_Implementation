731766cd6ee3685a1c7014189141caf56d9f0de4
Haon <88240193+msung99@users.noreply.github.com>
2024-03-19 21:21:47 +0900

    [Silver I] Title: 정수 삼각형, Time: 16 ms, Memory: 3988 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,28 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int s[505][505];
-int d[505][505];
+int arr[502][502];
+int d[502][502];
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	cin >> n;
-	for (int i = 1; i <= n; i++) 
-		for (int j = 1; j <= i; j++) 
-			cin >> s[i][j];
+  int n;
+  cin >> n;
+  for(int i=1; i<=n; i++) {
+    for(int j=1; j<=i; j++) {
+      cin >> arr[i][j];
+    }
+  }
 
-	d[1][1] = s[1][1];
-	d[2][1] = d[1][1] + s[2][1];
-	d[2][2] = d[1][1] + s[2][2];
+  d[1][1] = arr[1][1];
 
-	for (int i = 3; i <= n; i++) {
-		for (int j = 1; j <= i; j++) {
-			d[i][j] = max(d[i-1][j-1], d[i-1][j]) + s[i][j];
-		}
-	}
-	cout << *max_element(d[n], d[n] + n + 1);
+  for(int i=2; i<=n; i++) {
+    for(int j=1; j<=i; j++) {
+      d[i][j] = max(d[i-1][j-1], d[i-1][j]) + arr[i][j];
+    }
+  }
+
+  cout << *max_element(d[n] + 1, d[n] + n + 1);
+
+  return 0;
 }
