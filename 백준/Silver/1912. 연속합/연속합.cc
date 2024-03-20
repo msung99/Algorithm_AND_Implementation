1fc0c8de37fb30f9ba8017d20df49b268ebec662
Haon <88240193+msung99@users.noreply.github.com>
2024-03-20 09:47:18 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,27 +1,27 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 int n;
-int s[100005];
-int d[100005];
+int arr[100002]; // 입력값
+int d[100002]; // d[i] : i번째 항으로 끝나는 연속합 중에 최댓값
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n;
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	// 테이블 채우기
-	for (int i = 1; i <= n; i++) {
-		cin >> s[i];
-		d[i] = s[i]; 
-	}
+  cin >> n;
 
-	for (int i = 1; i <= n; i++)
-		d[i] = max(d[i], d[i - 1] + s[i]);
+  for(int i=1; i<=n; i++) {
+    cin >> arr[i];
+  }
 
-	cout << *max_element(d + 1, d + n + 1);
+  for(int i=1; i<=n; i++) {
+    d[i] = max(0, d[i-1]) + arr[i];
+    // max(0, d[i-1]) => d[i-1] 이 양수라면 이득이므로 더한다. 반대로 음수이므로 더하지 않고 그대로 냅둔다. (= 0을 더한다)
+  }
+
+  cout << *max_element(d + 1, d + n + 1);
+  return 0;
 }
