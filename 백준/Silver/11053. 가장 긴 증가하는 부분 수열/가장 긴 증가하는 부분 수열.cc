1488d23d5f5eead379c831cce71687573699cef0
Haon <88240193+msung99@users.noreply.github.com>
2024-03-20 16:57:49 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -2,28 +2,31 @@
 using namespace std;
 
 int n;
-int s[1005];
-int d[1005];
+int arr[1002];
+int d[1002];
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	cin >> n;
+  cin >> n;
 
-	fill(d, d + n + 1, 1);
+  for(int i=1; i<=n; i++) {
+    cin >> arr[i];
+    d[i] = 1;
+  }
 
-	for (int i = 1; i <= n; i++)
-		cin >> s[i];
+  for(int i=1; i<=n; i++) {
+    for(int j=1; j<i; j++) { // 현재 i보다 작은 인덱스들, 즉 i번쨰 이전의 부분 합들 중에서 최대인 것을 추출
+      if(arr[i] > arr[j]) {
+        d[i] = max(d[i], d[j] + 1);
+      }
+    }
+  }
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= i; j++) {
-			if (s[i] > s[j]) {
-				d[i] = max(d[i], d[j] + 1);
-			}
-		}
-	}
+  cout << *max_element(d + 1, d + n + 1);
 
-	cout << *max_element(d, d + n + 1);
+  return 0;
 }
