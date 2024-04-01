ca205366652e1a9ea1fff472bd90ed8f68dea1ea
Haon <88240193+msung99@users.noreply.github.com>
2024-04-01 21:09:11 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,64 +1,56 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-// - 각 케이스마다 존재하는 치킨집 중에 폐업되는 치킨집 2의 상태를 0으로 바꾼다.
-// - 잠시 생각해보면, 치킨집이 늘어나면 늘어날수록 치킨거리가 당연히 줄어든다. 따라서 바로 M개를 고르는 경우만 생각하면 된다. 
-// ( 치킨집을 1개, 2개, 3개, .. M-1 개 고르는 경우는 고려하지 말것! )
-
-int board[51][51]; // 원본 도시
- // xxxxx 각 케이스마다 폐업되는 치킨집 상태가 최신화된 도시
-vector<pair<int, int>> chicken; // 치킨집의 좌표를 저장하는 벡터
-vector<pair<int, int>> house;
 int n, m;
-
-// 치킨집 중에 m 개의 치킨집을 폐업시키는 조합을 생성하는 함수
-void chicken_close() {
-	vector<int> brute(chicken.size(), 1); // chicken 벡터의 사이즈만큼 1로 채움
-	fill(brute.begin(), brute.begin() + chicken.size() - m, 0); // 1로 채운 brute 의 인덱스 0 부터 인덱스 chichen.size() - m 를 0으로 바꿈 
-	// => ex. 치킨집이 5개 ( = chicken.size()) 이고 m 이 3인 경우 brute 는 {0,0,1,1,1} 로 채워져서 치킨집 3개를 선택 할수 있도록 해준다.
-	do {
-		for (int i = 0; i < chicken.size(); i++) {
-			if (brute[i] == 0)
-				continue;
-			/* ~~~ */
-		}
-		cout << '\n';
-	} while (next_permutation(brute.begin(), brute.end()));
-}
+int board[53][53];
+vector<pair<int,int>> chicken;
+vector<pair<int,int>> house;
 
 int main(void)
 {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> board[i][j];
-			if (board[i][j] == 1)
-				house.push_back({ i,j });
-			if (board[i][j] == 2)
-				chicken.push_back({ i,j });
-		}
-	} 
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	vector<int> brute(chicken.size(), 1); // chicken 벡터의 사이즈만큼 1로 채움
-	fill(brute.begin(), brute.begin() + chicken.size() - m, 0); // 1로 채운 brute 의 인덱스 0 부터 인덱스 chichen.size() - m 를 0으로 바꿈 
-	// => ex. 치킨집이 5개 ( = chicken.size()) 이고 m 이 3인 경우 brute 는 {0,0,1,1,1} 로 채워져서 치킨집 3개를 선택 할수 있도록 해준다.
-	int mn = 9999999;
-	do {
-		int dist = 0;
-		for (auto h : house) {
-			int tmp = 99999999;
-			for (int i = 0; i < chicken.size(); i++) {
-				if (brute[i] == 0)
-					continue;
-				tmp = min(tmp, abs(chicken[i].first - h.first) + abs(chicken[i].second - h.second));
-			}
-			dist += tmp;
-		}
-		mn = min(mn, dist);
-	} while (next_permutation(brute.begin(), brute.end()));
+  cin >> n >> m;
+  
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<n; j++) {
+      cin >> board[i][j];
 
-	cout << mn;
+      if(board[i][j] == 1) {
+        house.push_back({i, j});
+      }
+
+      if(board[i][j] == 2) {
+        chicken.push_back({i, j});
+      }
+    }
+  }
+
+  vector<int> brute(chicken.size(), 1); // 조합(combination)에 사용할 리스트 생성
+  fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
+
+  int ans = 999999999; // 정답 저장 변수
+
+  do {
+    int dist = 0; // 도시의 치킨 거리를 저장할 변수
+    for(auto h : house) { // 각 집에 대하여
+      int tmp = 999999999;
+      for(int i=0; i<chicken.size(); i++) {
+        if(brute[i] == 0) {
+          continue;
+        }        
+        // 2개의 치킨 집을 순열 방식으로 추출하고, 현재 집과 추출된 치킨 집에 대한 거릿값 계산
+        tmp = min(tmp, abs(chicken[i].first - h.first) + abs(chicken[i].second - h.second));
+      }
+      dist += tmp;
+    }
+    // 한 집에대한 순환이 끝나면 현재 집에 대해 계산된 거리값과 비교하여 최솟값을 구함
+    ans = min(ans, dist);
+  } while(next_permutation(brute.begin(), brute.end()));
+
+  cout << ans;
+  
+  return 0;
 }
