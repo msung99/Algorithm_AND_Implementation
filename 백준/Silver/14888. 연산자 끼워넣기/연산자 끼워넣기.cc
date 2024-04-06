7ba9df9f50c7f67f101dca864099dffd85d9e0ef
Haon <88240193+msung99@users.noreply.github.com>
2024-04-06 10:04:18 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int num[12];
+int ops[12];
+int mn = 2100000000;
+int mx = -2100000000;
+
+int main(void)
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  cin >> n;
+
+  for(int i=0; i<n; i++) {
+    cin >> num[i];
+  }
+
+  for(int op=0, idx =1; op<4; op++) {
+    int x;
+    cin >> x;
+    while(x--) {
+      ops[idx++] = op;
+    }
+  }
+
+  do {
+    int res = 0;
+    for(int i=0; i<n; i++) {
+      if(ops[i] == 0) {
+        res += num[i];
+      } 
+      else if(ops[i] == 1) {
+        res -= num[i];
+      }
+      else if(ops[i] == 2) {
+        res *= num[i];
+      }
+      else if(ops[i] == 3) {
+        res /= num[i];
+      }
+    }
+    mx = max(res, mx);
+    mn = min(res, mn);
+  } while(next_permutation(ops+ 1, ops + n));
+
+  cout << mx << "\n";
+  cout << mn;
+
+  return 0;
+}
