6f3f221d034a1334f394c8daefc9b4ba4ca5a862
Haon <88240193+msung99@users.noreply.github.com>
2024-04-08 09:49:55 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,40 +1,37 @@
-// 최적의 해 : 가능한 회의 중에서 가장 먼저 끝나는 회의를 택하기
-
-/*
-명제 : 현재 시간이 t라고 할때, 시작 시간이 t 이상인 모든 회의 중에서 가장 먼저 끝나는 회의를 택하는 것이 최적해이다.
-
-=> 증명 : "귀류법" 을 사용하자. 명제를 거짓이라고 가정하고,
-회의 A 대신 회의 B를 택했을 때 더 많은 회의를 배정할 수 있다고 가정
-*/
-
 #include <bits/stdc++.h>
 using namespace std;
 
 int n;
-pair<int, int> s[100005]; // (종료시간, 시작시작) 쌍으로 저장
+pair<int, int> arr[100005];
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> s[i].second >> s[i].first;
-	}
-	sort(s, s + n); // 끝나는 시간이 빠른 순으로, 끝나는 시간이 같다면 시작하는 시간이 빠른 순으로 정렬
+    cin >> n;
 
-	int ans = 0; 
-	int t = 0; // 현재시간
+    for(int i=0; i<n; i++) {
+        cin >> arr[i].second >> arr[i].first;
+    }
 
-	for (int i = 0; i < n; i++) {
-		if (t > s[i].second)  
-			continue;
-		// 지금보는 스캐쥴의 시작시간이 t 이하면 바로 회의를 배정
-		// => 회의를 배정한다는 것? = ans 를 1증가시키고, t 는 s[i].first 로 변경시키는 것
-		ans++;
-		t = s[i].first; 
-	}
+    // 끝나는 시간이 가장 빠른것을 앞단으로 정렬
+    // 또한 끝나는 시간이 동일하다면, 시작하는 시간이 빠른 순으로 배치한다.
+    sort(arr, arr + n);
 
-	cout << ans;
+    int ans = 0;
+    int time = 0; // 현재 시간
+
+    for(int i=0; i<n; i++) {
+        if(time > arr[i].second) {
+            continue;
+        }
+        ans++;
+        time = arr[i].first; // 지금보는 시간이 time 이하려면 바로 회의를 배정한다.
+    }
+
+    cout << ans;
+
+    return 0;
 }
