22a79ede9e1cc3ad2ee112eabd0bd9d5fe1ab2a1
Haon <88240193+msung99@users.noreply.github.com>
2024-05-01 23:49:50 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,57 +1,66 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
 int n;
-int left_child[27];
-int right_child[27];
+int lc[30];
+int rc[30];
 
-void pre_order(int cur)
-{
-	cout << char(cur + 'A' - 1);
-	if (left_child[cur] != 0)
-		pre_order(left_child[cur]);
-	if (right_child[cur] != 0)
-		pre_order(right_child[cur]);
+void preOrder(int cur) {
+    cout << char(cur + 'A' - 1);
+    if(lc[cur] != 0) {
+        preOrder(lc[cur]);
+    }
+    if(rc[cur] != 0) {
+        preOrder(rc[cur]);
+    }
 }
 
-void in_order(int cur)
-{
-	if (left_child[cur] != 0)
-		in_order(left_child[cur]);
-	cout << char(cur + 'A' - 1);
-	if (right_child[cur] != 0)
-		in_order(right_child[cur]);
+void inOrder(int cur) {
+    if(lc[cur] != 0) {
+        inOrder(lc[cur]);
+    }
+    cout << char(cur + 'A' - 1);
+    if(rc[cur] != 0) {
+        inOrder(rc[cur]);
+    }
 }
 
-void post_order(int cur)
-{
-	if (left_child[cur] != 0)
-		post_order(left_child[cur]);
-	if (right_child[cur] != 0)
-		post_order(right_child[cur]);
-	cout << char(cur + 'A' - 1);
+void postOrder(int cur) {
+    if(lc[cur] != 0) {
+        postOrder(lc[cur]);
+    }
+    if(rc[cur] != 0) {
+        postOrder(rc[cur]);
+    }
+    cout << char(cur + 'A' - 1);
 }
 
 int main(void)
 {
-	cin >> n;
-	for(int i = 1; i <= n; i++)
-	{
-		char cur, left, right;
-		cin >> cur >> left >> right;
-		if (left != '.')
-			left_child[cur - 'A' + 1] = left - 'A' + 1;
-		if (right != '.')
-			right_child[cur - 'A' + 1] = right - 'A' + 1;
-	}
-	pre_order(1);
-	cout << '\n';
-	in_order(1);
-	cout << '\n';
-	post_order(1);
-	cout << '\n';
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        char c, l, r;
+        cin >> c >> l >> r;
+        if(l != '.') {
+            lc[c-'A'+1] = l-'A'+1;
+        }
+        if(r != '.') {
+            rc[c-'A'+1] = r-'A'+1;
+        }
+    }
+
+    preOrder(1);
+    cout << "\n";
+
+    inOrder(1);
+    cout << "\n";
+
+    postOrder(1);
+    cout << "\n";
+
+    return 0;
 }
