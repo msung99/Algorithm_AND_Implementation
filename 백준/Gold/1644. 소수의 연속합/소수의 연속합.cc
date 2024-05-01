9ea692eed53545aec40732b929dac8eaa37968e1
Haon <88240193+msung99@users.noreply.github.com>
2024-05-01 12:17:01 +0900

    [Gold III] Title: 소수의 연속합, Time: 32 ms, Memory: 5712 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,53 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void)
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+const int MXN = 4000002;
+vector<bool> seive(MXN, true);
+vector<int> primes;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  
+  for(int i=2; i*i<MXN; i++){
+    if (!seive[i]) continue;
+    for (int j = i * i; j < MXN; j += i) 
+      seive[j] = false;
+  }
+  for (int i = 2; i < MXN; i++) if (seive[i]) primes.push_back(i);
+  primes.push_back(0);
 
-	int n;
-	cin >> n;
-	vector<bool> v(n + 1, 1); // 0 ~ n 이 소수인지 아닌지 판별
-
-	// 에라토스테네스의 체
-	for (int i = 2; i * i <= n; i++) {
-		if (v[i] == 0)
-			continue;
-		for (int j = i + i; j <= n; j += i) {
-			v[j] = 0;  // j 는 i의 배수이기 때문에 소수가 아니다.
-		}
-	}
-	vector<int> prime;
-	for (int i = 2; i <= n; i++) {
-		if (v[i])
-			prime.push_back(i); // v[i] 가 1인 값만 prime 벡터에 추가하여 연속된 소수의 집합 생성
-	}
-
-	// 투 포인터 생성
-	int s = 0, e = 0; // 소수 값을 참조할 인덱스를 저장할 두개의 변수 선언 => 투포인터
-	int sum = 0; // 연속된 소수의 합
-	int ans = 0; // n을 만들 수 있는 경우의 수
-	while (true) {
-		if (sum > n) { // sum 이 n보다 크다면 s번째 소수 값을 sum 에서 뺸뒤, s값 증가
-			sum -= prime[s];
-			s++;
-		}
-		else if (sum < n) { // sum이 n보다 작으면 e번째 소수 값을 sum 에 더한뒤, e값 증가
-			if (e >= prime.size()) { // e가 소수를 저장한 벡터의 크기와 같거나 크다면 멈춤
-				break;
-			}
-			sum += prime[e];
-			e++;
-		}
-		else {  // sum이 n과 같다면 경우의 수 1증가
-			ans++;
-			if (e >= prime.size()) // e가 소수를 저장한 벡터의 크기와 같거나 크다면 멈춤
-				break;
-			// 그렇지않다면 sum에 e번째 소수값을 sum에 더한뒤, e값 증가
-			sum += prime[e];
-			e++;
-		}
-	}
-	cout << ans;
+  int target, s = 0, e = 1, ans = 0, tmpSum = primes[0];
+  cin >> target;
+  while (1) {
+    if (tmpSum == target) ans++;
+    if (tmpSum <= target) tmpSum += primes[e++];
+    if (target < tmpSum) tmpSum -= primes[s++];
+    if (e == int(primes.size())) break;
+  }
+  cout << ans;
 }
