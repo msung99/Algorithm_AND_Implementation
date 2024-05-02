6889c417c9bdaa6d1da654bd599eb0f78d6e8be4
Haon <88240193+msung99@users.noreply.github.com>
2024-05-02 18:15:54 +0900

    [Gold V] Title: 노드사이의 거리, Time: 12 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,65 +1,50 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <stack>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<pair<int, int>> adj_list[1001]; 
 int n, m;
-bool visited[1001];
-
-int BFS(int x, int y) 
-{
-	fill(visited, visited + 1001, false);
-	queue<pair<int, int>> q;
-	visited[x] = true;
-	q.push(make_pair( x,0 ));
-
-	while (!q.empty())
-	{
-		int curX = q.front().first;
-		int curDist = q.front().second;
-		q.pop();
-
-		if (curX == y)   // BFS 탐색을 돌리다가 우리가 원하는 x의 반대편 정점인 y 를 찾게되면 
-			return curDist; // 현재까지 계산한 거리 값을 리턴
-
-		// 현재 탐색중인 정점 curX 와 인접한 정점들에 대해 모두 탐색
-		for (int i = 0; i < adj_list[curX].size(); i++) {
-			
-			int nextX = adj_list[curX][i].first;
-			int nextDist = adj_list[curX][i].second;
-
-			if (!visited[nextX]) {
-				visited[nextX] = true;
-				q.push(make_pair(nextX, curDist + nextDist));  // 현재까지 탐색하면서 이어온 정점까지의 거리(curDist) 에 다음 정점으로 넘어갔을때의 정점까지의 거리(nextDist) 를 추가
-			}
-		}
-	}
-}
+vector<pair<int,int>> adj_list[1002];
+bool visited[1002];
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n >> m;
+    cin >> n >> m;
+    for(int i=1; i<=n-1; i++) {
+        int u, v, d;
+        cin >> u >> v >> d;
+        adj_list[u].push_back({v, d});
+        adj_list[v].push_back({u, d});
+    }
 
-	fill(visited, visited + 1001 , false);
+    while(m--) {
+        int source, dest;
+        cin >> source >> dest;
+        fill(visited, visited + n + 1 , false);
+        queue<pair<int,int>> q;
+        q.push({source, 0});
+        visited[source] = true;
 
-	for (int i = 0; i < n - 1; i++)
-	{
-		int u, v, dist;
-		cin >> u >> v >> dist;
-		adj_list[u].push_back({ v, dist });
-		adj_list[v].push_back({ u, dist });
-	}
-	for (int i = 0; i < m; i++)
-	{
-		int x, y;
-		cin >> x >> y;
-		cout << BFS(x, y) << '\n';
-	}
+        while(!q.empty()) {
+            auto [cur, dist] = q.front();
+            q.pop();
+
+            if(cur == dest) {
+                cout << dist << "\n";
+                break;
+            }
+
+            for(auto [next, nextDist] : adj_list[cur]) {
+                if(visited[next]) {
+                    continue;
+                }
+                visited[next] = true;
+                q.push({next, dist + nextDist});
+            }
+        }
+    }
+
+    return 0;
 }
