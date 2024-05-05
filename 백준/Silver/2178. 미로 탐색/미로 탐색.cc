10e320c744799aa745422653a652e7c6e0f3e8b5
Haon <88240193+msung99@users.noreply.github.com>
2024-05-05 19:17:46 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2236 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,36 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,m;
-int dx[4] = {0,1,0,-1};
-int dy[4] = {-1,0,1,0};
-int board[101][101];
-int dist[101][101];
+int n, m;
+int board[102][102];
+int dist[102][102];
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
 
-void bfs() {
+void bfs(int x, int y) {
     queue<pair<int,int>> q;
-    dist[0][0] = 1;
-    q.push({0,0});
-    int x,y;    
+    q.push({x, y});
+
+    dist[0][0] = 0;
 
     while(!q.empty()) {
-        tie(y, x) = q.front();
+        auto cur = q.front();
+        int x = cur.first;
+        int y = cur.second;
         q.pop();
-        for(int i=0; i<4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
 
-            if(ny < 0 || ny >= n || nx < 0 || nx >= m || board[ny][nx] == 0) {
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
                 continue;
             }
-            if(dist[ny][nx]) {
+            if(board[nx][ny] != 1 || dist[nx][ny] != -1) {
                 continue;
             }
-            dist[ny][nx] = dist[y][x] + 1;
-            q.push({ny, nx});
+            q.push({nx, ny});
+            dist[nx][ny] = dist[x][y] + 1;
         }
     }
-    printf("%d", dist[n-1][m-1]);
 }
 
 int main(void)
@@ -39,11 +41,26 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
 
-    scanf("%d %d", &n, &m);
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            scanf("%1d", &board[i][j]);
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++) {
+        fill(dist[i], dist[i] + m, -1);
+    }
+
+    for(int i=0; i<n; i++) {
+        string str;
+        cin >> str;
+
+        for(int j=0; j<m; j++) {
+            if(str[j] == '1') {
+                board[i][j] = 1;
+            }
         }
     }
-    bfs();
+
+    bfs(0, 0);
+
+    cout << dist[n-1][m-1] + 1;
+
+    return 0;
 }
