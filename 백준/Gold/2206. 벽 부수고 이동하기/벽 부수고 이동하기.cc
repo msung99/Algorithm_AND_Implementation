9318de91c3d54436bf91f58f8c23a2d6555b8ac4
Haon <88240193+msung99@users.noreply.github.com>
2024-05-06 22:26:07 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 76 ms, Memory: 12640 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -2,63 +2,72 @@
 using namespace std;
 
 int n, m;
-int dx[4] = { 0,1,0,-1 };
-int dy[4] = { -1,0,1,0 };
-char board[1001][1001];
-int dist[1001][1001][2];
+char board[1002][1002];
+int dist[1002][1002][2];
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
 
-int bfs() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			dist[i][j][0] = -1;
-			dist[i][j][1] = -1;
-		}
-	}
-	dist[0][0][0] = 1;
-	dist[0][0][1] = 1;
+void bfs() {
+    queue<tuple<int,int,int>> q;
+    q.push({0,0,0});
+    
+    while(!q.empty()) {
+        auto cur = q.front();
+        int x, y, broken;
+        tie(x, y, broken) = cur;
+        q.pop();
 
-	queue<tuple<int, int, int>> q;
-	q.push({0,0,0});
+        if(x == n-1 && y == m-1) {
+            cout << dist[x][y][broken];
+            return;
+        }
 
-	while (!q.empty()) {
-		int x, y, broken;
-		tie(x, y, broken) = q.front();
-		q.pop();
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-		if (x == n - 1 && y == m - 1)
-			return dist[x][y][broken];
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                continue;
+            }
 
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+            if(dist[nx][ny][broken] == -1 && board[nx][ny] == '0') {
+                dist[nx][ny][broken] = dist[x][y][broken] + 1;
+                q.push({nx, ny, broken});
+            }
 
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m)
-				continue;
-
-			if (board[nx][ny] == '0' && dist[nx][ny][broken] == -1) {
-				dist[nx][ny][broken] = dist[x][y][broken] + 1;
-				q.push({ nx, ny, broken });
-			}
-			
-			if (!broken && board[nx][ny] == '1' && dist[nx][ny][1] == -1) {
-				dist[nx][ny][1] = dist[x][y][broken] + 1;
-				q.push({ nx,ny,1 });
-			}
-		}
-	}
-	return -1;
+            if(dist[nx][ny][broken] == -1 && board[nx][ny] == '1' && !broken) {
+                dist[nx][ny][1] = dist[x][y][broken] + 1;
+                q.push({nx, ny, 1});
+            }
+        }
+    }
+    cout << -1;
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n >> m;
-	for (int i = 0; i < n; ++i)
-		for (int j = 0; j < m; ++j)
-			cin >> board[i][j];
-	cout << bfs();
-	return 0;
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            dist[i][j][0] = dist[i][j][1] = -1;
+        }
+    }
+    dist[0][0][0] = 1;
+    dist[0][0][1] = 1;
+    
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    bfs();
+
+    return 0;
 }
