8ec0719bb49e955da2fc401cb4d50a0306d6b35a
Haon <88240193+msung99@users.noreply.github.com>
2024-05-06 15:48:03 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,81 +1,122 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string board[101];
-bool visited1[101][101];
 int n;
-int dx[4] = { 0,1,0,-1 };
-int dy[4] = { -1,0,1,0 };
+char board[102][102];
+bool visited[102][102];
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1 ,0, 1, 0};
 
-void bfs1(int x, int y) {
-	queue<pair<int, int>> q1;
-	q1.push({ x,y});
-	while (!q1.empty()) {
-		int x = q1.front().first;
-		int y = q1.front().second;
-		q1.pop();
-		visited1[x][y] = true;
+void bfs(int x, int y) {
+    queue<pair<int,int>> q;
+    q.push({x, y});
+    visited[x][y] = true;
 
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+    while(!q.empty()) {
+        auto cur = q.front();
+        int x = cur.first;
+        int y = cur.second;
+        visited[x][y] = true;
+        q.pop();
 
-			if (nx < 0 || nx >= n || ny < 0 || ny >= n)
-				continue;
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-			if (board[x][y] != board[nx][ny] || visited1[nx][ny])
-				continue;
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                continue;
+            }
 
-			q1.push({ nx,ny });
-			visited1[nx][ny] = true;
-		}
-	}
+            // 색깔이 다르다면 continue
+            if(visited[nx][ny] || board[x][y] != board[nx][ny]) {
+                continue;
+            }
+
+            visited[nx][ny] = true;
+            q.push({nx, ny});
+        }
+    }
+}
+
+void bfs2(int x, int y) {
+    queue<pair<int,int>> q;
+    q.push({x, y});
+    visited[x][y] = true;
+
+    while(!q.empty()) {
+        auto cur = q.front();
+        int x = cur.first;
+        int y = cur.second;
+        visited[x][y] = true;
+        q.pop();
+
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                continue;
+            }
+
+            // 색깔이 다르다면 continue
+            if(visited[nx][ny] || board[x][y] != board[nx][ny]) {
+                continue;
+            }
+        }
+    }
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> board[i];
-	}
+    cin >> n;
 
-	// 적록색약이 아닌 경우 (R, G 를 다른 색깔로 분류)
-	int count1 = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (!visited1[i][j]) {
-				bfs1(i, j);
-				count1++;
-			}
-		}
-	}
+    for(int i=0; i<n; i++) {
+        string s;
+        cin >> s;
+        for(int j=0; j<s.length(); j++) {
+            board[i][j] = s[j];
+        }
+    }
 
-	// 적록색약인 경우 (R, G 를 동일한 색깔로 취급)
-	// => 초록과 빨강을 구분 못하므로 초록을 빨강으로 바꿔줌
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (board[i][j] == 'G')
-				board[i][j] = 'R';
-		}
-	}
+    int cnt1 = 0;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(!visited[i][j]) {
+                bfs(i, j);
+                cnt1++;
+            }
+        }
+    }
 
-	for (int i = 0; i < n; i++) {
-		fill(visited1[i], visited1[i] + n, false);
-	}
+    cout << cnt1 << " ";
 
-	int count2 = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (!visited1[i][j]) {
-				bfs1(i, j);
-				count2++;
-			}
-		}
-	}
-	cout << count1 << ' ' << count2;
-	return 0;
+    for(int i=0; i<n; i++) {
+        fill(visited[i], visited[i] + n, false);
+    }
+
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(board[i][j] == 'R') {
+                board[i][j] = 'G';
+            }
+        }
+    }
+
+    int cnt2 = 0;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(!visited[i][j]) {
+                bfs(i, j);
+                cnt2++;
+            }
+        }
+    }
+
+    cout << cnt2;
+
+    return 0;
 }
