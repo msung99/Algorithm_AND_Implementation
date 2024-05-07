4fdef50cf2890acf16ca5147a61bd72c10abfce1
Haon <88240193+msung99@users.noreply.github.com>
2024-05-08 00:55:05 +0900

    [Silver III] Title: N과 M (3), Time: 360 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -1,33 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[8];
+// 재귀적 사고: K-1 번쨰 수를 택했다면 K 번째 수도 택할 수 있다.
+// K 번째수는 K-1 번째까지 등장하지 수 중에서도 택할 수 있다.
+int arr[10];
 int n, m;
 
-// 현재 k개까지 수를 택했다.
 void func(int k) {
-	if (k == m) {
-		for (int i = 0; i < m; i++) {
-			cout << arr[i] << ' ';
-		}
-		cout << "\n";
-		return;
-	}
+    if(k == m) {
+        for(int i=0; i<m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << "\n";
+        return;
+    }
 
-	for (int i = 1; i <= n; i++) {
-		arr[k] = i;
-		func(k + 1);
-	}
+    for(int i=1; i<=n; i++) {
+        arr[k] = i;
+        func(k+1);
+    }
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n >> m;
-	func(0);
+    cin >> n >> m;
 
-	return 0;
+    func(0);
+
+    return 0;
 }
