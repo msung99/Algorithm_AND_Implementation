53f97c3f84eb713d2e9e0455bfc09eda6402ddcf
Haon <88240193+msung99@users.noreply.github.com>
2024-05-08 00:49:22 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -2,36 +2,40 @@
 using namespace std;
 
 int n, m;
-int arr[10]; // 수열
-bool isused[10]; // isused[i] : 숫자 i가 현재 사용되었는지 유무
+int arr[10];
+bool isused[10];
+// 재귀적 사고: K-1 번쨰 수를 택했다면 K 번째 수도 택할 수 있다.
+// K 번째수는 K-1 번째까지 등장하지 않았던 수 중에서 택할 수 있다.
 
-// 현재 k 개까지 숫자를 택한 상황에서 arr[k] 를 정하는 함수
+// 현재 k번째 수를 택함을 의미
 void func(int k) {
-	if (k == m) {  // base condition : 현재 m개를 택했다면 수열을 출력하고 종료
-		for (int i = 0; i < m; i++) {
-			cout << arr[i] << ' ';
-		}
-		cout << "\n";
-		return;
-	}
-	
-	// i부터 n까지 수를 차례대로 확인하며 아직 쓰이지 않은 수를 찾아낸다.
-	for (int i = 1; i <= n; i++) {
-		if (!isused[i]) {
-			arr[k] = i; // 숫자 i를 아직 사용안했다면 수열의 k번째 숫자로 i를 선택
-			isused[i] = 1;
-			func(k + 1);
-			isused[i] = 0;
-		}
-	}
+    if(k == m) {
+        for(int i=0; i<m; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for(int i=1; i<=n; i++) {
+        if(!isused[i]) {
+            arr[k] = i; // k번쨰 수는 k-1번째까지 등장하지 않았던 수 (1~n 사이의 수) 를 택한다.
+            isused[i] = true;
+            func(k+1); // k번쨰 수를 택했다면, k+1번쨰 수도 택할 수 있다.
+            isused[i] = false;
+        }        
+    }
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n >> m;
-	func(0);
+    cin >> n >> m;
+
+    func(0);
+
+    return 0;
 }
