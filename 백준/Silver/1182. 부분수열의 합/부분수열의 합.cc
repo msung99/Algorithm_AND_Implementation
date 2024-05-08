443c8f14c493ec1e88665c1658c6f286aa2323ba
Haon <88240193+msung99@users.noreply.github.com>
2024-05-08 10:32:19 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -1,32 +1,41 @@
-#include <iostream>
+// k 번째 수에 대해서까지 부분수열을 만들었다면 k+1 번쨰 수에 대해서도 부분수열을 만들 수 있다.
+// k+1 번쨰 수에 대해 부분수열을 만드는 방법 : k번째 수를 택하는 경우 / k번쨰 수를 택하지 않는 경우
+#include <bits/stdc++.h>
 using namespace std;
 
 int n, s;
-int num_list[100];
+int arr[22];
+int res;
 
-int cnt;
+void func(int k, int total) {
+  // n번째 숫자에 대해서까지 선택 여부를 모두 결정해줬다면
+  if(k == n) {
+    if(total == s) {
+      res++;
+    }
+    return;
+  }
 
-void func(int cur, int total) // cur : 현재 탐색하며 살펴보고 있는 숫자의 인덱스 번호, total : 부분수열의 원소들의 합
-{
-	if (cur == n) // 부분수열의 원소가 n개가 되었을 때
-	{
-		if (total == s) // 만든 부분수열의 원소들의 합이 s 가 되었다면
-			cnt++; // 경우의 수 카운팅
-		return;
-	}
-	// 부분수열에 들어갈 주어진 숫자들에 대해 2가지 경우로 나눔. 1) 수열에 그 숫자( = num_list[cur])를 포함시킨다 2) 포함 안시킨다
-	func(cur + 1, total); // 부분수열에 포함 안시키는 경우
-	func(cur + 1, total + num_list[cur]); // 포함시키는 경우 
+  func(k+1, total);
+  func(k+1, total + arr[k]);
 }
 
 int main(void)
 {
-	cin >> n >> s;
-	for (int i = 0; i < n; i++)
-		cin >> num_list[i];
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	func(0, 0);
-	if (s == 0)  // 부분수열의 크기가 양수인것만 카운팅 한다고 했으므로 공집합은 제외해 줘야함. s가 0이 아닐때는 상관없고, s 가 0일때는 cnt에서 1을 뺴줘야함
-		cnt--;
-	cout << cnt;
+  cin >> n >> s;
+  for(int i=0; i<n; i++) {
+    cin >> arr[i];
+  }
+
+  func(0, 0);
+  if(s == 0) {
+    res--;
+  }
+  cout << res;
+
+  return 0;
 }
