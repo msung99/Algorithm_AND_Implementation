49f02e42a02bed6c32f6df6cd0ed8f359f9cdd8b
Haon <88240193+msung99@users.noreply.github.com>
2024-05-08 12:31:00 +0900

    [Gold IV] Title: N-Queen, Time: 1676 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,58 +1,51 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool visited1[40];
-bool visited2[40];
-bool visited3[40];
-
-int cnt = 0; // n개의 퀸을 적절히 배치 가능한 경우의 수
 int n;
+bool isused1[30];
+bool isused2[30];
+bool isused3[30];
+int cnt;
 
-void func(int cur)  // func(cur) 에서는 (cur, 0), (cur, 1), ..., (cur, n-1) 에 퀸을 둘 수 있는지 확인한다.
-{                   // 둘 수 있는지는 visited1, visited2, visited3 를 확인하면 된다.
-	// 만일 둘 수 있다면 visited1[i], visited2[cur+i], visited3[cur-i+n-1] 을 true 로 변경한 후 func(cur+1) 을 호출하면 된다.
+// k-1번쨰까지 퀸을 배치했다면, K번째 퀸도 배치할 수 있다.
+// K번쨰 퀸을 배치하는 방법 : k-1 번쨰까지 배치한 퀸들과 동일한 y축, (x+y)축, (x-y)축에 위치해선 안된다.
+void func(int k) {
+  if(k == n) { // k번째 퀸까지 배치하는데 모두 성공했다면 
+    cnt++; // 배치를 카운팅
+    return;
+  }
 
-	if (cur == n) { // func(n) 을 호출하면 퀸을 n개를 놓는데 성공했다는 의미이니 cnt를 1 증가시킴
-		cnt++;
-		return;
-	}
+  for(int i=0; i<n; i++) {
+    // 퀸을 배치하려는데, 이미 동일한 축 / (x+y)축 / (x-y)축에 배치된 퀸이 존재한다면
+    if(isused1[i] || isused2[k+i] || isused3[k-i+n-1]) {
+      continue; // 배치 못함
+    }
 
-	// x좌표는 cur인 상태에서 y좌표인 i 값을 0부터 n-1 까지 돌음. (즉 위에서 언급했듯이, (cur, 0), (cur, 1), ..., (cur, n-1) 에 퀸을 둘 수 있는지 확인)
-	for (int i = 0; i < n; i++) 
-	{
-		// 여기서 cur이 x좌표값이고, i가 y 좌표값 이다.  (즉 cur은 행을, i는 열을 의미한다)
+    // k번째 퀸을 배치한다.
+    // 배치할때 x축, (x+y)축, (x-y)축에 배치 여부를 표시한다.
+    isused1[i] = true;
+    isused2[k+i] = true;
+    isused3[k-i+n-1] = true;
+    
+    func(k+1);
 
-		// 1. visited1[i] : 같은 열에 퀸이 있는지를 저장. => y좌표가 일치하는지 확인
-		// (x,y) 에 퀸이 있으면 visited[y] 를, 즉 visited[i] 를 true 로 만들면 된다.
-		//  ex) (0,1) 과 (2,1) 이 y좌표가 1로 동일하므로 둘은 같은 열에 있는 퀸으로, 두 퀸은 서로 공격을 하게된다. (부적절한 배치)
-		
-		// 2. visited2[i + cur] : 좌측 하단과 우측 상단을 잇는 대각선에 퀸이 있는지를 저장 => x + y 값이 같으면 같은 대각선에 두 퀸이 위치해 있는것이다
-		// i + cur 은 x + y 를 의미한다. (x,y) 에 퀸이 있으면 visited2[x+y]를, 다시말해 visited2[i+cur] 를 true 로 만들면 된다.
-		// ex) (3,0) 과 (1,2) 은 x + y = 3 으로, 같은 대각선 상에 위치해있어서 두 퀸은 서로 공격하게 된다. (부적절한 배치)
-		
-		// 3. visited3[cur - i + n -1] : 좌측 상단과 우측 하단을 잇는 대각선에 퀸이 있는지를 저장 => x - y 값이 같으면 같은 대각선에 두 퀸이 위치해 있는것
-		// (x,y) 에 퀸이 있으면 visited3[x - y + n - 1] 를, 즉 visited3[cur - i + n - 1] 를 true 로 만들면 된다.
-		// ex) (1,1) 과 (3,3) 은 x - y = 0 으로, 같은 대각선 상에 위치해있어서 두 퀸을 서로 공격하게 된다. (부적절한 배치)
-
-		if (visited1[i] || visited2[i + cur] || visited3[cur - i + n - 1]) // 부적절한 퀸의 배치인 경우
-			continue;   // 거기에는 퀸 배치 안함
-
-		visited1[i] = 1;
-		visited2[i + cur] = 1;
-		visited3[cur - i + n - 1] = 1;
-
-		func(cur + 1); 
-
-		visited1[i] = 0;
-		visited2[i + cur] = 0;
-		visited3[cur - i + n - 1] = 0;
-	}
+    isused1[i] = false;
+    isused2[k+i] = false;
+    isused3[k-i+n-1] = false;
+  }
 }
 
 int main(void)
 {
-	cin >> n;
-	func(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	cout << cnt;
+  cin >> n;
+
+  func(0);
+
+  cout << cnt;
+
+  return 0;
 }
--- 백준/Gold/9663. N－Queen/README.md
+++ 백준/Gold/9663. N－Queen/README.md
@@ -1,14 +1,18 @@
-# [Gold V] N-Queen - 9663 
+# [Gold IV] N-Queen - 9663 
 
 [문제 링크](https://www.acmicpc.net/problem/9663) 
 
 ### 성능 요약
 
-메모리: 2020 KB, 시간: 1636 ms
+메모리: 2020 KB, 시간: 1676 ms
 
 ### 분류
 
-백트래킹(backtracking), 브루트포스 알고리즘(bruteforcing)
+백트래킹, 브루트포스 알고리즘
+
+### 제출 일자
+
+2024년 5월 8일 12:30:50
 
 ### 문제 설명
 
