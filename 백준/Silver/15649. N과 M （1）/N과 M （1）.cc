5fa366d99b5b01ef6737ef45fd10f25c087b9de2
Haon <88240193+msung99@users.noreply.github.com>
2024-05-09 21:08:34 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,41 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+// 1~n 사이의 숫자중에 중복없이 m개를 고른 수열
 int n, m;
 int arr[10];
 bool isused[10];
-// 재귀적 사고: K-1 번쨰 수를 택했다면 K 번째 수도 택할 수 있다.
-// K 번째수는 K-1 번째까지 등장하지 않았던 수 중에서 택할 수 있다.
 
-// 현재 k번째 수를 택함을 의미
 void func(int k) {
-    if(k == m) {
-        for(int i=0; i<m; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << "\n";
-        return;
+  if(k == m) {
+    for(int i=0; i<m; i++) {
+      cout << arr[i] << ' ';
     }
+    cout << "\n";
+  }
 
-    for(int i=1; i<=n; i++) {
-        if(!isused[i]) {
-            arr[k] = i; // k번쨰 수는 k-1번째까지 등장하지 않았던 수 (1~n 사이의 수) 를 택한다.
-            isused[i] = true;
-            func(k+1); // k번쨰 수를 택했다면, k+1번쨰 수도 택할 수 있다.
-            isused[i] = false;
-        }        
+  for(int i=1; i<=n; i++) {
+    if(!isused[i]) {
+      isused[i] = true;
+      arr[k] = i;
+      func(k+1);
+      isused[i] = false;
     }
+  }
 }
 
 int main(void)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-    cin >> n >> m;
+  cin >> n >> m;
 
-    func(0);
+  func(0);
 
-    return 0;
+  return 0;
 }
