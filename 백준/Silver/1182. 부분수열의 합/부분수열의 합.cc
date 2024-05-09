2a79abe732dd12e5a1c3adba99557c49bd09f770
Haon <88240193+msung99@users.noreply.github.com>
2024-05-10 00:17:10 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -2,21 +2,19 @@
 using namespace std;
 
 int n, s;
-int arr[22];
-int res;
+int cnt;
+int num[22];
 
-// k 번쨰 수를 택했다면 k+1 번쨰 수도 택할 수 있다.
-// k+1번째 수를 택하는 방법 : k+1 번쨰 수를 택한다 / 택하지 않는다.
 void func(int k, int total) {
   if(k == n) {
     if(total == s) {
-      res++;      
+      cnt++;
     }
     return;
   }
 
-  func(k+1, total + arr[k]);
   func(k+1, total);
+  func(k+1, total + num[k]);
 }
 
 int main(void)
@@ -28,15 +26,15 @@ int main(void)
   cin >> n >> s;
 
   for(int i=0; i<n; i++) {
-    cin >> arr[i];
+    cin >> num[i];
   }
 
   func(0, 0);
 
   if(s == 0) {
-    res--;
+    cnt--;
   }
-  cout << res;
+  cout << cnt;
 
   return 0;
 }
