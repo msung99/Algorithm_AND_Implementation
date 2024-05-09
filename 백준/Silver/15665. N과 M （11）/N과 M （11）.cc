65bac6511b2cbd6d3130dd83e386036a0f06b85e
Haon <88240193+msung99@users.noreply.github.com>
2024-05-09 22:05:47 +0900

    [Silver II] Title: N과 M (11), Time: 120 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -1,25 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// k-1 번쨰 수를 택하여 순열을 만들 수 있다면, K번째 수를 택하여 순열을 만들 수 있다.
-// 택하는 방법 : 중복되는 원소들을 제거하여 그 중 원소를 택한다.
 int n, m;
-int arr[10002];
+int arr[10];
 int num[10];
-bool isAppeared[10002];
 
-void func(int k, int cnt) {
+void func(int k) {
   if(k == m) {
     for(int i=0; i<m; i++) {
       cout << arr[i] << ' ';
     }
     cout << "\n";
     return;
-  } 
+  }
 
-  for(int i=0; i<cnt; i++) {
-    arr[k] = num[i];
-    func(k+1, cnt);
+  int tmp = 0;
+  for(int i=0; i<n; i++) {
+    if(tmp != num[i]) {
+      arr[k] = num[i];
+      tmp = num[i];
+      func(k+1);
+    }  
   }
 }
 
@@ -31,20 +32,12 @@ int main(void)
 
   cin >> n >> m;
 
-  int cnt = 0; // 중복된 숫자를 제거하고 남은 순수한 원소 갯수
-
   for(int i=0; i<n; i++) {
-    int data;
-    cin >> data;
-    if(!isAppeared[data]) {
-      isAppeared[data] = true;
-      num[cnt] = data;
-      cnt++;
-    }
+    cin >> num[i];
   }
+  sort(num, num + n);
 
-  sort(num, num + cnt);
-  func(0, cnt);
+  func(0);
 
   return 0;
 }
