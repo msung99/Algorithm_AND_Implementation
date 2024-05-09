a281c8920d88b3dbf473ae391fa1cb388d493bd7
Haon <88240193+msung99@users.noreply.github.com>
2024-05-09 18:30:45 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2088 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,51 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
 int board[130][130];
 int cnt[2];
 
-bool check(int x, int y, int size) {
-	for (int i = x; i < x + size; i++) {
-		for (int j = y; j < y + size; j++) {
-			if (board[i][j] != board[x][y])
-				return false;
-		}
-	}
-	return true;
-}
+void func(int n, int startX, int startY) {
+  if(n == 1) {
+    cnt[board[startX][startY]]++;
+    return;
+  }
 
-void func(int x, int y, int size) {
-	if (check(x, y, size)) {
-		cnt[board[x][y]]++;
-		return;
-	}
+  bool isSameArea = true;
+  int color = board[startX][startY];
 
-	int divideSize = size / 2;
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 2; j++) {
-			func(x + i*divideSize, y + j*divideSize, divideSize);
-		}
-	}
+  for(int i=startX; i < startX + n; i++) {
+    for(int j=startY; j < startY + n; j++) {
+      if(color != board[i][j]) {
+        isSameArea = false;
+        break;
+      }
+    }
+  }
+
+  if(isSameArea) {
+    cnt[board[startX][startY]]++;
+  } else {
+    int size = n/2;
+    for(int i=0; i<2; i++) {
+      for(int j=0; j<2; j++) {
+        int newX = startX + size * i;
+        int newY = startY + size * j;
+        func(size, newX, newY);
+      }
+    }
+  }
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-	cin >> n;
+  int n;
+  cin >> n;
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<n; j++) {
+      cin >> board[i][j];
+    }
+  }
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> board[i][j];
-		}
-	}
+  func(n, 0, 0);
 
-	func(0, 0, n);
-	for (int i = 0; i < 2; i++) {
-		cout << cnt[i] << "\n";
-	}
-	return 0;
+  cout << cnt[0] << "\n";
+  cout << cnt[1];
+
+  return 0;
 }
