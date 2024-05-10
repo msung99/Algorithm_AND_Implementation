6d0e7c3b5249ca589dff024a9c0d8d0184d9abba
Haon <88240193+msung99@users.noreply.github.com>
2024-05-10 14:00:21 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,21 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int res = 1;
-// a를 k-1번 곱한 결과를 구할 수 있다면, k번 곱한 결과도 구할 수 있다.
-
-long long func(long long a, long long b, long long c) {
+long long func(int a, int b, int c) {
   if(b == 1) {
     return a % c;
   }
 
-  long long val = func(a, b/2, c);
-  val = val * val % c;
-  if(b%2 == 1) {
-    return val * a % c;
+  long long div = func(a, b/2, c);
+  if(b % 2 == 0) {
+    return div * div % c;
+  } else {
+    return ((div * div % c) * a) % c;
   }
-  return val;
 }
+
 
 int main(void)
 {
@@ -23,10 +21,8 @@ int main(void)
   cin.tie(0);
   cout.tie(0);
 
-  long long a, b, c;
-
+  int a, b, c;
   cin >> a >> b >> c;
-
   cout << func(a, b, c);
 
   return 0;
