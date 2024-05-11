a21a8ce87f917dd543e55f98be64f3a84c97bc8f
Haon <88240193+msung99@users.noreply.github.com>
2024-05-11 19:13:39 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -2,55 +2,50 @@
 using namespace std;
 
 int n, m;
-int board[53][53];
 vector<pair<int,int>> chicken;
 vector<pair<int,int>> house;
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-  cin >> n >> m;
-  
-  for(int i=0; i<n; i++) {
-    for(int j=0; j<n; j++) {
-      cin >> board[i][j];
+    cin >> n >> m;
 
-      if(board[i][j] == 1) {
-        house.push_back({i, j});
-      }
-
-      if(board[i][j] == 2) {
-        chicken.push_back({i, j});
-      }
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            int data;
+            cin >> data;
+            if(data == 1) {
+                house.push_back({i, j});
+            }
+            if(data == 2) {
+                chicken.push_back({i, j});
+            }
+        }
     }
-  }
 
-  vector<int> brute(chicken.size(), 1); // 조합(combination)에 사용할 리스트 생성
-  fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
+    vector<int> brute(chicken.size(), 1);
+    fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
+    // chicken.size() 개 중에서 m개를 선택하기 위한 셋팅
+    int mn = 999999999;
 
-  int ans = 999999999; // 정답 저장 변수
+    do {
+        int dist = 0;
+        for(auto h : house) {
+            int tmp = 999999999;
+            for(int i=0; i<chicken.size(); i++) {
+                if(brute[i] == 1) {
+                    tmp = min(tmp, abs(chicken[i].first - h.first) + abs(chicken[i].second - h.second));                    
+                }
+            }
+            dist += tmp;
+        }
+        mn = min(mn, dist);
+    } while(next_permutation(brute.begin(), brute.end()));
 
-  do {
-    int dist = 0; // 도시의 치킨 거리를 저장할 변수
-    for(auto h : house) { // 각 집에 대하여
-      int tmp = 999999999;
-      for(int i=0; i<chicken.size(); i++) {
-        if(brute[i] == 0) {
-          continue;
-        }        
-        // 2개의 치킨 집을 순열 방식으로 추출하고, 현재 집과 추출된 치킨 집에 대한 거릿값 계산
-        tmp = min(tmp, abs(chicken[i].first - h.first) + abs(chicken[i].second - h.second));
-      }
-      dist += tmp;
-    }
-    // 한 집에대한 순환이 끝나면 현재 집에 대해 계산된 거리값과 비교하여 최솟값을 구함
-    ans = min(ans, dist);
-  } while(next_permutation(brute.begin(), brute.end()));
+    cout << mn;
 
-  cout << ans;
-  
-  return 0;
+    return 0;
 }
