61aef63fbb1273a939b125bf50d6b81bea436735
Haon <88240193+msung99@users.noreply.github.com>
2024-05-12 17:02:33 +0900

    [Gold V] Title: 로봇 청소기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.cc
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, d;
-int x, y; // (x,y): 로봇의 현재 좌표
-int board[52][52]; // -1: 이미 방문한 좌표
-int ans; 
+int n, m;
+int x, y, d;
+// d => 1: 북, 2: 동, 3: 남, 4: 서
+int board[52][52];
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 
@@ -16,45 +16,49 @@ int main(void)
 
     cin >> n >> m;
     cin >> x >> y >> d;
-
+    
     for(int i=0; i<n; i++) {
         for(int j=0; j<m; j++) {
             cin >> board[i][j];
         }
     }
 
-    int checkCnt = 0; // 방향 탐지 횟수
+    int cnt = 0; // 방향 탐지 횟수
     while(true) {
-        // 청소하지 않은 빈 칸일 경우 청소함.
+
+        // 현재 칸이 청소되지 않았다면 청소를 한다.
         if(board[x][y] == 0) {
-            ans++;
+            cnt++;
         }
-        board[x][y] = -1; // 방문(청소 완료) 처리
-        bool cleaned = false; // 4방향 중 청소된 곳이 있는지
+        board[x][y] = -1; // 현재 칸에 대해 방문 완료(청소 완료) 처리
+
+        bool isCleaned = false; // 4방향 중 청소된 곳이 있는지
         for(int i=0; i<4; i++) {
-            d = (d + 3) % 4; // 반시계 방향으로 회전함.
-            if(board[x + dx[d]][y + dy[d]] == 0) {
+            d = (d + 3) % 4;  // 반시계 방향으로 회전함.
+            if(board[x+dx[d]][y+dy[d]] == 0) {
                 x = x + dx[d];
                 y = y + dy[d];
-                cleaned = true;
+                isCleaned = true;
                 break;
             }
         }
 
-        if(cleaned) {
+        // 4방향 중에 청소 가능한 곳이 있어서 그곳으로 이동한 경우
+        if(isCleaned) {
             continue;
         }
 
         // 4방향 중 청소를 못한 경우, 후진하여 청소를 시도
         if(board[x-dx[d]][y-dy[d]] == 1) {
-            break; // 후진이 불가능하면 멈춤
+            break;
         }
 
-        // 후진이 가능하다면 후진을 함.
+        // 후진이 가능하다면 후진을 한다.
         x = x - dx[d];
         y = y - dy[d];
     }
-    cout << ans;
-    
+
+    cout << cnt;
+
     return 0;
 }
