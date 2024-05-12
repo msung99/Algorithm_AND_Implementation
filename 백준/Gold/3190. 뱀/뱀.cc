7bc5b4ffa540ae59fc1c26fd16c315d409c96449
Haon <88240193+msung99@users.noreply.github.com>
2024-05-12 18:55:50 +0900

    [Gold IV] Title: 뱀, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.cc
+++ 백준/Gold/3190. 뱀/뱀.cc
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, k, l;
+int n;
+int k;
+int l;
 int board[102][102];
-int move_dir = 1; // 현재 뱀의 이동 방향 
-int second;
-int dx[4] = {1, 0, -1 ,0};
-int dy[4] = {0, 1, 0, -1};
 deque<pair<int,int>> dq;
-queue<pair<int, char>> switchDir;
+queue<pair<int,char>> switchDir;
+int moveDir = 1;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
 int main(void)
 {
@@ -16,16 +17,15 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
 
-    cin >> n >> k;
-
-    for(int i=0; i<k; i++) {
+    cin >> n;
+    cin >> k;
+    while(k--) {
         int x, y;
         cin >> x >> y;
-        board[x][y] = -1;
+        board[x][y] = -1; // 사과를 -1로 표시
     }
 
     cin >> l;
-    
     while(l--) {
         int time;
         char command;
@@ -33,43 +33,51 @@ int main(void)
         switchDir.push({time, command});
     }
 
-    dq.push_front({1, 1}); // 뱀의 시작점은 (1,1)    
-    // dq에 담기는 정보 = 현재 뱀의 위치
+    dq.push_back({1, 1});
+
+    int time = 0;
     while(true) {
-        move_dir %= 4;
-        pair<int,int> head = dq.front(); // 현재 뱀의 머리 좌표 == q.front()
-        board[head.first][head.second] = 1; // 현재 좌표에 뱀의 몸통 표식을 채우기
-        second++;
+        time++;
+        auto head = dq.front();
+        int x = head.first;
+        int y = head.second;
+        board[x][y] = 1; 
 
-        int nx = head.first + dx[move_dir];
-        int ny = head.second + dy[move_dir];
+        int nx = x + dx[moveDir];
+        int ny = y + dy[moveDir];
 
-        // 벽을 만났거나 뱀 자기자신의 몸과 만난경우 
-        if(nx < 1 || nx > n || ny < 1 || ny > n || board[nx][ny] == 1) {
+        // 범위를 벗어난 경우
+        if(nx < 1 || nx > n || ny < 1 || ny > n) {
+            break;
+        }
+        // 뱀의 본인 몸통 자신을 만나서 충돌한 경우
+        if(board[nx][ny] == 1) {
             break;
         }
 
-        // 사과가 없는 좌표를 지나간다면
+        // 이동한 좌표에 사과가 없다면, 꼬리를 잘라낸다.
         if(board[nx][ny] != -1) {
-            // 꼬리를 자름
-            auto tail = dq.back();
-            board[tail.first][tail.second] = 0; 
-            dq.pop_back();                
+            auto tail = dq.back(); // 꼬리 좌표를 추출
+            // 꼬리 제거
+            board[tail.first][tail.second] = 0;  
+            dq.pop_back();
         }
-        dq.push_front({nx, ny});
 
-        auto sd = switchDir.front();
-        if(sd.first == second) {
-            if(sd.second == 'D') {
-                move_dir += 3;
-            } else if(sd.second == 'L') {
-                move_dir += 1;
+        auto dir = switchDir.front();
+        if(dir.first == time) {
+            char command = dir.second;
+            if(command == 'D') {
+                moveDir = (moveDir + 3) % 4;
+            }
+            if(command == 'L') {
+                moveDir = (moveDir + 1) % 4;
             }
             switchDir.pop();
         }
+        dq.push_front({nx, ny}); // 뱀의 머리를 최신화. 현재 새롭게 이동한 좌표로 최신화한다.
     }
-
-    cout << second;
+    
+    cout << time;
 
     return 0;
 }
