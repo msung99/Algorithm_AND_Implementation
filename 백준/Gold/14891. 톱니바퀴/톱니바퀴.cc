df4b4f6642222e784eb847867136fed34321428d
Haon <88240193+msung99@users.noreply.github.com>
2024-05-13 19:54:28 +0900

    [Gold V] Title: 톱니바퀴, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.cc
@@ -3,59 +3,62 @@ using namespace std;
 
 string board[4];
 
-void func(int num, int dir) {
-    int dirs[4] = {};
-    dirs[num] = dir;
+void go(int num, int dir) {
+  int dirs[4] = {};
+  num--;
+  dirs[num] = dir;
 
-    int idx = num;
-    // 왼쪽으로 회전 전파
-    while(idx > 0 && board[idx-1][2] != board[idx][6]) {
-        dirs[idx-1] = -dirs[idx];
-        idx--;
+  // 왼쪽으로 회전 전파
+  for(int idx = num; idx > 0; idx--) {
+    if(board[idx][6] == board[idx-1][2]) {
+      break;
     }
+    dirs[idx-1] = -dirs[idx];
+  }
 
-    idx = num;
-    // 오른쪽으로 회전 전파
-    while(idx < 3 && board[idx][2] != board[idx+1][6]) {
-        dirs[idx+1] = -dirs[idx];
-        idx++;
+  // 오른쪽으로 회전 전파
+  for(int idx = num; idx < 3; idx++) {
+    if(board[idx][2] == board[idx+1][6]) {
+      break;
     }
+    dirs[idx+1] = -dirs[idx];
+  }
 
-    for(int i=0; i<4; i++) {
-        if(dirs[i] == -1) {
-            rotate(board[i].begin(), board[i].begin() + 1, board[i].end());
-        }
-        else if(dirs[i] == 1) {
-            rotate(board[i].begin(), board[i].begin() + 7, board[i].end());
-        }
+  for(int i=0; i<4; i++) {
+    if(dirs[i] == 1) { // 시계방향 회전
+      rotate(board[i].begin(), board[i].begin() + 7, board[i].end());
+    } 
+    else if(dirs[i] == -1) { // 반시계방향 회전
+      rotate(board[i].begin(), board[i].begin() + 1, board[i].end());
     }
+  }
 }
 
 int main(void)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-    for(int i=0; i<4; i++) {
-        cin >> board[i];
+  for(int i=0; i<4; i++) {
+    cin >> board[i];
+  }
+  
+  int total = 0;
+  int k;
+  cin >> k;
+  while(k--) {
+    int num, dir;
+    cin >> num >> dir;
+    go(num, dir);
+  }
+
+  for(int i=0; i<4; i++) {
+    if(board[i][0] == '1') {
+      total += (1 << i);
     }
+  }
+  cout << total;
 
-    int k;
-    cin >> k;
-    while(k--) {
-        int num, dir;
-        cin >> num >> dir;
-        func(num -1, dir);
-    }
-
-    int ans = 0;
-    for(int i=0; i<4; i++) {
-        if(board[i][0] == '1') {
-            ans += (1 << i);
-        }
-    }
-    cout << ans;
-
-    return 0;
+  return 0;
 }
