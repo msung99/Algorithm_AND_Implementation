46af4578ef3feec17311bb49bb5148873e5ea94a
Haon <88240193+msung99@users.noreply.github.com>
2024-05-13 14:28:58 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -4,8 +4,8 @@ using namespace std;
 int n;
 int num[12];
 int ops[12];
-int mn = 2100000000;
-int mx = -2100000000;
+int mn = 1999999999;
+int mx = -1999999999;
 
 int main(void)
 {
@@ -14,38 +14,34 @@ int main(void)
   cout.tie(0);
 
   cin >> n;
-
   for(int i=0; i<n; i++) {
     cin >> num[i];
   }
 
-  for(int op=0, idx =1; op<4; op++) {
+  for(int op=0, idx=1; op<4; op++) {
     int x;
     cin >> x;
     while(x--) {
       ops[idx++] = op;
     }
   }
-
+  
   do {
     int res = 0;
     for(int i=0; i<n; i++) {
       if(ops[i] == 0) {
         res += num[i];
-      } 
-      else if(ops[i] == 1) {
+      } else if(ops[i] == 1) {
         res -= num[i];
-      }
-      else if(ops[i] == 2) {
+      } else if(ops[i] == 2) {
         res *= num[i];
-      }
-      else if(ops[i] == 3) {
+      } else if(ops[i] == 3) {
         res /= num[i];
       }
     }
     mx = max(res, mx);
     mn = min(res, mn);
-  } while(next_permutation(ops+ 1, ops + n));
+  } while(next_permutation(ops+1, ops+n));
 
   cout << mx << "\n";
   cout << mn;
