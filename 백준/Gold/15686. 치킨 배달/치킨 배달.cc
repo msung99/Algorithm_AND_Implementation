57f6e1eba35eddf7f11744743f65bc5cd813fff2
Haon <88240193+msung99@users.noreply.github.com>
2024-05-13 19:03:29 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -2,49 +2,52 @@
 using namespace std;
 
 int n, m;
-vector<pair<int,int>> chicken;
 vector<pair<int,int>> house;
+vector<pair<int,int>> chicken;
 
 int main(void)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-    cin >> n >> m;
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            int data;
-            cin >> data;
-            if(data == 1) {
-                house.push_back({i, j});
-            }
-            if(data == 2) {
-                chicken.push_back({i, j});
-            }
+  cin >> n >> m;
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<n; j++) {
+      int data;
+      cin >> data;
+      if(data == 1) {
+        house.push_back({i, j});
+      }
+      if(data == 2) {
+        chicken.push_back({i, j});
+      }
+    }
+  }
+
+  vector<int> brute(chicken.size(), 1);
+  fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
+
+  int mn = 1999999999;
+  do {
+    int cur = 0;
+    for(auto h : house) { // 각 집에대해 현재 부여된 m개의 치킨집중에 적절히 선택하여 최소 치킨거리 값을 계산 
+      int houseMin = 199999999;
+
+      for(int i=0; i<brute.size(); i++) {
+        if(brute[i] == 1) {
+          houseMin = min(houseMin, abs(h.first - chicken[i].first) + abs(h.second - chicken[i].second));          
         }
+      }
+
+      cur += houseMin;
     }
 
-    vector<int> brute(chicken.size(), 1);
-    fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
-    int ans = 999999999;
+    mn = min(cur, mn);
 
-    do {
-        int total = 0;
-        for(auto h : house) {
-            int dist = 9999999;
-            for(int i=0; i<chicken.size(); i++) {
-                if(brute[i] == 1) {
-                    int cur = abs(h.first - chicken[i].first) + abs(h.second - chicken[i].second);
-                    dist = min(cur, dist);
-                }
-            }
-            total += dist;
-        }
-        ans = min(ans, total);
-    } while(next_permutation(brute.begin(), brute.end()));
+  } while(next_permutation(brute.begin(), brute.end()));
 
-    cout << ans;
+  cout << mn;
 
-    return 0;
+  return 0;
 }
