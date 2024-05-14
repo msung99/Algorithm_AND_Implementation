7b7fed010835d9475a80281411d6b5ba53267b5b
Haon <88240193+msung99@users.noreply.github.com>
2024-05-15 02:07:51 +0900

    [Silver II] Title: 제곱수의 합, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -6,19 +6,19 @@ int d[100002];
 
 int main(void)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
 
-    cin >> n;
-    for(int i=1; i<=n; i++) {
-        d[i] = i;
-        for(int j=1; j*j<=i; j++) {
-            d[i] = min(d[i], d[i - j*j] + 1);
-        }
+  cin >> n;
+  for(int i=1; i<=n; i++) {
+    d[i] = i;
+    for(int j=1; j*j<=i; j++) {
+      d[i] = min(d[i], d[i - j*j] + 1);
     }
+  }
 
-    cout << d[n];
+  cout << d[n];
 
-    return 0;
+  return 0;
 }
