83cff04b949709f9ff52d403d2be1f671de2a1db
Haon <88240193+msung99@users.noreply.github.com>
2024-05-14 19:44:23 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -2,9 +2,9 @@
 using namespace std;
 
 int n;
-int t[1002];
-int p[1002];
-int d[1002];
+int d[20]; // i번째 날에 상담을 진행했을 때 얻을 수 있는 최대 "누적 " 수익
+int t[20];
+int p[20];
 
 int main(void)
 {
@@ -13,25 +13,23 @@ int main(void)
   cout.tie(0);
 
   cin >> n;
-
   for(int i=1; i<=n; i++) {
     cin >> t[i] >> p[i];
   }
 
-  // 역순으로 순회
-  for(int i=n; i >=1; i--) {
-    // 상담이 가능한 날짜라면
+  for(int i=n; i>0; i--) {
+    // 상담이 진행 가능한 날짜인 경우
     if(i + t[i] <= n+1) {
-      d[i] = max(d[i+1], d[i + t[i]] + p[i]); 
-      // d[i+1] : 상담을 하지 않았을 때 최대 누적 수익 / d[i +t[i]] + p[i] : 상담했을 떄의 누적 수익
+      // i + t[i] 번째 날에 상담을 진행했을 때와, 상담을 진행하지 않았을때 얻을 수 있는 최대 "누적" 수익 중에 최댓값을 취함
+      d[i] = max(d[i + t[i]] + p[i], d[i+1]);
     } 
-    // 상담이 불가능하다면
+    // 상담이 진행 불가능한 날짜인 경우
     else {
       d[i] = d[i+1];
     }
   }
 
-  cout << *max_element(d + 1, d + n + 1);
+  cout << *max_element(d+1, d+n+1);
 
   return 0;
 }
