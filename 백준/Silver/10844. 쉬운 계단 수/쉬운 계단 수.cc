8902d300e76024f61bece5413c531c9956e23227
Haon <88240193+msung99@users.noreply.github.com>
2024-05-14 21:22:57 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,12 +1,11 @@
+// d[i][j] : i번쨰 수로 숫자 j를 선택하는 경우의 수 
+// d[i][j] = d[i-1][j+1] + d[i-1][j-1]
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int n;
-int d[102][10];
-// d[i][j] : 길이 i를 가지며 맨 끝 자릿수(일의 자리 수)가 j로 끝나는 수의 가짓수
-
-// d[i][j] = d[i-1][j+1] + d[i-1][j-1]
-// d[[2][2] = d[1][3] + d[1][[1]  => d[2][2] : 길이 2지를 가지며 맨 끝수가 1인 숫자의 경우의 수
+int d[102][102];
 
 int main(void)
 {
@@ -15,6 +14,7 @@ int main(void)
   cout.tie(0);
 
   cin >> n;
+  int cnt = 0;
 
   for(int i=1; i<=9; i++) {
     d[1][i] = 1;
@@ -22,30 +22,21 @@ int main(void)
 
   for(int i=2; i<=n; i++) {
     for(int j=0; j<=9; j++) {
-      // 끝 자리수가 0일때 특이 케이스 처리 : 끝 자리수의 바로 다음 자릿수(끝에서 2번쨰 자릿수) 로 반드시 1만 올 수 있음
-      if(j == 0) {
-        d[i][0] = d[i-1][1];
+      if(j != 0) {
+        d[i][j] += d[i-1][j-1];
       }
-
-      // 끝 자리수가 9일때 특이 케이스 처리 : 끝 자리수의 바로 다음 자릿수(끝에서 2번쨰 자릿수) 로 반드시 8만 올 수 있음
-      else if(j == 9) {
-        d[i][9] = d[i-1][8];
+      if(j != 9) {
+        d[i][j] += d[i-1][j+1];
       }
-
-      // 끝 자리수가 2~8일때
-      else {
-        d[i][j] = d[i-1][j+1] + d[i-1][j-1];
-      }
-      
       d[i][j] %= 1000000000;
     }
   }
 
-  int result =0;
+  long long res = 0;
   for(int i=0; i<=9; i++) {
-    result = (result + d[n][i]) % 1000000000;
+    res += d[n][i];
   }
-  cout << result;
+  cout << res % 1000000000;
 
   return 0;
 }
