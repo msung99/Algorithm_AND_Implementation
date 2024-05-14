c664fe64db6e75f5aa8a7996dae716cae13eb4ee
Haon <88240193+msung99@users.noreply.github.com>
2024-05-14 22:50:13 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5948 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,7 +1,10 @@
+// https://ongveloper.tistory.com/36
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int d[1002][1002];
+string str1, str2;
+int d[1002][1002]; // d[i][j] : str1 의 i행까지의 문자열과, str2 의 j열까지의 문자열 사이의 LCS 길이
 
 int main(void)
 {
@@ -9,14 +12,14 @@ int main(void)
   cin.tie(0);
   cout.tie(0);
 
-  string str1, str2;
-  cin >> str1 >> str2;
+  cin >> str1;
+  cin >> str2;
 
-  int size1 = str1.length();
-  int size2 = str2.length();
+  int len1 = str1.length();
+  int len2 = str2.length();
 
-  for(int i=1; i<=size1; i++) {
-    for(int j=1; j<=size2; j++) {
+  for(int i=1; i<=len1; i++) {
+    for(int j=1; j<=len2; j++) {
       if(str1[i-1] == str2[j-1]) {
         d[i][j] = d[i-1][j-1] + 1;
       } else {
@@ -25,7 +28,7 @@ int main(void)
     }
   }
 
-  cout << d[size1][size2];
+  cout << d[len1][len2];
 
   return 0;
 }
