96e44730eafff5acc8a999f6718803c09ed9bd72
Haon <88240193+msung99@users.noreply.github.com>
2024-05-14 17:17:20 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -2,8 +2,8 @@
 using namespace std;
 
 int n;
-int arr[100002]; // 입력값
-int d[100002]; // d[i] : i번째 항으로 끝나는 연속합 중에 최댓값
+int arr[100002];
+int d[100002]; // d[i] : i번째 항까지의 연속합 중에 최댓값
 
 int main(void)
 {
@@ -12,16 +12,15 @@ int main(void)
   cout.tie(0);
 
   cin >> n;
-
-  for(int i=1; i<=n; i++) {
+  for(int i=0; i<n; i++) {
     cin >> arr[i];
   }
-
-  for(int i=1; i<=n; i++) {
+  
+  for(int i=0; i<n; i++) {
     d[i] = max(0, d[i-1]) + arr[i];
-    // max(0, d[i-1]) => d[i-1] 이 양수라면 이득이므로 더한다. 반대로 음수이므로 더하지 않고 그대로 냅둔다. (= 0을 더한다)
   }
+  
+  cout << *max_element(d, d + n);
 
-  cout << *max_element(d + 1, d + n + 1);
   return 0;
 }
