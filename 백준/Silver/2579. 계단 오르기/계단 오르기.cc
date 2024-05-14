3e9de4d3b69fa3048040db6da6b7bedfadfe87a1
Haon <88240193+msung99@users.noreply.github.com>
2024-05-14 16:18:33 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,12 +1,12 @@
-// d[i][j] : 현재까지 j개의 계단을 연속해서 밟고 i번째 계단을 연속해서 밟고 i번째 계단까지 올라섰을 때 점수 합의 최댓값, 단 i번째 계단은 반드시 밟아야 함.
-// d[i][1] = max(d[i-2][1], d[i-2][2]) + arr[i]
-// d[i][2] = d[i-1][1] + arr[i]
+
+// d[i][j] :현재까지 j개의 계단을 연속해서 밟고 i번째 계단까지 올라섰을 때 점수 합의 최댓값. 
+// 단, i번째 계단을 반드시 밟아야 함
 #include <bits/stdc++.h>
 using namespace std;
 
 int n;
-int d[302][3];
 int arr[302];
+int d[302][3];
 
 int main(void)
 {
@@ -15,21 +15,16 @@ int main(void)
   cout.tie(0);
 
   cin >> n;
-  for(int i=1; i<=n; i++) {
+  for(int i=0; i<n; i++) {
     cin >> arr[i];
   }
 
-  d[1][1] = arr[1];
-  d[1][2] = 0;
-  d[2][1] = arr[2];
-  d[2][2] = arr[1] + arr[2];
-
-  for(int i=3; i<=n; i++) {
+  for(int i=0; i<n; i++) {
     d[i][1] = max(d[i-2][1], d[i-2][2]) + arr[i];
     d[i][2] = d[i-1][1] + arr[i];
   }
 
-  cout << max(d[n][1], d[n][2]);
+  cout << max(d[n-1][1], d[n-1][2]);
 
   return 0;
 }
