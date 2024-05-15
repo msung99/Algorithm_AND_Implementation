66a04ed10ec1552df6fa7af953fdd13a1249bb59
Haon <88240193+msung99@users.noreply.github.com>
2024-05-15 14:29:22 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.cc
+++ 백준/Gold/9084. 동전/동전.cc
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int t, n, m;
-int d[10002]; 
-int coin[22];
+int t;
+int n;
+int money;
+int d[10002];
+int coin[10002];
 
 int main(void)
 {
@@ -14,21 +16,20 @@ int main(void)
     cin >> t;
     while(t--) {
         fill(d, d + 10001, 0);
-        d[0] = 1; // 동전 0원을 만드는 경우의 수는 1가지
-        
         cin >> n;
+        d[0] = 1;
         for(int i=0; i<n; i++) {
             cin >> coin[i];
         }
-
-        cin >> m;
+        cin >> money;
 
         for(int i=0; i<n; i++) {
-            for(int j=coin[i]; j<=m; j++) {
+            for(int j=coin[i]; j<=money; j++) {
                 d[j] += d[j - coin[i]];
             }
         }
-        cout << d[m] << "\n";
+        cout << d[money] << "\n";
     }
+
     return 0;
 }
