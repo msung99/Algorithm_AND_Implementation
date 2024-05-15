810bd6a8b62f5124bcc404475a464800de30cec3
Haon <88240193+msung99@users.noreply.github.com>
2024-05-15 22:24:38 +0900

    [Gold V] Title: 회장뽑기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.cc
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.cc
@@ -2,70 +2,64 @@
 using namespace std;
 
 int n;
-vector<int> adj_list[55];
-int dist[55];
-int score[55];
+vector<int> adj_list[52];
+int dist[52];
+int scores[52];
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-  cin >> n;
+    cin >> n;
 
-  while(true) {
-    int u, v;
-    cin >> u >> v;
-
-    if(u == -1 && v == -1) {
-      break;
+    while(true) {
+        int u, v;
+        cin >> u >> v;
+        if(u == -1 && v == -1) {
+            break;
+        }
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
     }
 
-    adj_list[u].push_back(v);
-    adj_list[v].push_back(u);
-  }
+    for(int i=1; i<=n; i++) {
+        fill(dist, dist + n + 1, -1);        
 
-  for(int i=1; i<=n; i++) {
-    queue<int> q;
-    fill(dist, dist + n + 2, -1);
+        queue<int> q;
+        q.push(i);
+        dist[i] = 0;
+    
+        while(!q.empty()) {
+            int cur = q.front();
+            q.pop();
 
-    dist[i] = 0;
-    q.push(i);
-
-    while(!q.empty()) {
-      int cur = q.front();
-      q.pop();
-
-      for(int next : adj_list[cur]) {
-        if(dist[next] != -1) {
-          continue;
+            for(auto next : adj_list[cur]) {
+                if(dist[next] != -1) {
+                    continue;
+                }
+                dist[next] = dist[cur] + 1;
+                q.push(next);
+            }
         }
 
-        dist[next] = dist[cur] + 1;
-        q.push(next);
-      }
+        int score = *max_element(dist + 1, dist + n+1);
+        scores[i] = score;
     }
 
-    int minValue = *max_element(dist + 1, dist + n + 1);
-    score[i] = minValue;
-  }
+    int minValue = *min_element(scores + 1, scores + n+1);
+    vector<int> person;
+    for(int i=1; i<=n; i++) {
+        if(scores[i] == minValue) {
+            person.push_back(i);
+        }
+    }
 
-  int minScore = *min_element(score + 1, score + n + 1);
-  vector<int> personList;
-  int cnt = 0;
+    cout << minValue << ' ' << person.size() << "\n";
+    for(int i=0; i<person.size(); i++) {
+        cout << person[i] << ' ' ;
+    }
 
-  for(int i=1; i<=n; i++) {
-    if(score[i] == minScore) {
-      personList.push_back(i);      
-      cnt++;
-    }    
-  }
-
-  cout << minScore << ' ' << cnt << "\n";
-  for(int i=0; i<personList.size(); i++) {
-    cout << personList[i] << ' ';
-  }
-
-  return 0;
+    return 0;
 }
