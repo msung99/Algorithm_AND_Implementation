98a14447cb93352e12828aa586cfd15c656c43e9
Haon <88240193+msung99@users.noreply.github.com>
2024-05-15 23:52:32 +0900

    [Gold IV] Title: 구슬 찾기, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/2617. 구슬 찾기/구슬 찾기.cc
+++ 백준/Gold/2617. 구슬 찾기/구슬 찾기.cc
@@ -2,61 +2,62 @@
 using namespace std;
 
 int n, m;
-bool visited[101];
-vector<int> heavy[101]; // heavy[i] : i보다 무거운 구슬 리스트
-vector<int> light[101];
+bool visited[102];
+// 단방향 그래프 2개로 나눔
+vector<int> heavy[102]; // heavy[i] : i보다 무거운 구슬 리스트
+vector<int> light[102];
 
-bool solve(int start, vector<int> (&adj_list)[]) {
-  fill(visited, visited + n + 1, false);
+bool bfs(int st, vector<int> adj_list[102]) {
+    fill(visited, visited + n+1, false);
 
-  queue<int> q;
-  q.push(start);
-  visited[start] = true;
+    queue<int> q;
+    q.push(st);
+    visited[st] = true;
 
-  int mid = (n+1) / 2;
-  int cnt = 0;
+    int mid = (n+1) / 2;
+    int cnt = 0;
 
-  while(!q.empty()) {
-    int cur = q.front();
-    q.pop();
-
-    for(int next : adj_list[cur]) {
-      if(visited[next]) {
-        continue;
-      }
-      cnt++;
-      q.push(next);
-      visited[next] = true;
+    while(!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        for(int next : adj_list[cur]) {
+            if(visited[next]) {
+                continue;
+            }
+            cnt++;
+            q.push(next);
+            visited[next] = true;
+        }
     }
-  }
 
-  if(cnt >= mid) {
-    return true;
-  } 
-  return false;
+    if(cnt >= mid) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-  cin >> n >> m;
+    cin >> n >> m;
 
-  for(int i=0; i<m; i++) {
-    int u, v;
-    cin >> u >> v;
-    heavy[u].push_back(v);
-    light[v].push_back(u);
-  }
+    for(int i=0; i<m; i++) {
+        int u, v;
+        cin >> u >> v;
+        heavy[u].push_back(v);
+        light[v].push_back(u);
+    }
 
-  int ans = 0;
-  for(int i=1; i<=n; i++) {
-    int start = i;
-    ans += (solve(start, heavy) || solve(start, light));
-  }
+    int ans = 0;
+    for(int i=1; i<=n; i++) {
+        int st = i;
+        ans += (bfs(st, heavy) || bfs(st, light));
+    }
+    cout << ans;
 
-  cout << ans;
-  return 0;
+    return 0;
 }
