5b0c1163ef51fc020b6d5b4391f3dc27cbfcec4e
Haon <88240193+msung99@users.noreply.github.com>
2024-05-16 21:53:05 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 40 ms, Memory: 8448 KB -BaekjoonHub

--- 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.cc
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.cc
@@ -1,17 +1,16 @@
-// connected component 개수 - 1 = 정답
 #include <bits/stdc++.h>
 using namespace std;
 
 int n, m;
 vector<int> adj_list[100002];
-bool visited[100002];
+bool vistied[100002];
 
 void dfs(int cur) {
-    if(visited[cur]) {
+    if(vistied[cur]) {
         return;
     }
-    visited[cur] = true;
-    for(auto next : adj_list[cur]) {
+    vistied[cur] = true;
+    for(auto next :adj_list[cur]) {
         dfs(next);
     }
 }
@@ -23,25 +22,26 @@ int main(void)
     cout.tie(0);
 
     cin >> n >> m;
-
     for(int i=0; i<m; i++) {
         int u, v;
         cin >> u >> v;
-        
         adj_list[u].push_back(v);
         adj_list[v].push_back(u);
     }
 
-    int cnt = 0;
+    int groupCnt = 0;
     for(int i=1; i<=n; i++) {
-        if(visited[i]) {
+        if(vistied[i]) {
             continue;
         }
         dfs(i);
-        cnt++;
+        groupCnt++;
     }
 
-    cout << (cnt -1) + (m+cnt-1) - (n-1);
+    cout << (groupCnt -1) + (m+groupCnt-1) - (n-1);
+    // groupCnt = 연결 요소의 개수
+    // (groupCnt-1) 개의 간선을 추가해 주어진 그래프를 연결 그래프로 만들면,
+    // 현재 그래프에는 (m+groupCnt-1) 개의 간선이 있고 트리는 n-1개의 간선이 있기 때문애 (m+groupCnt-1) - (n-1)개의 간선을 제거해야 한다.
 
     return 0;
 }
