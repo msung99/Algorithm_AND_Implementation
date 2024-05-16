3205fbc98c7036610a69f67a7d5af50adbac54f2
Haon <88240193+msung99@users.noreply.github.com>
2024-05-16 17:23:51 +0900

    [Gold IV] Title: 트리, Time: 28 ms, Memory: 3088 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.cc
+++ 백준/Gold/4803. 트리/트리.cc
@@ -1,54 +1,80 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MXN = 510;
-vector<int> adj[MXN];
-bool vis[MXN], isTree;
-int n, m, u, v, tc, trees;
+int n, m;
+vector<int> adj_list[502];
+bool visited[502];
+int treeCnt;
+bool isTree;
+
 void dfs(int cur, int prev) {
-  for (int nxt : adj[cur]) {
-    if (nxt == prev) // next가 부모(prev)일 경우 건너뜀
-      continue;
-    if (vis[nxt]) { // 트리일 경우 자식은 반드시 이전에 방문한 적이 없었어야 하고, 이전에 방문을 했다면 현재 보고 있는 connected component가 트리가 아님을 의미
-      isTree = false;
-      continue;
+    for(int next : adj_list[cur]) {
+        // next가 부모(prev)일 경우 건너뜀
+        if(next == prev) {
+            continue;
+        }
+
+        // 트리일 경우 자식은 반드시 이전에 방문한 적이 없었어야 하고, 
+        // 이전에 방문을 했다면 현재 보고 있는 connected component가 사이클이 존재하여 트리가 아님을 의미.
+        if(visited[next]) {
+            isTree = false;
+            continue; // 주의: 트리가 아님이 판명되는 즉시 return 하는게 아니라, 같은 사이클 내에 존재하는
+            // 다른 모든 정점들에 대해 vistied 를 true 로 처리해줘야함. 이 작업을 위해 즉시 return 하지 않고 continue 처리함
+        }
+        visited[next] = true;
+        dfs(next, cur);
     }
-    vis[nxt] = true;
-    dfs(nxt, cur);
-  }
 }
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
 
-  while (1) {
-    cin >> n >> m;
-    if (!n && !m) break;
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-    fill(vis, vis + n + 1, 0);
-    for (int i = 1; i <= n; i++)
-      adj[i].clear();
-    trees = 0;
+    int round = 1;
+    while(true) {
+        cin >> n >> m;
 
-    while (m--) {
-      cin >> u >> v;
-      adj[u].push_back(v);
-      adj[v].push_back(u);
+        if(n == 0 && m == 0) {
+            break;
+        }
+
+        fill(visited + 1, visited+n+1, false);
+        for(int i=1; i<=n; i++) {
+            adj_list[i].clear();
+        }
+
+        for(int i=0; i<m; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj_list[u].push_back(v);
+            adj_list[v].push_back(u);
+        }        
+
+        for(int i=1; i<=n; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                isTree = true;
+                dfs(i, -1);
+                
+                if(isTree) {
+                    treeCnt++;
+                }
+            }
+        }
+
+        cout << "Case " << round << ": ";
+        if(treeCnt == 0) {
+            cout << "No trees.\n";
+        } else if(treeCnt == 1) {
+            cout << "There is one tree.\n";
+        } else {
+            cout << "A forest of " << treeCnt << " trees.\n";
+        }
+        treeCnt = 0;
+        round++;
     }
-    for (int i = 1; i <= n; i++) {
-      if (vis[i]) continue;
-      vis[i] = true;
-      isTree = true;
-      dfs(i, -1);
-      trees += isTree;
-    }
-    cout << "Case " << ++tc << ": ";
-    if (!trees)
-      cout << "No trees." << '\n';
-    else if (trees == 1)
-      cout << "There is one tree." << '\n';
-    else
-      cout << "A forest of " << trees << " trees." << '\n';
-  }
+
+    return 0;
 }
