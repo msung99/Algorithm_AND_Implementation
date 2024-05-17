ad690e73513c01aeac101ee00d1e8759fe2ec9df
Haon <88240193+msung99@users.noreply.github.com>
2024-05-17 23:28:43 +0900

    [Gold IV] Title: 세 수의 합, Time: 56 ms, Memory: 8300 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -1,42 +1,45 @@
-// a[i] + a[j] + a[k] = a[l] 을 만족하는 a[l] 중에서 최댓값
-
-// 배열 two : 미리 배열 a에서 두 원소의 합을 다 묶어놓은 배열. 즉, a[i], a[j], a[k] 중에서 a[i] 와 a[j] 를 미리 합으로 묶어놓은 배열이다.
-// 예를들어 a가 1,4,6 이라고 하면 two 는  1+1, 1+4, 4+4, 4+6, 6+6 = 2,5,7,8,10,12 가 된다.
-
-// two[m] + a[k] = a[l] 을 만족하는 a[l] 중에서 최댓값
-// => O(n^2 x logN) 풀이 : k, l 에 대한 2중 for문을 돌리고, a[l] - a[k] 가 배열 two 에 있는지 이분탐색 log(N^2) = 2xlogN
-
+// 이 문제처럼 뭔가 2개를 묶고나서 어느 한쪽의 값을 이분탐색으로 찾아서 시간복잡도를 낮추는 아이디어는 이분탐색 응용 문제에서 많이 등장함
 
 #include <bits/stdc++.h>
 using namespace std;
 
+// a[i] + a[j] + a[k] = a[l] 을 만족하는 a[l] 중에서 최댓값
+// two[m] + a[k] = a[l] 을 만족하는 a[l] 중에서 최댓값
+
 int n;
-int a[1005];
-vector<int > two;
+int arr[1005];
+vector<int> two;
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	for (int i = 0; i < n; i++) 
-		cin >> a[i];
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
 
-	sort(a, a + n);
-	// 배열 two : 미리 배열 a에서 두 원소의 합을 다 묶어놓은 배열.
-	for (int i = 0; i < n; i++) {
-		for (int j = i; j < n; j++)
-			two.push_back(a[i] + a[j]);
-	}
-	sort(two.begin(), two.end()); // 이분탐색을 진행하기 위해 정렬도 시켜놓는다.
+    sort(arr, arr + n);
 
-	for (int i = n - 1; i > 0; i--) {
-		for (int j = 0; j < i; j++) {
-			if (binary_search(two.begin(), two.end(), a[i] - a[j])) { // 배열 two 를 이분탐색하면서 a[l] - a[k] 가 존재하는지 탐색
-				cout << a[i]; // 존재하면 해당 값을 출력
-				return 0;
-			}
-		}
-	}
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            two.push_back(arr[i] + arr[j]);
+        }
+    }
+
+    sort(two.begin(), two.end());
+
+    // arr[i] - arr[j] 값이 two 배열에 존재하는지 확인
+    for(int i=n-1; i>0; i--) {
+        for(int j=0; j<i; j++) {
+            if(binary_search(two.begin(), two.end(), arr[i] - arr[j])) {
+                cout << arr[i];
+                return 0;
+            }
+        }
+    }
+
+    return 0;
 }
