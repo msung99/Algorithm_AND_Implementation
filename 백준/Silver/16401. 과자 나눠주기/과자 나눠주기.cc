174da600c6e9bf42f12a1434dc30b22ab420286e
Haon <88240193+msung99@users.noreply.github.com>
2024-05-18 14:10:52 +0900

    [Silver II] Title: 과자 나눠주기, Time: 208 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.cc
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.cc
@@ -1,48 +1,44 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	//입력
-	int M, N;
-	vector<int> L;
-	cin >> M >> N;
-	for (int i = 0; i < N; i++) {
-		int l;
-		cin >> l;
-		L.push_back(l);
-	}
+    int m, n;
+    cin >> m >> n;
 
-	//이분탐색, 이진탐색
-	int left = 1, right = *max_element(L.begin(), L.end());
-	//left: 가장 작은 수, right: 가장 큰 수
-	int snack = 0;
+    vector<int> l;
 
-    // m명의 조카에게 길이가 mid 인 과자를 나눠줄떄 몇명(count) 에게 나눠줄 수 있는지 구함
-	while (left <= right) {
-		int mid = (left + right) / 2; //중앙값
-		int count = 0;
-		for (int i = 0; i < N; i++) {
-			count += L[i] / mid; //mid 과자 길이로 나눠줄수 있는 양
-            // ex) mid = 8 일때
-            // (1/8) + (2/8) + (3/8) + ... (8/8) + (9/8) + (10/8)
-            // = 0+0+0+...+0+1+1+1 = 3
-		}
-		if (count >= M) { //과자 나눠줄수 있을 때
-			left = mid + 1;
-			snack = mid;
-		}
-		else if (count < M) { //과자 나눠줄수 없을 때
-			right = mid - 1;
-		}
-	}
-	cout << left-1 << "\n";
+    for(int i=0; i<n; i++) {
+        int len;
+        cin >> len;
+        l.push_back(len);
+    }
 
-	return 0;
+    int left = 1;
+    int right = *max_element(l.begin(), l.end());
+    int snack = 0;
+
+    // 길이가 mid 인 과자를 나눠줄 떄 몇명(count) 에게 나눠줄 수 있는지를 구함
+    while(left <= right) {
+        int mid = (left + right) / 2;
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            count += l[i] / mid; // 각 과자에 대해 카운팅
+        }
+
+        if(count >= m) { // m명 이상에게 길이가 Mid 인 과자를 나눠줄 수 있다면
+            left = mid + 1;
+            snack = mid;
+        }
+        else if(count < m) {
+            right = mid - 1;
+        }
+    }
+    cout << left - 1;
+
+    return 0;
 }
