438c5351161348da19ef64b63b4c36b58518661e
Haon <88240193+msung99@users.noreply.github.com>
2024-05-19 01:57:29 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,5 +1,10 @@
+// 문제 조건: "연속해서 두 개 이상의 연산자가 나타나지 않고", "식의 값을 최소로 만드는"
+// '-' 뒤에 오는 식을 전부 뺄셈 처리하면 식의 값이 최소가 된다.
 #include <bits/stdc++.h>
 using namespace std;
+
+int sign = 1;
+int tmp, ans;
 
 int main(void)
 {
@@ -7,28 +12,23 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
 
-    int sign = 1;
-    string str;
-    cin >> str;
+    string s;
+    cin >> s;
 
-    int ans = 0;
-    int tmp = 0;
-    for(int i=0; i<str.size(); i++) {
-        if(str[i] == '+' || str[i] == '-') {
-            ans += tmp * sign;             
-            if(str[i] == '-') {
+    for(char c : s) {
+        if(c == '+' || c == '-') {
+            ans += sign * tmp;
+
+            // '-' 뒤에 오는 식을 전부 뺄셈 처리 해버리기 위해 sign = -1 로 셋팅
+            if(c == '-') {
                 sign = -1;
             }
             tmp = 0;
         } else {
             tmp *= 10;
-            tmp += str[i] - '0';
+            tmp += c - '0';
         }
     }
-
     ans += tmp * sign;
-
     cout << ans;
-
-    return 0;
 }
