fc425faceb8d0b257da6a6a17f3c181856e455cb
Haon <88240193+msung99@users.noreply.github.com>
2024-05-19 01:31:17 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -1,8 +1,9 @@
+// https://beginnerdeveloper-lit.tistory.com/36
 #include <bits/stdc++.h>
 using namespace std;
 
 int n;
-vector<int> arr;
+int arr[100002];
 
 int main(void)
 {
@@ -11,21 +12,20 @@ int main(void)
     cout.tie(0);
 
     cin >> n;
-
     for(int i=0; i<n; i++) {
-        int data;
-        cin >> data;
-        arr.push_back(data);
+        cin >> arr[i];
     }
+    sort(arr, arr + n);
 
-    sort(arr.begin(), arr.end());
+    int mx = arr[n-1]; // 우선 최댓값을 가장 큰 로프 하나만 이용했을 때로 초기화
+    int cur;
 
-    int ans = 0;
-
-    for(int i=1; i<=n; i++) {
-        ans = max(ans, arr[n-i] * i);
+    for(int i=n-1; i >= 0; i--) {
+        // 현재 로프를 n-i 개 사용했으며, 사용하는 로프들 중에 i번째 루프가 들 수 있는 중량이 가장 작은 루프임
+        cur = arr[i] * (n-i); // 현재 로프의 중량 ( * 몇 개 달았는지)
+        mx = max(mx, cur);
     }
-    cout << ans;
+    cout << mx;
 
     return 0;
 }
