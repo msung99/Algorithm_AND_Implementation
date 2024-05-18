23fbcfad25d586f9551d487138f9fb6cea217f11
Haon <88240193+msung99@users.noreply.github.com>
2024-05-18 13:57:28 +0900

    [Silver II] Title: 과자 나눠주기, Time: 208 ms, Memory: 8292 KB -BaekjoonHub

--- 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.cc
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.cc
@@ -1,45 +1,48 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int n, m;
-int arr[1000002];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-// 막대 과자의 길이가 len일 경우 m조각 이상이 나오는가?
-bool solve(int len) {
-    if(len == 0) {
-        return true;
-    }
-    long long cnt = 0;
-    for(int i=0; i<n; i++) {
-        cnt += arr[i] / len;
-    }
-    return cnt >= m;
-}
+	//입력
+	int M, N;
+	vector<int> L;
+	cin >> M >> N;
+	for (int i = 0; i < N; i++) {
+		int l;
+		cin >> l;
+		L.push_back(l);
+	}
 
-int main(void)
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	//이분탐색, 이진탐색
+	int left = 1, right = *max_element(L.begin(), L.end());
+	//left: 가장 작은 수, right: 가장 큰 수
+	int snack = 0;
 
-    cin >> m >> n;
-    for(int i=0; i<n; i++) {
-        cin >> arr[i];
-    }
+    // m명의 조카에게 길이가 mid 인 과자를 나눠줄떄 몇명(count) 에게 나눠줄 수 있는지 구함
+	while (left <= right) {
+		int mid = (left + right) / 2; //중앙값
+		int count = 0;
+		for (int i = 0; i < N; i++) {
+			count += L[i] / mid; //mid 과자 길이로 나눠줄수 있는 양
+            // ex) mid = 8 일때
+            // (1/8) + (2/8) + (3/8) + ... (8/8) + (9/8) + (10/8)
+            // = 0+0+0+...+0+1+1+1 = 3
+		}
+		if (count >= M) { //과자 나눠줄수 있을 때
+			left = mid + 1;
+			snack = mid;
+		}
+		else if (count < M) { //과자 나눠줄수 없을 때
+			right = mid - 1;
+		}
+	}
+	cout << left-1 << "\n";
 
-    int st = 0;
-    int en = *max_element(arr, arr + n);
-
-    while(st < en) {
-        int mid = (st + en + 1) / 2;
-        if(solve(mid)) {
-            st = mid;
-        } else {
-            en = mid - 1;
-        }
-    }
-
-    cout << st << "\n";
-
-    return 0;
+	return 0;
 }
