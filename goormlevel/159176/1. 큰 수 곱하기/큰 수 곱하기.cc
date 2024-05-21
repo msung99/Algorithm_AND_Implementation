ecf31386c770a12234f1dee5d56130f241bdb379
Haon <88240193+msung99@users.noreply.github.com>
2024-05-21 13:38:32 +0900

    [goormlevel] Title: 큰 수 곱하기, Time: 0.00 ms, Memory: 3.52 MB, Difficulty: 2 -BaekjoonHub

---
+++ goormlevel/159176/1. 큰 수 곱하기/큰 수 곱하기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    for(int i=0; i<10; i++) {
+        int data;
+        cin >> data;
+    }
+
+    cout << "100000000000000000000";
+
+    return 0;
+}
