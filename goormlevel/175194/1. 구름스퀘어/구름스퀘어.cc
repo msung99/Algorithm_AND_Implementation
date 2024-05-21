3642e6f4c61abd4037bb3d453f908f05761d126e
Haon <88240193+msung99@users.noreply.github.com>
2024-05-21 12:20:20 +0900

    [goormlevel] Title: 구름스퀘어, Time: 0.02 ms, Memory: 3.95 MB, Difficulty: 2 -BaekjoonHub

---
+++ goormlevel/175194/1. 구름스퀘어/구름스퀘어.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+pair<int,int> arr[200002];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+
+    for(int i=0; i<n; i++) {
+        cin >> arr[i].second >> arr[i].first;
+    }
+
+    sort(arr, arr + n);
+
+    int cnt = 0;
+    int time = 0;
+    for(int i=0; i<n; i++) {
+        if(time + 1 <= arr[i].second) {
+            time = arr[i].first;
+            cnt++;
+        }        
+    }
+    cout << cnt;
+
+    return 0;
+}
