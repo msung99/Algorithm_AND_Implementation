1e3f7f287d796960d252eb3dbb6db994286ceb93
Haon <88240193+msung99@users.noreply.github.com>
2024-06-14 17:09:29 +0900

    [goormlevel] Title: 폭탄 구현하기 (2), Time: 0.03 ms, Memory: 3.74 MB, Difficulty: 2 -BaekjoonHub

--- goormlevel/195691/1. 폭탄 구현하기 （2）/폭탄 구현하기 （2）.cc
+++ goormlevel/195691/1. 폭탄 구현하기 （2）/폭탄 구현하기 （2）.cc
@@ -2,64 +2,57 @@
 using namespace std;
 
 int n, k;
-char board[202][202];
-int dist[202][202];
+char board[502][502];
+int dist[502][502];
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {-1, 0, 1, 0};
 
 int main(void)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    cin >> n >> k;
-
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            cin >> board[i][j];
-        }
-    }
-
-    while(k--) {
-        int x, y;
-        cin >> x >> y;
-
-        if(board[x][y] == '#') {
-            
-        } else if(board[x][y] == '@') {
-            dist[x][y] += 2;
-        } else {
-            dist[x][y] += 1;
-        }
-
-
-        for(int i=0; i<4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(nx <= 0 || nx > n || ny <= 0 || ny > n) {
-                continue;
-            }
-
-            if(board[nx][ny] == '#') {
-                continue;
-            } else if(board[nx][ny] == '@') {
-                dist[nx][ny] += 2;
-            } else {
-                dist[nx][ny] += 1;
-            }
-        }
-    }
-
-    int mx = 0;
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            mx = max(mx, dist[i][j]);
-        }
-    }
-    cout << mx;
-
-
-    return 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	cin >> n >> k;
+	
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n; j++) {
+			cin >> board[i][j];
+		}
+	}
+	
+	while(k--) {
+		int x, y;
+		cin >> x >> y;
+		
+		if(board[x][y] == '0') {
+				dist[x][y] += 1;
+		}
+		
+		if(board[x][y] == '@') {
+			dist[x][y] += 2;
+		}
+		
+		for(int i=0; i<4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(board[nx][ny] == '0') {
+				dist[nx][ny] += 1;
+			}
+			if(board[nx][ny] == '@') {
+				dist[nx][ny] += 2;
+			}
+		}
+	}
+	
+	int mx = -1999999999;
+	for(int i=1; i<=n; i++) {
+		for(int j=1; j<=n; j++) {
+			mx = max(mx, dist[i][j]);
+		}
+	}
+	cout << mx;
+	
+	return 0; 
 }
