09f3c5208fca1b683d52dbd748c6fe0f8cf5b962
Haon <88240193+msung99@users.noreply.github.com>
2024-06-14 15:59:49 +0900

    [goormlevel] Title: 통증, Time: 0.00 ms, Memory: 3.44 MB, Difficulty: 2 -BaekjoonHub

--- goormlevel/195690/1. 통증/통증.cc
+++ goormlevel/195690/1. 통증/통증.cc
@@ -5,17 +5,28 @@ int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cout.tie(0);
+	cout.tie(0);	
 	
 	int n;
 	cin >> n;
 	
-	int items[3] = {14, 7, 1};
 	int cnt = 0;
-	for(int item : items) {
-		cnt += n / item;
-		n %= item;
+	
+	while(n >= 14) {
+		n -= 14;
+		cnt++;
 	}
+	
+	while(n >= 7) {
+		n -= 7;
+		cnt++;
+	}
+	
+	while(n >=1) {
+		n -= 1;
+		cnt++;
+	}
+	
 	cout << cnt;
 	
 	return 0;
