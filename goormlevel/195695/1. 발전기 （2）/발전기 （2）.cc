0be292ef9956022ec59d382cfa1c1825246b90ab
Haon <88240193+msung99@users.noreply.github.com>
2024-06-14 23:45:56 +0900

    [goormlevel] Title: 발전기 (2), Time: 0.04 ms, Memory: 5.19 MB, Difficulty: 3 -BaekjoonHub

--- goormlevel/195695/1. 발전기 （2）/발전기 （2）.cc
+++ goormlevel/195695/1. 발전기 （2）/발전기 （2）.cc
@@ -3,80 +3,76 @@ using namespace std;
 
 int n, k;
 int board[1002][1002];
-bool visited[1001][1002];
+bool visited[1002][1002];
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {-1, 0, 1, 0};
+int res[32];
 
-int sz[1002];
-
-void bfs(int x, int y) {
-    queue<pair<int,int>> q;
-    q.push({x, y});
-    visited[x][y] = true;
-
-    int area = 0;
-    while(!q.empty()) {
-        area++;
-        auto cur = q.front();
-        q.pop();
-
-        for(int i=0; i<4; i++) {
-            int nx = cur.first + dx[i];
-            int ny = cur.second + dy[i];
-
-            if(nx < 0 || nx >= n || ny < 0 || ny >= n) {
-                continue;
-            }
-
-            if(board[cur.first][cur.second] != board[nx][ny] || visited[nx][ny]) {
-                continue;
-            }
-
-            visited[nx][ny] = true;
-            q.push({nx, ny});
-        }
-    }
-
-    if(area >= k) {
-        sz[board[x][y]]++;        
-    }
+void bfs(int sx, int sy) {
+	queue<pair<int,int>> q;
+	q.push({sx, sy});
+	visited[sx][sy] = true;
+	
+	int area = 0;
+	while(!q.empty()) {
+		area++;
+		auto cur = q.front();
+		int x = cur.first;
+		int y = cur.second;
+		q.pop();
+		
+		for(int i=0; i<4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(nx < 0 || nx >= n || ny < 0 || ny >= n) {
+				continue;
+			}
+			
+			if(board[nx][ny] != board[x][y] || visited[nx][ny]) {
+				continue;
+			}
+			visited[nx][ny] = true;
+			q.push({nx, ny});
+		}
+	}
+	
+	if(area >= k) {
+		res[board[sx][sy]]++;
+	}
 }
 
 int main(void)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    cin >> n >> k; // 원소가 k개 이상인 연결 요소를 단지로 취급
-
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            cin >> board[i][j];
-        }
-    }
-
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            if(!visited[i][j]) {
-                bfs(i, j);
-            }
-        }
-    }
-
-    int mx = -999999999;
-    for(int i=1; i<=30; i++) {
-        mx = max(mx, sz[i]);
-    }
-
-    vector<int> buildings;
-    for(int i=1; i<=30; i++) {
-        if(sz[i] == mx) {
-            buildings.push_back(i);
-        }
-    }
-    sort(buildings.begin(), buildings.end(), greater<>());
-    cout << buildings[0];
-
-    return 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	cin >> n >> k;
+	
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			cin >> board[i][j];
+		}
+	}
+	
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			if(!visited[i][j]) {
+				bfs(i, j);
+			}
+		}
+	}
+	
+	int mx = 0;
+	int ans = 0;
+	for(int i=1; i<=30; i++) {
+		if(res[i] >= mx) {
+			mx = res[i];
+			ans = i;
+		}
+	}
+	cout << ans;
+		
+	return 0;
 }
