90bd050c93d4f1b5b77c55366fe95d085882504d
Haon <88240193+msung99@users.noreply.github.com>
2024-06-15 18:10:50 +0900

    [goormlevel] Title: 카드 모으기, Time: 0.13 ms, Memory: 10.64 MB, Difficulty: 2 -BaekjoonHub

--- goormlevel/175909/1. 카드 모으기/카드 모으기.cc
+++ goormlevel/175909/1. 카드 모으기/카드 모으기.cc
@@ -1,34 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m;
+
+int play() {
+	unordered_set<int> dic;
+	
+	bool isSuccess = false;
+	for(int i=1; i<=m; i++) {
+		int data;
+		cin >> data;
+		dic.insert(data);
+		
+		if(dic.size() == n) {
+			return i;
+		}
+	}
+	return -1;
+}
+
 int main(void)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-    unordered_set<int> dic;
-
-    int n, m;
-    cin >> n >> m; // 4, 10
-
-    int res = -1;
-    for(int i=1; i<=m; i++) {
-        int data;
-        cin >> data;
-        dic.insert(data);
-
-        if(dic.size() == n) {
-            res = i;
-            break;
-        }
-
-        if(i == m) {
-            break;
-        }
-    }
-
-    cout << res;
-
-    return 0;
+	cin >> n >> m;
+	
+	cout << play();
+	
+	return 0;
 }
