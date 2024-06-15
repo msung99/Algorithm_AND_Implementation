cd1c5e4f083fcfb7c207979db0a4279a4a4504f4
Haon <88240193+msung99@users.noreply.github.com>
2024-06-15 17:56:47 +0900

    [Silver III] Title: 수강신청, Time: 148 ms, Memory: 25364 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.cc
+++ 백준/Silver/13414. 수강신청/수강신청.cc
@@ -1,13 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-unordered_map<string, int> dic;
-
 bool cmp(pair<string, int>& a, pair<string, int>& b) {
     return a.second < b.second;
 }
-
 
 int main(void)
 {
@@ -15,26 +11,26 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
 
+    int n, m;
     cin >> n >> m;
+
+    unordered_map<string, int> dic;
 
     for(int i=1; i<=m; i++) {
         string input;
         cin >> input;
-
         dic[input] = i;
     }
-
-    vector<pair<string, int>> res(dic.begin(), dic.end());
-    sort(res.begin(), res.end(), cmp);
+    vector<pair<string, int>> arr(dic.begin(), dic.end());
+    sort(arr.begin(), arr.end(), cmp);
 
     int cnt = 0;
-
-    for(auto element : res) {
+    for(auto element : arr) {
         if(cnt == n) {
             break;
         }
         cnt++;
-        cout << element.first << "\n";
+        cout << element.first << "\n"; 
     }
 
     return 0;
