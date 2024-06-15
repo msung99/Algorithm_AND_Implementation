94e612f85bcaf9818deabe3b466e791b597a91b6
Haon <88240193+msung99@users.noreply.github.com>
2024-06-15 13:29:01 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,9 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> adj_list[105];
-bool visited[105];
 int n, m;
+vector<int> adj_list[102];
+bool visited[102];
+
+void bfs(int st) {
+    queue<int> q;
+    q.push(st);
+    visited[st] = true;
+
+    int area = 0;
+    while(!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        area++;
+
+        for(int next : adj_list[cur]) {
+            if(visited[next]) {
+                continue;
+            }
+            visited[next] = true;
+            q.push(next);
+        }
+    }    
+    cout << area -1;
+}
 
 int main(void)
 {
@@ -11,37 +34,16 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
 
-    cin >> n >> m; // 7, 6
+    cin >> n >> m;
 
-    for(int i=0; i<m; i++) {
+    while(m--) {
         int u, v;
         cin >> u >> v;
         adj_list[u].push_back(v);
         adj_list[v].push_back(u);
     }
 
-    int start = 1;
-    queue<int> q;
-    q.push(start);
-
-    int cnt = 0;
-    while(!q.empty()) {
-        int cur = q.front();
-        q.pop();
-
-        if(visited[cur]) {
-            continue;
-        }
-
-        visited[cur] = true;
-        cnt++;
-
-        for(auto next : adj_list[cur]) {
-            q.push(next);
-        }
-    }
-
-    cout << cnt -1;
+    bfs(1);
 
     return 0;
 }
