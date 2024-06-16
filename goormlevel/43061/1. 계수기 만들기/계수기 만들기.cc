dc417e031030f7651eff5251512449194b257c87
Haon <88240193+msung99@users.noreply.github.com>
2024-06-16 14:57:57 +0900

    [goormlevel] Title: 계수기 만들기, Time: 0.00 ms, Memory: 3.42 MB, Difficulty: 2 -BaekjoonHub

--- goormlevel/43061/1. 계수기 만들기/계수기 만들기.cc
+++ goormlevel/43061/1. 계수기 만들기/계수기 만들기.cc
@@ -1,48 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int arr[10];
+int counter[10];
 int num[10];
 
 int main(void)
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    cin >> n;
-
-    for(int i=0; i<n; i++) {
-        cin >> arr[i];
-    }
-
-    for(int i=0; i<n; i++) {
-        cin >> num[i];
-    }
-
-    int k;
-    cin >> k;
-
-    while(k--) {
-        int idx = n-1;
-        bool isOverflow = true;
-
-        while(isOverflow) {
-            if(num[idx] == arr[idx]) {
-                isOverflow = true;
-                num[idx] = 0;
-            } else {
-                isOverflow = false;
-                num[idx]++;
-            }
-            idx--;
-        }
-    }
-
-    for(int i=0; i<n; i++) {
-        cout << num[i];
-    }
-
-    return 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int n;
+	cin >> n;
+	
+	for(int i=0; i<n; i++) {
+		int data;
+		cin >> data;
+		counter[i] = data;
+	}
+	
+	for(int i=0; i<n; i++) {
+		int data;
+		cin >> data;
+		num[i] = data;
+	}
+	
+	int k;
+	cin >> k;
+	while(k--) {
+		int idx = n-1;
+		bool isOverflow = true;
+		
+		while(isOverflow) {
+			num[idx]++;
+			if(num[idx] <= counter[idx]) {
+				isOverflow = false;
+			} else {
+				num[idx] = 0;
+				idx--;
+				if(idx < 0) {
+					break;
+				}
+			}
+		} 
+	}
+	
+	for(int i=0; i<n; i++) {
+		cout << num[i];
+	}
+	
+	return 0;
 }
