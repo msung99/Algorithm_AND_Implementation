a41b8fc1deebff9016d81701f9da7e1213dc3bfc
Haon <msung6924@kakao.com>
2024-08-18 15:27:08 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,29 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long func(int a, int b, int c) {
-  if(b == 1) {
-    return a % c;
-  }
+long long a, b, c, k;
+long long ans;
 
-  long long div = func(a, b/2, c);
-  if(b % 2 == 0) {
-    return div * div % c;
-  } else {
-    return ((div * div % c) * a) % c;
-  }
+long long power(long long b) {
+    if(b == 0) {
+        return 1;
+    }
+    if(b == 1) {
+        return a % c;
+    }
+    k = power(b/2) % c;
+    if(b % 2 == 0) {
+        return k*k % c;
+    }
+	return k * k % c * a % c;
 }
-
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-  int a, b, c;
-  cin >> a >> b >> c;
-  cout << func(a, b, c);
-
-  return 0;
+    cin >> a >> b >> c;
+    cout << power(b);
 }
