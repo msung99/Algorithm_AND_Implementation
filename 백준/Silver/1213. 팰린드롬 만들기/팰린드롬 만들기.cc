f5c37b42fab98bc274aa9edf8fe3e66ec56aa73f
Haon <msung6924@kakao.com>
2024-08-18 15:01:45 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt[200];
-char mid;
-int flag;
+string s, ans;
+int alpha[26];
+
+bool canNotMakePlain() {
+    int cnt = 0;
+    for(int i=0; i<26; i++) {
+        if(alpha[i] % 2 == 1) {
+            cnt++;
+        }
+    }
+    return cnt > 1;
+}
 
 int main(void)
 {
@@ -11,36 +20,37 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
 
-    string str;
-    cin >> str;
-
-    for(int i=0; i<str.size(); i++) {
-        cnt[str[i]]++;
+    cin >> s;
+    for(int i=0; i<s.size(); i++) {
+        alpha[s[i] - 'A']++;
     }
 
-    string result;
-    for(int i = 'Z'; i >= 'A'; i--) {
-        if(cnt[i]) {
-            if(cnt[i] % 2 == 1) {
-                mid = char(i);
-                flag++;
-                cnt[i]--;
-            }
-            if(flag == 2) {
-                break;
-            }
-            for(int j=0; j < cnt[i]; j+=2) {
-                result = char(i) + result;
-                result += char(i);
-            }
+    if(canNotMakePlain()) {
+        cout << "I'm Sorry Hansoo"; 
+        return 0;
+    }
+
+    // . 'A'부터 'Z'까지 나온 알파벳의 개수/2만큼 알파벳을 정답변수 ans에 더해준다.
+    for(int i=0; i<26; i++) {
+        for(int j=0; j<alpha[i]/2; j++) {
+            ans += i + 'A';
         }
     }
-    if(mid) {
-        result.insert(result.begin() + result.size() / 2, mid);
-    } 
-    if(flag == 2) {
-        cout << "I'm Sorry Hansoo\n"; 
-    } else {
-        cout << result << "\n";
+
+    // 남은 홀수개의 알파벳 1개는 팰린드롬을 만들려면 중앙에 위치해야 하므로 그 후에 더해준다.
+    for(int i=0; i<26; i++) {
+        if(alpha[i] % 2) {
+            ans += i + 'A';
+        }
     }
+
+    //  'Z'부터 'A'까지 나온 알파벳의 개수/2만큼 알파벳을 정답변수 ans에 더해준다.
+    for(int i=25; i>=0; i--) {
+        for(int j=0; j<alpha[i]/2; j++) {
+            ans += i + 'A';
+        }
+    }
+    cout << ans;
+
+    return 0;
 }
