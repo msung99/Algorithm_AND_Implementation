98dec4e06c609859e06a4faa5a81e0f2efab104d
Haon <msung6924@kakao.com>
2024-08-18 13:44:00 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[30];
+int arr[27];
 
 int main(void)
 {
@@ -12,13 +12,13 @@ int main(void)
     string s;
     cin >> s;
 
-    for(int i=0; i<s.size(); i++) {
+    for(int i=0; i<s.length(); i++) {
         arr[s[i] - 'a']++;
     }
 
     for(int i=0; i<26; i++) {
         cout << arr[i] << ' ';
     }
-    
+
     return 0;
 }
