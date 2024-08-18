944fe7b0c7b3a52c78bba5b93530353afc6fae85
Haon <msung6924@kakao.com>
2024-08-18 13:59:26 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+stack<char> s;
+
 int main(void)
 {
     ios::sync_with_stdio(0);
@@ -9,13 +11,34 @@ int main(void)
 
     string str;
     cin >> str;
+    
+    for(int i=0; i<str.size()/2; i++) {
+        s.push(str[i]);
+    }
 
-    string temp = str; 
-    reverse(temp.begin(), temp.end());
+    bool isPalin = true;
+    int mid = 0;
 
-    if(temp == str) {
+    if(str.size() % 2 == 1) {
+        mid = str.size()/2 + 1;
+    } else {
+        mid = str.size()/2;
+    }
+
+    for(int i=mid; i<str.size(); i++) {
+        if(s.top() == str[i]) {
+            s.pop();
+        } else {
+            isPalin = false;
+            break;
+        }
+    }
+
+    if(isPalin) {
         cout << 1;
     } else {
         cout << 0;
     }
+
+    return 0;
 }
