556d3dadd6f84454211ed1fb171f5a82f095efaa
Haon <msung6924@kakao.com>
2024-10-02 09:23:27 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2088 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -1,59 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+int res[3];
 int board[130][130];
-int cnt[2];
 
-void func(int n, int startX, int startY) {
-  if(n == 1) {
-    cnt[board[startX][startY]]++;
-    return;
-  }
-
-  bool isSameArea = true;
-  int color = board[startX][startY];
-
-  for(int i=startX; i < startX + n; i++) {
-    for(int j=startY; j < startY + n; j++) {
-      if(color != board[i][j]) {
-        isSameArea = false;
-        break;
-      }
+void func(int k, int startX, int startY) {
+    if(k == 1) {
+        res[board[startX][startY]]++;
+        return;        
     }
-  }
 
-  if(isSameArea) {
-    cnt[board[startX][startY]]++;
-  } else {
-    int size = n/2;
-    for(int i=0; i<2; i++) {
-      for(int j=0; j<2; j++) {
-        int newX = startX + size * i;
-        int newY = startY + size * j;
-        func(size, newX, newY);
-      }
+    bool isSameArea = true;
+    int color = board[startX][startY];
+
+    for(int i=startX; i<startX+k; i++) {
+        for(int j=startY; j<startY+k; j++) {
+            if(color != board[i][j]) {
+                isSameArea = false;
+                break;
+            }
+        }
     }
-  }
+
+
+    if(isSameArea) {
+        res[color]++;
+    } else {
+        int size = k/2;
+        for(int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
+                func(size, startX+size*i, startY+size*j);
+            }
+        }
+    }
 }
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    cin >> n;
 
-  int n;
-  cin >> n;
-  for(int i=0; i<n; i++) {
-    for(int j=0; j<n; j++) {
-      cin >> board[i][j];
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> board[i][j];
+        }
     }
-  }
 
-  func(n, 0, 0);
+    func(n, 0, 0);    
 
-  cout << cnt[0] << "\n";
-  cout << cnt[1];
+    cout << res[0] << "\n";
+    cout << res[1];
 
-  return 0;
+    return 0;
 }
