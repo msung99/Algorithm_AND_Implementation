30e5af9744d10bb9db2192bc7f899654a9bee136
Haon <msung6924@kakao.com>
2024-12-21 20:35:39 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -2,52 +2,54 @@
 using namespace std;
 
 int n, m;
-vector<pair<int,int>> house;
+int arr[52][52];
 vector<pair<int,int>> chicken;
+vector<pair<int,int>> house;
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-  cin >> n >> m;
-  for(int i=0; i<n; i++) {
-    for(int j=0; j<n; j++) {
-      int data;
-      cin >> data;
-      if(data == 1) {
-        house.push_back({i, j});
-      }
-      if(data == 2) {
-        chicken.push_back({i, j});
-      }
-    }
-  }
+    cin >> n >> m;
 
-  vector<int> brute(chicken.size(), 1);
-  fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> arr[i][j];
 
-  int mn = 1999999999;
-  do {
-    int cur = 0;
-    for(auto h : house) { // 각 집에대해 현재 부여된 m개의 치킨집중에 적절히 선택하여 최소 치킨거리 값을 계산 
-      int houseMin = 199999999;
+            if(arr[i][j] == 1) {
+                house.push_back({i, j});
+            }
 
-      for(int i=0; i<brute.size(); i++) {
-        if(brute[i] == 1) {
-          houseMin = min(houseMin, abs(h.first - chicken[i].first) + abs(h.second - chicken[i].second));          
+            if(arr[i][j] == 2) {
+                chicken.push_back({i, j});
+            }
         }
-      }
-
-      cur += houseMin;
     }
 
-    mn = min(cur, mn);
+    vector<int> brute(chicken.size(), 1);
+    fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
 
-  } while(next_permutation(brute.begin(), brute.end()));
+    int mn = 1999999999;
 
-  cout << mn;
+    do {
+        int cur = 0;
+        for(auto h : house) { // 각 집에대해 부여된 m개의 치킨 집 중에 적절히 선택하여 최소 치킨거리 값을 계산
+            int houseMin = 1999999999; 
 
-  return 0;
+            for(int i=0; i<brute.size(); i++) {
+                if(brute[i] == 1) {
+                  houseMin = min(houseMin, abs(h.first - chicken[i].first) + abs(h.second - chicken[i].second));          
+                }
+            }
+            cur += houseMin;
+        }
+        mn = min(cur, mn);
+
+    } while(next_permutation(brute.begin(), brute.end()));
+
+    cout << mn;
+
+    return 0;
 }
