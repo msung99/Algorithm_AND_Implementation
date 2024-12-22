7be17508156eb86115cbacfc8f54710b3278f80a
Haon <msung6924@kakao.com>
2024-12-22 12:48:04 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int n, int r, int c) {
+    if(n == 1) {
+        return 1;
+    }
+
+    int half = n/2;
+
+    if(r < half && c < half) {
+        return func(half, r, c);
+    }
+
+    else if(r < half && c >= half) {
+        return half*half + func(half, r, c-half);
+    }
+
+    else if(r >= half && c < half) {
+        return 2*half*half + func(half, r-half, c);
+    }
+
+    return 3*half*half + func(half, r-half, c-half);
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, r, c;
+    cin >> n >> r >> c;
+
+    n = (1 << n);
+
+    cout << func(n, r, c) - 1;
+
+    return 0;
+}
