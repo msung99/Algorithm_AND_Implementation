f676b23667010f7f131603f01e32b2788888518e
Haon <msung6924@kakao.com>
2025-01-27 13:38:24 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,38 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int m, n, k;
 int board[52][52];
 bool visited[52][52];
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {-1, 0, 1, 0};
-int m, n, k;
 
 void bfs(int x, int y) {
     queue<pair<int,int>> q;
-    q.push({x,y});
-    visited[x][y] = true;
+    q.push({x, y});
 
     while(!q.empty()) {
         auto cur = q.front();
-        int x = cur.first;
-        int y = cur.second;
         q.pop();
 
         for(int i=0; i<4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            int nx = cur.first + dx[i];
+            int ny = cur.second + dy[i];
 
             if(nx < 0 || nx >= n || ny < 0 || ny >= m) {
                 continue;
             }
 
-            if(visited[nx][ny] || board[nx][ny] != 1) {
+            if(visited[nx][ny] || board[nx][ny] == 0) {
                 continue;
             }
             visited[nx][ny] = true;
             q.push({nx, ny});
         }
-    }
+    }    
 }
 
 int main(void)
@@ -46,28 +43,28 @@ int main(void)
     while(t--) {
         cin >> m >> n >> k;
 
-        for(int i=0; i<k; i++) {
-            int u, v;
-            cin >> u >> v;
-            board[v][u] = 1;
-        }
-
-        int cnt = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(board[i][j] == 1 && !visited[i][j]) {
-                    bfs(i, j);
-                    cnt++;
-                }
-            }
-        }
-        cout << cnt << "\n";
-
         for(int i=0; i<n; i++) {
             fill(board[i], board[i] + m, 0);
             fill(visited[i], visited[i] + m, false);
         }
-    }    
+
+        while (k--) {
+            int x, y;
+            cin >> x >> y;
+            board[y][x] = 1;
+        }
+
+        int area = 0;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(!visited[i][j] && board[i][j] == 1) {
+                    bfs(i, j);
+                    area++;
+                }
+            }
+        }
+        cout << area << "\n";
+    }
 
     return 0;
 }
