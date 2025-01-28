6e240a19b9c63e14a6dad5ac1adc422b10098fe3
Haon <msung6924@kakao.com>
2025-01-28 12:27:16 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -3,61 +3,55 @@ using namespace std;
 
 int board[66][66];
 
-void func(int n, int startX, int startY) {
-  if(n == 1) {
-    cout << board[startX][startY];
-    return;
-  }
-
-  bool isSameArea = true;
-  int color = board[startX][startY];
-  for(int i=startX; i<startX+n; i++) {
-    for(int j=startY; j<startY+n; j++) {
-      if(color != board[i][j]) {
-        isSameArea = false;
-        break;
-      }      
+void func(int n, int sx, int sy) {
+    if(n == 1) {
+        cout << board[sx][sy];
+        return;
     }
-  }
 
-  if(isSameArea) {
-    cout << board[startX][startY];
-  } else {
-    int size = n/2;
-    cout << "(";
-    for(int i=0; i<2; i++) {
-      for(int j=0; j<2; j++) {
-        int newX = startX + size * i;
-        int newY = startY + size * j;
-        func(size, newX, newY);
-      }
+    bool isSameArea = true;
+    int color = board[sx][sy];
+    for(int i=sx; i<sx+n; i++) {
+        for(int j=sy; j<sy+n; j++) {
+            if(board[i][j] != color) {
+                isSameArea = false;
+                break;
+            }
+        }
     }
-    cout << ")";
-  }
+
+    if(isSameArea) {
+        cout << color;
+    } else {
+        cout << "(";
+        int size = n/2;
+        for(int i=0; i<2; i++) {
+            for(int j=0; j<2; j++) {
+                func(size, sx + size*i, sy + size*j);
+            }
+        }
+        cout << ")";
+    }
 }
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-  int n;
-  cin >> n;
+    int n;
+    cin >> n;
 
-  for(int i=0; i<n; i++) {
-    string s;
-    cin >> s;
-    for(int j=0; j<n; j++) {
-      if(s[j] == '0') {
-        board[i][j] = 0;
-      } else {
-        board[i][j] = 1;
-      }
+    for(int i=0; i<n; i++) {
+        string s;
+        cin >> s;
+        for(int j=0; j<n; j++) {
+            board[i][j] = s[j] - '0';
+        }
     }
-  }
 
-  func(n, 0, 0);
+    func(n, 0, 0);
 
-  return 0;
+    return 0;
 }
