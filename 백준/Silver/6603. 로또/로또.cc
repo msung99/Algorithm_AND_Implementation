370c5c97ed8b18740a52ad131a6751ce2a22e752
Haon <msung6924@kakao.com>
2025-01-30 13:15:24 +0900

    [Silver II] Title: 로또, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.cc
+++ 백준/Silver/6603. 로또/로또.cc
@@ -2,42 +2,43 @@
 using namespace std;
 
 int n;
-int arr[20];
-int num[20];
+int num[15];
+int res[15];
 
-void func(int k, int start) {
-  if(k == 6) {
-    for(int i=0; i<6; i++) {
-      cout << arr[i] << ' ';
+void func(int k, int st) {
+    if(k == 6) {
+        for(int i=0; i<6; i++) {
+            cout << res[i] << ' ';
+        }
+        cout << "\n";
+        return;
     }
-    cout << "\n";
-    return;
-  }
 
-  for(int i=start; i<n; i++) {
-    arr[k] = num[i];
-    func(k+1, i+1);
-  }
+    for(int i=st; i<n; i++) {
+        res[k] = num[i];
+        func(k+1, i+1);
+    }
 }
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-  while(true) {
-    cin >> n;
-    if(n == 0) {
-      break;
+    while(true) {
+        cin >> n;
+
+        if(n == 0) {
+            break;
+        }
+
+        for(int i=0; i<n; i++) {
+            cin >> num[i];
+        }
+        func(0, 0);
+        cout << "\n";
     }
-    for(int i=0; i<n; i++) {
-      cin >> num[i];
-    }
 
-    func(0, 0);
-    cout << "\n";
-  }
-
-  return 0;
+    return 0;
 }
