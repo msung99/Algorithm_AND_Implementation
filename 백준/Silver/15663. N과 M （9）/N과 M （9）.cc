0cf552ab27f319bf49c96cdbbb9489902761c4d5
Haon <msung6924@kakao.com>
2025-01-30 12:41:05 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -2,45 +2,45 @@
 using namespace std;
 
 int n, m;
-int arr[10];
 int num[10];
+int res[10];
 bool isused[10];
 
 void func(int k) {
-  if(k == m) {
-    for(int i=0; i<m; i++) {
-      cout << arr[i] << ' ';
+    if(k == m) {
+        for(int i=0; i<m; i++) {
+            cout << res[i] << ' ';
+        }
+        cout << "\n";
+        return;
     }
-    cout << "\n";
-    return;
-  }
-  
-  int tmp = 0;
-  for(int i=0; i<n; i++) {
-    if(!isused[i] && tmp != num[i]) {
-      arr[k] = num[i];
-      isused[i] = true;
-      tmp = num[i];
-      func(k+1);
-      isused[i] = false;
+
+    int lastNum = 0;
+    for(int i=0; i<n; i++) {
+        if(!isused[i] && lastNum != num[i]) {
+            isused[i] = true;
+            res[k] = num[i];
+            lastNum = num[i];
+            func(k+1);
+            isused[i] = false;
+        }
     }
-  }
 }
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-  cin >> n >> m;
+    cin >> n >> m;
 
-  for(int i=0; i<n; i++) {
-    cin >> num[i];
-  }
-  sort(num, num + n);
+    for(int i=0; i<n; i++) {
+        cin >> num[i];
+    }
+    sort(num, num + n);
 
-  func(0);
+    func(0);
 
-  return 0;
+    return 0;
 }
