02fbf57fc1e451142d3a9708fe2c31837074eb80
Haon <msung6924@kakao.com>
2025-01-30 11:44:32 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,37 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-// 1~n 사이의 숫자중에 중복없이 m개를 고른 수열
+
 int n, m;
 int arr[10];
 bool isused[10];
 
 void func(int k) {
-  if(k == m) {
-    for(int i=0; i<m; i++) {
-      cout << arr[i] << ' ';
+    if(k == m) {
+        for(int i=0; i<m; i++) {
+            cout << arr[i] << ' ';
+        }
+        cout << "\n";
+        return;
     }
-    cout << "\n";
-  }
 
-  for(int i=1; i<=n; i++) {
-    if(!isused[i]) {
-      isused[i] = true;
-      arr[k] = i;
-      func(k+1);
-      isused[i] = false;
+    for(int i=1; i<=n; i++) {
+        if(!isused[i]) {
+            isused[i] = true;
+            arr[k] = i;
+            func(k+1);
+            isused[i] = false;
+        }
     }
-  }
 }
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-  cin >> n >> m;
+    cin >> n >> m;
 
-  func(0);
+    func(0);
 
-  return 0;
+    return 0;
 }
