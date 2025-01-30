30f09ba841d7b05d87b4ec0f06433f1540fc603e
Haon <msung6924@kakao.com>
2025-01-30 12:04:53 +0900

    [Silver III] Title: N과 M (5), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -3,42 +3,42 @@ using namespace std;
 
 int n, m;
 int arr[10];
-int num[10];
+int res[10];
 bool isused[10];
 
 void func(int k) {
-  if(k == m) {
-    for(int i=0; i<m; i++) {
-      cout << arr[i] << ' ';
+    if(k == m) {
+        for(int i=0; i<m; i++) {
+            cout << res[i] << ' ';
+        }
+        cout << "\n";
+        return;
     }
-    cout << "\n";
-    return;
-  }
 
-  for(int i = 0; i < n; i++) {
-    if(!isused[i]) {
-      arr[k] = num[i];
-      isused[i] = true;
-      func(k+1);
-      isused[i] = false;
+    for(int i=0; i<n; i++) {
+        if(!isused[i]) {
+            isused[i] = true;
+            res[k] = arr[i];
+            func(k+1);
+            isused[i] = false;
+        }
     }
-  }
 }
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-  cin >> n >> m;
+    cin >> n >> m;
 
-  for(int i=0; i<n; i++) {
-    cin >> num[i];
-  }
-  sort(num, num + n);
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
 
-  func(0);
+    func(0);
 
-  return 0;
+    return 0;
 }
