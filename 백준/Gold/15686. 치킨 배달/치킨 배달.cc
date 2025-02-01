e0ceb6acad88bd697f8623f7cdff3b78fdebf681
Haon <msung6924@kakao.com>
2025-02-01 18:52:53 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -2,9 +2,9 @@
 using namespace std;
 
 int n, m;
-int arr[52][52];
-vector<pair<int,int>> chicken;
+int board[52][52];
 vector<pair<int,int>> house;
+vector<pair<int,int>> chicken;
 
 int main(void)
 {
@@ -16,40 +16,40 @@ int main(void)
 
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
-            cin >> arr[i][j];
+            cin >> board[i][j];
 
-            if(arr[i][j] == 1) {
+            if(board[i][j] == 1) {
                 house.push_back({i, j});
             }
 
-            if(arr[i][j] == 2) {
+            if(board[i][j] == 2) {
                 chicken.push_back({i, j});
             }
         }
     }
 
     vector<int> brute(chicken.size(), 1);
-    fill(brute.begin(), brute.begin() + chicken.size() - m, 0);
+    fill(brute.begin(), brute.begin() + chicken.size()-m, 0);
 
-    int mn = 1999999999;
+    int ans = 1999999999;
 
     do {
         int cur = 0;
-        for(auto h : house) { // 각 집에대해 부여된 m개의 치킨 집 중에 적절히 선택하여 최소 치킨거리 값을 계산
-            int houseMin = 1999999999; 
 
+        for(auto h : house) {
+            int houseMin = 1999999999;
+            
             for(int i=0; i<brute.size(); i++) {
                 if(brute[i] == 1) {
-                  houseMin = min(houseMin, abs(h.first - chicken[i].first) + abs(h.second - chicken[i].second));          
+                    houseMin = min(houseMin, abs(h.first - chicken[i].first) + abs(h.second - chicken[i].second));
                 }
             }
             cur += houseMin;
         }
-        mn = min(cur, mn);
-
+        ans = min(ans, cur);
     } while(next_permutation(brute.begin(), brute.end()));
 
-    cout << mn;
+    cout << ans;
 
     return 0;
 }
