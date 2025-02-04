e28723c31c121ce05558a8e2500c24796c619caa
Haon <msung6924@kakao.com>
2025-02-04 17:55:13 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 116 ms, Memory: 25140 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -2,7 +2,6 @@
 using namespace std;
 
 int n, m;
-vector<string> res;
 
 int main(void)
 {
@@ -14,22 +13,23 @@ int main(void)
 
     unordered_map<string, int> dic1;
     unordered_map<int, string> dic2;
-
+    
     for(int i=1; i<=n; i++) {
         string s;
         cin >> s;
+
         dic1[s] = i;
         dic2[i] = s;
-    }    
+    }
 
     while(m--) {
         string s;
         cin >> s;
 
-        if(s[0] > '0' && s[0] <= '9') {
-            cout << dic2[stoi(s)] << "\n";
-        } else {
+        if(s[0] >= 'A' && s[0] <= 'Z') {
             cout << dic1[s] << "\n";
+        } else {
+            cout << dic2[stoi(s)] << "\n";
         }
     }
 
