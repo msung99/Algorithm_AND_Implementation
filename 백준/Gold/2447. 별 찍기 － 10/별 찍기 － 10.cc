75a584e0b6151542a66cfd8cd7f9accefde06afa
Haon <msung6924@kakao.com>
2025-02-04 14:39:11 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 68 ms, Memory: 6748 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -4,43 +4,40 @@ using namespace std;
 char board[2200][2200];
 
 void func(int n, int startX, int startY) {
-  if(n == 1) {
-    board[startX][startY] = '*';
-    return;
-  }
-
-  for(int i=0; i<3; i++) {
-    for(int j=0; j<3; j++) {
-      if(i == 1 && j == 1) {
-        continue;
-      }
-
-      int size = n/3;
-      int newX = startX + size*i;
-      int newY = startY + size*j;
-      func(size, newX, newY);
+    if(n == 1) {
+        board[startX][startY] = '*';
+        return;
     }
-  }
+
+    for(int i=0; i<3; i++) {
+        for(int j=0; j<3; j++) {
+            if(i == 1 && j == 1) {
+                continue;
+            }
+            int size = n/3;
+            func(size, startX + size*i, startY + size*j);
+        }
+    }
 }
 
 int main(void)
 {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-  int n;
-  cin >> n;
+    int n;
+    cin >> n;
 
-  for(int i=0; i<n; i++) {
-    fill(board[i], board[i] + n, ' ');
-  }
+    for(int i=0; i<n; i++) {
+        fill(board[i], board[i] + n, ' ');
+    }
 
-  func(n, 0, 0);
+    func(n, 0, 0);
 
-  for(int i=0; i<n; i++) {
-    cout << board[i] << "\n";
-  }
+    for(int i=0; i<n; i++) {
+        cout << board[i] << "\n";
+    }
 
-  return 0;
+    return 0;
 }
