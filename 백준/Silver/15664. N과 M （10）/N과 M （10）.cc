0c15b82811fef9e67a96073f1147a4374e9d859f
Haon <msung6924@kakao.com>
2025-02-04 16:06:20 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -1,45 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int num[10];
+int res[10];
 int n, m;
 bool isused[10];
-int arr[10];
-int num[10];
 
-void func(int k, int start) {
-	if (k == m) {
-		for (int i = 0; i < m; i++) {
-			cout << arr[i] << ' ';
-		}
-		cout << "\n";
-		return;
-	}
+void func(int k, int st) {
+    if(k == m) {
+        for(int i=0; i<m; i++) {
+            cout << res[i] << ' ';
+        }        
+        cout << "\n";
+        return;
+    }
 
-	int tmp = 0;
-	for (int i = start; i < n; i++) {
-		if (!isused[i] && tmp != num[i]) {
-			isused[i] = true;
-			arr[k] = num[i];
-			tmp = arr[k];
-			func(k + 1, i + 1);
-			isused[i] = false;
-		}
-	}
+    int lastNum = 0;
+    for(int i=st; i<n; i++) {
+        if(!isused[i] && lastNum != num[i]) {
+            isused[i] = true;
+            lastNum = num[i];
+            res[k] = num[i];
+            func(k+1, i+1);
+            isused[i] = false;
+        }
+    }
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n >> m;
+    cin >> n >> m;
 
-	for (int i = 0; i < n; i++) {
-		cin >> num[i];
-	}
-	sort(num, num + n);
-	func(0, 0);
+    for(int i=0; i<n; i++) {
+        cin >> num[i];
+    }
+    sort(num, num+n);
 
-	return 0;
+    func(0, 0);
+
+    return 0;
 }
