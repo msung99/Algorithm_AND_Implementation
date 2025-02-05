cd318c4d81b25bff0b73df818853fc5d6f3282b8
Haon <msung6924@kakao.com>
2025-02-05 12:04:11 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 68 ms, Memory: 6536 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -5,19 +5,22 @@ int n, m;
 vector<int> adj_list[1002];
 bool visited[1002];
 
-void bfs(int st) {
+void bfs(int x) {
+    visited[x] = true;
+
     queue<int> q;
-    q.push(st);
+    q.push(x);
 
     while(!q.empty()) {
-        int cur = q.front();
+        auto cur = q.front();
         q.pop();
 
-        for(int next : adj_list[cur]) {
-            if(!visited[next]) {
-                visited[next] = true;
-                q.push(next);
+        for(int data : adj_list[cur]) {
+            if(visited[data]) {
+                continue;
             }
+            q.push(data);
+            visited[data] = true;
         }
     }
 }
@@ -30,22 +33,22 @@ int main(void)
 
     cin >> n >> m;
 
-    for(int i=0; i<m; i++) {
+    while(m--) {
         int u, v;
         cin >> u >> v;
         adj_list[u].push_back(v);
-        adj_list[v].push_back(u);        
+        adj_list[v].push_back(u);
     }
 
-    int cnt = 0;
+    int area = 0;
     for(int i=1; i<=n; i++) {
         if(!visited[i]) {
+            area++;
             bfs(i);
-            cnt++;
         }
     }
 
-    cout << cnt;
+    cout << area;
 
     return 0;
 }
