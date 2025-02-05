a1aa854c0f0d2f1217a51dab164bb67c10ce4a60
Haon <msung6924@kakao.com>
2025-02-05 13:22:17 +0900

    [Gold V] Title: 회장뽑기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.cc
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.cc
@@ -1,10 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
 vector<int> adj_list[52];
 int dist[52];
-int scores[52];
+int score[52];
+int n;
+
+void bfs(int st) {
+    queue<int> q;
+    q.push(st);
+    dist[st] = 0;
+
+    while(!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        for(int next : adj_list[cur]) {
+            if(dist[next] != -1) {
+                continue;
+            }
+            dist[next] = dist[cur] + 1;
+            q.push(next);
+        }
+    }
+}
 
 int main(void)
 {
@@ -17,48 +36,35 @@ int main(void)
     while(true) {
         int u, v;
         cin >> u >> v;
+
         if(u == -1 && v == -1) {
             break;
         }
+
         adj_list[u].push_back(v);
         adj_list[v].push_back(u);
     }
 
     for(int i=1; i<=n; i++) {
-        fill(dist, dist + n + 1, -1);        
+        fill(dist, dist + n+1, -1);
+        bfs(i);
 
-        queue<int> q;
-        q.push(i);
-        dist[i] = 0;
-    
-        while(!q.empty()) {
-            int cur = q.front();
-            q.pop();
-
-            for(auto next : adj_list[cur]) {
-                if(dist[next] != -1) {
-                    continue;
-                }
-                dist[next] = dist[cur] + 1;
-                q.push(next);
-            }
-        }
-
-        int score = *max_element(dist + 1, dist + n+1);
-        scores[i] = score;
+        int curScore = *max_element(dist+1, dist+n+1);
+        score[i] = curScore;
     }
 
-    int minValue = *min_element(scores + 1, scores + n+1);
+    int minValue = *min_element(score+1, score+n+1);
+
     vector<int> person;
     for(int i=1; i<=n; i++) {
-        if(scores[i] == minValue) {
+        if(score[i] == minValue) {
             person.push_back(i);
         }
     }
 
     cout << minValue << ' ' << person.size() << "\n";
     for(int i=0; i<person.size(); i++) {
-        cout << person[i] << ' ' ;
+        cout << person[i] << ' ';
     }
 
     return 0;
