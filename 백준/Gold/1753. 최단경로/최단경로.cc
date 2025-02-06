af7a6ae6e0e4e2c2b381909f606e1d27f44ab88e
Haon <msung6924@kakao.com>
2025-02-06 13:34:12 +0900

    [Gold IV] Title: 최단경로, Time: 84 ms, Memory: 9184 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,34 +1,10 @@
-// 1번 정점에서 n번 정점으로 이동 가능한 최단 경로 2가지
-// 1 -> a -> b -> n
-// 1 -> b -> a -> n
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, e;
-vector<pair<int,int>> adj_list[805];
-int dist[805];
-int infinite = 0x3f3f3f3f;
-
-long long djikstra(int source, int dest) {
-    fill(dist, dist + n + 1, infinite);
-    dist[source] = 0;
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-    pq.push({dist[source], source});
-
-    while(!pq.empty()) {
-        auto cur = pq.top();
-        pq.pop();
-
-        for(auto next : adj_list[cur.second]) {
-            if(dist[next.second] <= dist[cur.second] + next.first) {
-                continue;
-            }
-            dist[next.second] = dist[cur.second] + next.first;
-            pq.push({dist[next.second], next.second});
-        }
-    }
-    return dist[dest];
-}
+int v, e, st;
+vector<pair<int,int>> adj_list[20005]; // {가중치, 정점 번호}
+int dist[20005]; // 최단 거리 테이블
+int infinite = 1999999999;
 
 int main(void)
 {
@@ -36,26 +12,59 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
 
-    cin >> n >> e;
+    cin >> v >> e >> st;
 
     while(e--) {
         int u, v, w;
         cin >> u >> v >> w;
         adj_list[u].push_back({w, v});
-        adj_list[v].push_back({w, u});
     }
 
-    int v1, v2;
-    cin >> v1 >> v2;
+    fill(dist, dist+v+1, infinite);
 
-    long long ans1 = djikstra(1, v1) + djikstra(v1, v2) + djikstra(v2, n);
-    long long ans2 = djikstra(1, v2) + djikstra(v2, v1) + djikstra(v1, n);
-    long long res = min(ans1, ans2);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
 
-    if(res >= infinite) {
-        res = -1;
+    dist[st] = 0;
+    pq.push({dist[st], st}); // 1. 우선순위 큐에 {0, 시작점} 추가
+
+    // 매번 아직 거리가 확정되지 않은 정점을 중에서 가장 가까운 정점을 찾아 거리를 확정함.
+    while(!pq.empty()) {
+        // 2-1. 우선순위 큐에서 거리가 가장 작은 원소를 선택함
+        auto cur = pq.top();  // {가중치, 정점 번호}
+        pq.pop();
+
+        // 2-2. 이미 최소 거리값을 확정한 정점인 경우 (선택한 원소의 거리값이 최단 거리 테이블에 있는 값과 다를 경우) continue
+        if(dist[cur.second] != cur.first) {
+            continue;
+        }
+
+        // 3. 정점 cur과 이웃한 정점(next)들에 대해 최단거리 테이블 값보다 
+        // cur 을 거쳐가는 것이 더 효율적인(작은) 최단 거리 값일 경우
+        // next 의 최단거리 테이블 값을 갱신하고 우선순위 큐에 (거리, next) 를 추가
+        for(auto next : adj_list[cur.second]) {
+            // 특정 정점의 거리가 확정됐는지 여부를 저장하기 않기 때문에, 매번 거리를 비교해야 함
+
+            // 기존에 next 에 계산된 최소 거리값(= d[next.second]) 이 현재 계산된 거리값보다 더 클 경우 continue
+            // => ex) 기존에 계산된 정점 next 의 거리값이 무한대(infinfie) 이고,
+            // 현재 cur 을 통해 next 로 거쳐가는 값이 3이라면,
+            // 정점 next 에 도달 가능한 거리값 중에 현재까지 계산된 최소 거리값이 3이다.
+            if(dist[next.second] <= dist[cur.second] + next.first) {
+                continue;
+            }
+
+            // d[next.second] 를 갱신하고 우선순위 큐에 (거리, next.second) 를 추가한다.
+            dist[next.second] = dist[cur.second] + next.first;
+            pq.push({dist[next.second], next.second});
+        }
     }
-    cout << res;
+
+    for(int i=1; i<=v; i++) {
+        if(dist[i] == infinite) {
+            cout << "INF\n";
+        } else {
+            cout << dist[i] << "\n";
+        }
+    }
 
     return 0;
 }
