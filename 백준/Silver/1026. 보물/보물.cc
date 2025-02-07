b7b586bd1df61cf6e1ce6382e644cb6b782ed9e3
Haon <msung6924@kakao.com>
2025-02-07 17:59:59 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.cc
+++ 백준/Silver/1026. 보물/보물.cc
@@ -1,30 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int A[51];
-int B[51];
-
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	cin >> n;
+    int n;
+    cin >> n;
 
-	for (int i = 0; i < n; i++) 
-		cin >> A[i];
+    vector<int> arr1;
+    vector<int> arr2;
 
-	for (int i = 0; i < n; i++)
-		cin >> B[i];
+    for(int i=0; i<n; i++) {
+        int data;
+        cin >> data;
+        arr1.push_back(data);
+    }
 
-	sort(A, A + n);
-	sort(B, B + n);
+    for(int i=0; i<n; i++) {
+        int data;
+        cin >> data;
+        arr2.push_back(data);
+    }
 
-	int sum = 0;
-	for (int i = 0; i < n; i++) {
-		sum += A[i] * B[n-i-1];
-	}
+    sort(arr1.begin(), arr1.end());
+    sort(arr2.begin(), arr2.end(), greater<int>());
 
-	cout << sum;
+    int res = 0;
+    for(int i=0; i<n; i++) {
+        res += (arr1[i] * arr2[i]);
+    }
+    cout << res;
+
+    return 0;
 }
