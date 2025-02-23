7409e24f882085572d40c0468d405896c8d4c86d
Haon <msung6924@kakao.com>
2025-02-23 12:07:44 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 11.52 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,41 +1,34 @@
 
 
 class Solution {
-    int tryCnt = 0;
+    int convertCnt = 0;
     int deleteCnt = 0;
 
-    void calculateZeroCount(String s) {
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == '0') {
-                deleteCnt++;
-            }
+    // 이진 변환의 횟수와 + 변환 과정에서 제거된 모든 0의 개수
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+
+        while(!s.equals("1")) {
+            String tmp = removeZero(s);
+            deleteCnt += removeZeroCount(s, tmp);
+
+            int len = tmp.length();
+            s = Integer.toString(len, 2);
+            convertCnt++;
         }
+
+        answer[0] = convertCnt;
+        answer[1] = deleteCnt;
+
+        return answer;
     }
 
-    String convertLengthToBinary(String s) {
-        int len = s.length();
-        return Integer.toBinaryString(len);
-    }
-
-    String deleteZeros(String s) {
+    private String removeZero(String s) {
         return s.replace("0", "");
     }
 
-    public int[] solution(String s) {
-        int[] answer = new int[2];
-        String cur = s;
-
-        while (!cur.equals("1")) { // "1"이 될 때까지 반복
-            calculateZeroCount(cur);        // cur에 따라 동작해야 한다고 가정
-            cur = deleteZeros(cur);         // cur을 수정
-            cur = convertLengthToBinary(cur); // cur의 길이를 이진수로 변환
-            tryCnt++;
-        }
-        
-        answer[0] = tryCnt;
-        answer[1] = deleteCnt;
-        
-        return answer;
+    private int removeZeroCount(String s1, String s2) {
+        return s1.length() - s2.length();
     }
 }
 
