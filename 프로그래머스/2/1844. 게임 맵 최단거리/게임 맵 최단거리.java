5762427eb1261141c3755bbebd608742fb117ca5
Haon <msung6924@kakao.com>
2025-04-03 15:22:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 22.21 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,26 +1,29 @@
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 class Solution {
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {-1, 0, 1, 0};
+    int[][] dist;
+    int[] dx = {0, 1, 0, -1};
+    int[] dy = {-1, 0, 1, 0};
 
     public int solution(int[][] board) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0});
         int n = board.length;
         int m = board[0].length;
-        int[][] dist = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        dist = new int[n+1][m+1];
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
                 dist[i][j] = -1;
             }
         }
 
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
         dist[0][0] = 1;
 
-        while (!q.isEmpty()) {
+        while(!q.isEmpty()) {
             int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
@@ -36,11 +39,11 @@ class Solution {
                 if(board[nx][ny] == 0 || dist[nx][ny] != -1) {
                     continue;
                 }
-
-                dist[nx][ny] = dist[x][y] + 1;
                 q.add(new int[]{nx, ny});
+                dist[nx][ny] = dist[x][y] + 1;
             }
         }
+
         return dist[n-1][m-1];
     }
 }
