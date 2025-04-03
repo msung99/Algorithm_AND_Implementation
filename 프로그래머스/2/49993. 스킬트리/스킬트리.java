d8e6321ff95011c2d657c23b74f21f5faf54bcae
Haon <msung6924@kakao.com>
2025-04-03 10:46:51 +0900

    [level 2] Title: 스킬트리, Time: 1.91 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,24 +1,20 @@
 class Solution {
     public static int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-
-        for (String skill_tree : skill_trees) {
-            StringBuilder filteredSkills = new StringBuilder();
-
-            // skill_tree에서 skill에 포함된 문자만 순서대로 추출
-            for (int i = 0; i < skill_tree.length(); i++) {
-                char c = skill_tree.charAt(i);
-                if (skill.indexOf(c) != -1) { // skill에 해당 문자가 포함되어 있는지 확인
-                    filteredSkills.append(c);
+        int ans = 0;
+        for(String skill_tree : skill_trees) {
+            String filtered = "";
+            for (char c : skill_tree.toCharArray()) {
+                if (skill.contains(String.valueOf(c))) {
+                    filtered += String.valueOf(c);
                 }
             }
 
-            // 필터링된 스킬 순서가 skill의 앞부분과 일치하는지 확인
-            if (skill.startsWith(filteredSkills.toString())) {
-                answer++;
+
+
+            if(skill.startsWith(filtered.toString())) {
+                ans++;
             }
         }
-
-        return answer;
+        return ans;
     }
 }
